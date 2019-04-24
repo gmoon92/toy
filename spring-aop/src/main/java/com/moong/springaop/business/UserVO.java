8618ec35f2f20131451b9130c5572a75469ae6ff
gmun <gmun0929@gmail.com>
2019-04-24 19:53:52 +0900

    rename

---
+++ spring-aop/src/main/java/com/moong/springaop/business/UserVO.java
@@ -0,0 +1,18 @@
+package com.moong.springaop.business;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+	private long id;
+	private String name;
+	private Level level;
+	
+	public UserVO(long id, String name, Level level) {
+		this.id = id;
+		this.name = name;
+		this.level = level;
+	}
+	
+}
