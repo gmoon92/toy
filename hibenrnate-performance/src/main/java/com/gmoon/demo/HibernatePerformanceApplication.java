a13cdc6802ec4e99ea4807475e2e540c5c967d48
gmoon <gmoon@rsupport.com>
2020-12-22 01:24:44 +0900

    init project - 하이버네이트 성능 테스트

---
+++ hibenrnate-performance/src/main/java/com/gmoon/demo/HibernatePerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernatePerformanceApplication.class, args);
+    }
+
+}
