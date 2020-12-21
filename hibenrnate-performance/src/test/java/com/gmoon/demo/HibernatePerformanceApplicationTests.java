0145998a5ec7042284876498b2cedbb828249bee
gmoon92 <gmoon0929@gmail.com>
2020-12-22 01:24:44 +0900

    init project - 하이버네이트 성능 테스트

---
+++ hibenrnate-performance/src/test/java/com/gmoon/demo/HibernatePerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatePerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
