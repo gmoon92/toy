a6641f6328c74767819042720572cbe8cb1741d4
gmoon92 <gmoon0929@gmail.com>
2020-12-29 09:56:29 +0900

    구글 스프레드 시트 Java 연동 - 불필요한 코드 제거

--- google-spreadsheet/src/test/java/com/gmoon/spreadsheet/utils/GoogleSpreadSheetToResourcesTest.java
+++ google-spreadsheet/src/test/java/com/gmoon/spreadsheet/utils/GoogleSpreadSheetToResourcesTest.java
@@ -1,17 +1,12 @@
 package com.gmoon.spreadsheet.utils;
 
 import com.gmoon.spreadsheet.config.GoogleSpreadSheetProperties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestConstructor;
 
 @Slf4j
-@SpringBootTest(classes = { GoogleSpreadSheetToResources.class
-        , GoogleSpreadSheetProperties.class })
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@RequiredArgsConstructor
+@SpringBootTest(classes = { GoogleSpreadSheetToResources.class, GoogleSpreadSheetProperties.class })
 class GoogleSpreadSheetToResourcesTest {
 
     @Test
