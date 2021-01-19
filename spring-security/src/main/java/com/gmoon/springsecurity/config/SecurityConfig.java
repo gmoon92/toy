f8c593406319e581b6c44d6e7892c393cff6a664
gmoon <gmoon@rsupport.com>
2021-01-20 00:40:53 +0900

    spring security #1 웹페이지 만들기

---
+++ spring-security/src/main/java/com/gmoon/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.gmoon.springsecurity.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+}
