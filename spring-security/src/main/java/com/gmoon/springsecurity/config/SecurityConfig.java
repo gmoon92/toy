af1926514e89cb834bb95e7c5691751d13f63435
gmoon92 <gmoon0929@gmail.com>
2021-01-20 01:06:29 +0900

    spring security #1 웹페이지 만들기 - 해결된 문제 - 인증(Authentication) 가능 - 인증/미인증 url 설정 - 현재 사용자 정보를 알 수 있다. todo - 계정 생성 & Role - 비밀번호 로그...

--- spring-security/src/main/java/com/gmoon/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/gmoon/springsecurity/config/SecurityConfig.java
@@ -2,9 +2,24 @@ package com.gmoon.springsecurity.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Slf4j
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests() // [1] 인가(Authorization) 설정, 어떤 요청들을 어떻게 설정 할지
+            .mvcMatchers("/", "/info").permitAll()
+            .mvcMatchers("/admin").hasRole("ADMIN")
+            .anyRequest().authenticated(); // etc... 나머지 요청은 인증처리를 하겠다.
+
+    http.formLogin(); // [2] form login 설정
+    http.httpBasic();
+//    https://www.baeldung.com/spring-security-basic-authentication
+  }
 }
