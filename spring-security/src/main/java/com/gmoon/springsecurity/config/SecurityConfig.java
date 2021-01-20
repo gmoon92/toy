f74e640f1889a702b98259c5dfe345495191d5aa
gmoon92 <gmoon0929@gmail.com>
2021-01-20 22:46:07 +0900

    spring security #1 웹페이지 만들기 - PasswordEncoder - PasswordEncoderFactories - bcrypt 단반향 알고리즘 사용 todo security test

--- spring-security/src/main/java/com/gmoon/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/gmoon/springsecurity/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 
 }
