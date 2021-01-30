3f23f4eec64040335f2aaaae03ac28e43b14b1a9
gmoon <gmoon@rsupport.com>
2021-01-30 19:24:54 +0900

    Spring data jpa envers 설정 - todo : Spring LifeCycle Listener Bean 주입

--- hibernate-envers/src/main/java/com/gmoon/hibernateenvers/HibernateEnversApplication.java
+++ hibernate-envers/src/main/java/com/gmoon/hibernateenvers/HibernateEnversApplication.java
@@ -2,11 +2,7 @@ package com.gmoon.hibernateenvers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
-@EnableJpaRepositories
 @SpringBootApplication
 public class HibernateEnversApplication {
 
