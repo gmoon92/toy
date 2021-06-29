34b6ec1b523d928a964cef93b3d9b12f2d4685a7
gmoon92 <gmoon0929@gmail.com>
2021-06-29 09:57:56 +0900

    feat AccessDecisionManager 학습 테스트 관련 코드 추가
    - ExpressionHandler
    - Role Hierarchy

---
+++ spring-security/src/main/java/com/gmoon/springsecurity/account/MemberRole.java
@@ -0,0 +1,14 @@
+package com.gmoon.springsecurity.account;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberRole {
+  ADMIN("ADMIN", "ROLE_ADMIN"),
+  USER("USER", "ROLE_USER");
+
+  private final String value;
+  private final String name;
+}
