af4d9e7dba8929e9c13a0b88aafb45033c7a3b08
gmoon92 <gmoon0929@gmail.com>
2021-07-11 17:13:22 +0900

    refactor move class packaging

--- hibenrnate-performance/src/test/java/com/gmoon/demo/member/repository/MemberRepositoryTest.java
+++ hibenrnate-performance/src/test/java/com/gmoon/demo/member/repository/MemberRepositoryTest.java
@@ -1,10 +1,10 @@
-package com.gmoon.demo.repository;
+package com.gmoon.demo.member.repository;
 
 import com.gmoon.demo.base.BaseRepositoryTest;
-import com.gmoon.demo.domain.Member;
-import com.gmoon.demo.domain.MemberOption;
 import com.gmoon.demo.domain.QMember;
 import com.gmoon.demo.domain.QMemberOption;
+import com.gmoon.demo.member.Member;
+import com.gmoon.demo.member.MemberOption;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
