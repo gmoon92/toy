2e801b69ceb1a1a5d25bc9fadf6426ce18ee29c9
gmoon92 <gmoon0929@gmail.com>
2021-07-12 08:52:37 +0900

    refactor JPA 성능 - line 줄맞춤

--- hibenrnate-performance/src/main/java/com/gmoon/demo/team/repository/TeamRepository.java
+++ hibenrnate-performance/src/main/java/com/gmoon/demo/team/repository/TeamRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-    Team findByName(String name);
+  Team findByName(String name);
 }
