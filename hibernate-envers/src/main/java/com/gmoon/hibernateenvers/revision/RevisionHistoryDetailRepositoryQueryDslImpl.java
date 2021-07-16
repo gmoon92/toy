904393fcdf8fe313f4a77b6ade05398993afdb81
gmoon <gmoon@rsupport.com>
2021-07-16 21:52:22 +0900

    refactor hibernate 엔퍼스 모듈 - 컴파일 에러 이슈로 인한 코드 수정

--- hibernate-envers/src/main/java/com/gmoon/hibernateenvers/revision/RevisionHistoryDetailRepositoryQueryDslImpl.java
+++ hibernate-envers/src/main/java/com/gmoon/hibernateenvers/revision/RevisionHistoryDetailRepositoryQueryDslImpl.java
@@ -46,50 +46,50 @@ public class RevisionHistoryDetailRepositoryQueryDslImpl extends QuerydslReposit
 
   @Override
   public Optional<RevisionHistoryDetail> findPreRevisionHistoryDetail(RevisionHistoryDetail detail) {
-    QRevisionHistory qHistory = QRevisionHistory.revisionHistory;
-    QRevisionHistoryDetail qHistoryDetail = QRevisionHistoryDetail.revisionHistoryDetail;
+    QRevisionHistory history = QRevisionHistory.revisionHistory;
+    QRevisionHistoryDetail historyDetail = QRevisionHistoryDetail.revisionHistoryDetail;
 
     JPAQuery<RevisionHistoryDetail> query = new JPAQuery(entityManager);
-    query.select(qHistoryDetail)
-            .from(qHistory)
-            .innerJoin(qHistory.modifiedEntities, qHistoryDetail)
-            .where(qHistoryDetail.revision.id.eq(JPAExpressions.select(qHistory.id.max())
-                    .from(qHistory)
-                    .innerJoin(qHistory.modifiedEntities, qHistoryDetail)
-                    .where(qHistoryDetail.entityId.eq(detail.getEntityId())
-                            .and(qHistoryDetail.revisionTarget.eq(detail.getRevisionTarget()))
-                            .and(qHistoryDetail.revision.id.lt(detail.getRevision().getId())))));
+    query.select(historyDetail)
+            .from(history)
+            .innerJoin(history.details, historyDetail)
+            .where(historyDetail.revision.id.eq(JPAExpressions.select(history.id.max())
+                    .from(history)
+                    .innerJoin(history.details, historyDetail)
+                    .where(historyDetail.entityId.eq(detail.getEntityId())
+                            .and(historyDetail.revisionTarget.eq(detail.getRevisionTarget()))
+                            .and(historyDetail.revision.id.lt(detail.getRevision().getId())))));
     return Optional.ofNullable(query.fetchOne());
   }
 
   @Override
   public List<RevisionHistoryDetail> findAllByRevisionAndTarget(Long revisionNumber, RevisionTarget target) {
-    QRevisionHistoryDetail qHistoryDetail = QRevisionHistoryDetail.revisionHistoryDetail;
+    QRevisionHistoryDetail historyDetail = QRevisionHistoryDetail.revisionHistoryDetail;
 
     JPAQuery<RevisionHistoryDetail> query = new JPAQuery(entityManager);
-    return query.select(qHistoryDetail)
-            .from(qHistoryDetail)
-            .where(qHistoryDetail.revision.id.eq(revisionNumber)
-                    .and(qHistoryDetail.revisionTarget.eq(target)))
+    return query.select(historyDetail)
+            .from(historyDetail)
+            .where(historyDetail.revision.id.eq(revisionNumber)
+                    .and(historyDetail.revisionTarget.eq(target)))
             .fetch();
   }
 
   @Override
   public Page<RevisionListVO.DataVO> findAllBySearchVO(RevisionListVO.SearchVO searchVO) {
-    QRevisionHistory qHistory = QRevisionHistory.revisionHistory;
-    QRevisionHistoryDetail qHistoryDetail = QRevisionHistoryDetail.revisionHistoryDetail;
+    QRevisionHistory history = QRevisionHistory.revisionHistory;
+    QRevisionHistoryDetail historyDetail = QRevisionHistoryDetail.revisionHistoryDetail;
 
     JPAQuery<RevisionListVO.DataVO> query = new JPAQuery(entityManager);
 
     Pageable pageable = searchVO.getPageable();
-    query.select(new QRevisionListVO_DataVO(qHistory.id, qHistory.createdDt
-            , qHistory.updatedBy, qHistory.updatedByUsername
-            , qHistoryDetail.revisionTarget, qHistoryDetail.entityId
-            , qHistoryDetail.targetTeamName, qHistoryDetail.targetMemberName))
-            .from(qHistory)
-            .innerJoin(qHistory.modifiedEntities, qHistoryDetail)
-            .where(qHistoryDetail.revisionEventStatus.eq(RevisionEventStatus.DIRTY_CHECKING)
-                    .and(qHistoryDetail.revisionType.eq(RevisionType.MOD))
+    query.select(new QRevisionListVO_DataVO(history.id, history.createdDt
+            , history.updatedBy, history.updatedByUsername
+            , historyDetail.revisionTarget, historyDetail.entityId
+            , historyDetail.targetMemberName))
+            .from(history)
+            .innerJoin(history.details, historyDetail)
+            .where(historyDetail.revisionEventStatus.eq(RevisionEventStatus.DIRTY_CHECKING)
+                    .and(historyDetail.revisionType.eq(RevisionType.MOD))
                     .and(getSearchCondition(searchVO)));
 
     List<RevisionListVO.DataVO> list = getQuerydsl().applyPagination(pageable, query).fetch();
@@ -99,29 +99,26 @@ public class RevisionHistoryDetailRepositoryQueryDslImpl extends QuerydslReposit
   private BooleanBuilder getSearchCondition(RevisionListVO.SearchVO search) {
     BooleanBuilder builder = new BooleanBuilder();
 
-    QRevisionHistory qHistory = QRevisionHistory.revisionHistory;
-    QRevisionHistoryDetail qHistoryDetail = QRevisionHistoryDetail.revisionHistoryDetail;
+    QRevisionHistory history = QRevisionHistory.revisionHistory;
+    QRevisionHistoryDetail historyDetail = QRevisionHistoryDetail.revisionHistoryDetail;
 
     Date startDt = search.getStartDt();
     Date endDt = search.getEndDt();
 
     if (startDt != null)
-      builder.and(qHistory.createdDt.goe(startDt));
+      builder.and(history.createdDt.goe(startDt));
 
     if (endDt != null)
-      builder.and(qHistory.createdDt.loe(endDt));
+      builder.and(history.createdDt.loe(endDt));
 
     String searchKeyword = StringUtils.defaultString(search.getSearchKeyword());
     RevisionListVO.SearchVO.SearchType keywordCondition = search.getSearchType();
     switch (keywordCondition) {
       case MEMBER_NAME:
-        builder.and(qHistory.updatedByUsername.contains(searchKeyword));
+        builder.and(history.updatedByUsername.contains(searchKeyword));
         break;
       case TARGET_MEMBER_NAME:
-        builder.and(qHistoryDetail.targetMemberName.contains(searchKeyword));
-        break;
-      case TARGET_TEAM_NAME:
-        builder.and(qHistoryDetail.targetTeamName.contains(searchKeyword));
+        builder.and(historyDetail.targetMemberName.contains(searchKeyword));
         break;
     }
     return builder;
