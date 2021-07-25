93de08bc4ef4f7ba1053fc5accdfad276eff8915
gmoon92 <gmoon0929@gmail.com>
2021-07-25 19:06:21 +0900

    feat 신청서 도메인 추가

--- hibenrnate-performance/src/main/java/com/gmoon/demo/apply_form/ApplyForm.java
+++ hibenrnate-performance/src/main/java/com/gmoon/demo/apply_form/ApplyForm.java
@@ -1,11 +1,15 @@
 package com.gmoon.demo.apply_form;
 
+import com.gmoon.demo.base.EntityId;
 import com.gmoon.demo.member.domain.Member;
 import com.gmoon.demo.team.Team;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -13,9 +17,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-@Entity
 @Table
-@EqualsAndHashCode
+@Entity
+@Getter
+@EqualsAndHashCode(of = "id")
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApplyForm {
 
@@ -30,16 +36,41 @@ public class ApplyForm {
   @JoinColumn(name = "team_id", insertable = false, updatable = false)
   private Team team;
 
+  private String title;
+
+  private String content;
+
+  ApplyForm(Member member, Team team) {
+    this.member = member;
+    this.team = team;
+    this.id = new Id(member, team);
+  }
+
+  public static ApplyForm newInstance(Member member, Team team) {
+    return new ApplyForm(member, team);
+  }
+
+  public void fillOut(String title, String content) {
+    this.title = title;
+    this.content = content;
+  }
+
   @Embeddable
   @EqualsAndHashCode
   @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  public static class Id {
-    private Member member;
-    private Team team;
+  static class Id extends EntityId {
 
-    public Id(Member member, Team team) {
-      this.member = member;
-      this.team = team;
+    private static final long serialVersionUID = -4188030215536759764L;
+
+    @Column(name = "member_id")
+    private Long member;
+
+    @Column(name = "team_id")
+    private Long team;
+
+    Id(Member member, Team team) {
+      this.member = member.getId();
+      this.team = team.getId();
     }
   }
 
---
+++ hibenrnate-performance/src/main/java/com/gmoon/demo/base/EntityId.java
@@ -0,0 +1,8 @@
+package com.gmoon.demo.base;
+
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class EntityId implements Serializable {
+}
