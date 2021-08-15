07beac815a40c9751c27173edd7575e3601907e2
gmoon92 <gmoon0929@gmail.com>
2021-08-15 21:24:07 +0900

    refactor 맞는 패키지 메소드 이동

--- spring-security/src/main/java/com/gmoon/springsecurity/account/UserController.java
+++ spring-security/src/main/java/com/gmoon/springsecurity/account/UserController.java
@@ -1,15 +1,59 @@
 package com.gmoon.springsecurity.account;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.gmoon.springsecurity.sample.SampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
-  @Autowired
-  private AccountRepository accountRepository;
+  private final AccountRepository accountRepository;
+  private final SampleService sampleService;
+
+  @GetMapping("/")
+  public String index(Model model, Principal principal) {
+    String message;
+    if (principal == null) {
+      message = "Hello, Spring Security";
+    } else {
+      message = principal.getName();
+    }
+
+    model.addAttribute("message", String.format("Hello, %s", message));
+    return "index";
+  }
+
+  @GetMapping("/info")
+  public String info(Model model) {
+    model.addAttribute("message", "Hello, Spring Security");
+    return "info";
+  }
+
+  @GetMapping("/admin")
+  public String admin(Model model, Principal principal) {
+    model.addAttribute("message", "Hello, Admin" + principal.getName());
+    return "admin";
+  }
+
+  @GetMapping("/user")
+  public String user(Model model, Principal principal) {
+    model.addAttribute("message", "Hello, User" + principal.getName());
+    return "user";
+  }
+
+  @GetMapping("/dashboard")
+  public String dashboard(Model model, Principal principal) {
+    model.addAttribute("message", "Hello, Spring Security" + principal.getName());
+    AccountContext.setAccount(accountRepository.findByUsername(principal.getName()));
+
+    sampleService.dashboard();
+    return "dashboard";
+  }
 
   @GetMapping("/user/list")
   public String list(Model model) {
