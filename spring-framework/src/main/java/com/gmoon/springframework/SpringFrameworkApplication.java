d19d9af61ee5aa98265e0c1f8a5f404fa1a00126
gmoon92 <gmoon0929@gmail.com>
2021-08-22 22:13:31 +0900

    chore init Spring Framework leaning project

---
+++ spring-framework/src/main/java/com/gmoon/springframework/SpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringFrameworkApplication.class, args);
+  }
+
+}
