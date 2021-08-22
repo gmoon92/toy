69a5569620110076e5315c62b11af2321ea41e25
gmoon92 <gmoon0929@gmail.com>
2021-08-22 09:46:32 +0900

    feat BoardRepository Principal 접근 쿼리 추가
    - Spring Security Data 적용

--- spring-security/src/main/java/com/gmoon/springsecurity/board/BoardRepository.java
+++ spring-security/src/main/java/com/gmoon/springsecurity/board/BoardRepository.java
@@ -1,6 +1,12 @@
 package com.gmoon.springsecurity.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+  @Query("select b from Board b where b.author.id = ?#{principal.account.id}")
+  List<Board> findCurrentUserBoards();
 }
