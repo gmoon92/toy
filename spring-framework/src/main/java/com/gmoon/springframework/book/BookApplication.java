3c2bde01712720f7b66e9039a0efa4ccc772bd80
gmoon <gmoon@rsupport.com>
2021-09-03 23:51:20 +0900

    feat spring framework 핵심 기술 - 빈 설정 Java 방식 추가
    - @Bean
    - @ComponentScan

--- spring-framework/src/main/java/com/gmoon/springframework/book/BookApplication.java
+++ spring-framework/src/main/java/com/gmoon/springframework/book/BookApplication.java
@@ -1,6 +1,8 @@
 package com.gmoon.springframework.book;
 
+import com.gmoon.springframework.config.ApplicationConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
@@ -8,16 +10,25 @@ import java.util.Arrays;
 public class BookApplication {
 
   public static void main(String[] args) {
-    ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+    ApplicationContext context = getAnnotationApplicationContext();
     String[] beanDefinitionNames = context.getBeanDefinitionNames();
     System.out.println(Arrays.toString(beanDefinitionNames));
 
     BookService bookService1 = context.getBean(BookService.class);
-    System.out.println("bookService: " + bookService1);
+    System.out.println("bookService1: " + bookService1);
     BookService bookService2 = (BookService) context.getBean("bookService");
-    System.out.println("bookService: " + bookService2);
+    System.out.println("bookService2: " + bookService2);
 
     boolean isSingleton = bookService1 == bookService2;
     System.out.println("is singleton " + isSingleton);
   }
+
+  private static ApplicationContext getXmlApplicationContext() {
+    return new ClassPathXmlApplicationContext("application.xml");
+  }
+
+  private static ApplicationContext getAnnotationApplicationContext() {
+    return new AnnotationConfigApplicationContext(ApplicationConfig.class);
+  }
+
 }
