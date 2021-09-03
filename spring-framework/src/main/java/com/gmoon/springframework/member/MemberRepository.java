6526ac8a3644d0a04d89bb5c5f853b9b13d9b097
gmoon92 <gmoon0929@gmail.com>
2021-09-03 23:22:26 +0900

    feat spring framework 핵심 기술 - IoC 관련 예제 추가
    - 사용자 서비스
    - 사용자 레파지토리
    - 사용자 javaBean

---
+++ spring-framework/src/main/java/com/gmoon/springframework/member/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.gmoon.springframework.member;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+  public Member save(Member member) {
+    return null;
+  }
+}
---
+++ spring-framework/src/test/java/com/gmoon/springframework/member/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package com.gmoon.springframework.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class MemberServiceTest {
+
+  @Mock
+  MemberRepository memberRepository;
+
+  @Test
+  @DisplayName("IoC 개념, memberRepository mocking")
+  void save() {
+    // given
+    Member member = new Member();
+
+    // when
+    when(memberRepository.save(member)).thenReturn(member);
+    MemberService memberService = new MemberService(memberRepository);
+
+    // then
+    assertThat(memberService.save(member))
+            .isNotNull()
+            .hasFieldOrPropertyWithValue("status", MemberStatus.ENABLE);
+  }
+}
