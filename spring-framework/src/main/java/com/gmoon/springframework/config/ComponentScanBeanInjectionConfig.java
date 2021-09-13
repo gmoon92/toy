26c41120e08ad4d4a1f9fcadc20502f94fd4c150
gmoon <gmoon@rsupport.com>
2021-09-13 23:12:41 +0900

    refactor: spring bean injection 테스트 설정 파일 테스트 패키지로 이동

--- spring-framework/src/main/java/com/gmoon/springframework/config/ComponentScanBeanInjectionConfig.java
+++ spring-framework/src/main/java/com/gmoon/springframework/config/ComponentScanBeanInjectionConfig.java
@@ -1,10 +1,10 @@
 package com.gmoon.springframework.config;
 
-import com.gmoon.springframework.book.BookApplication;
+import com.gmoon.springframework.SpringFrameworkApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = BookApplication.class)
+@ComponentScan(basePackageClasses = SpringFrameworkApplication.class)
 public class ComponentScanBeanInjectionConfig {
 }
