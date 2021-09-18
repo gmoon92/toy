19a198649fd111e6252d13546a3b390031efaf57
gmoon <gmoon@rsupport.com>
2021-09-18 21:13:59 +0900

    feat: spring framwork 핵심 기술 - bean scope 추가

---
+++ spring-framework/src/main/java/com/gmoon/springframework/scope/Proto.java
@@ -0,0 +1,10 @@
+package com.gmoon.springframework.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Proto {
+}
---
+++ spring-framework/src/main/java/com/gmoon/springframework/scope/ScopeAppTestRunner.java
@@ -0,0 +1,32 @@
+package com.gmoon.springframework.scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScopeAppTestRunner implements ApplicationRunner {
+
+  @Autowired
+  ApplicationContext applicationContext;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    System.out.println("proto");
+    System.out.println(applicationContext.getBean(Proto.class));
+    System.out.println(applicationContext.getBean(Proto.class));
+    System.out.println(applicationContext.getBean(Proto.class));
+
+    System.out.println("single");
+    System.out.println(applicationContext.getBean(Single.class));
+    System.out.println(applicationContext.getBean(Single.class));
+    System.out.println(applicationContext.getBean(Single.class));
+
+    System.out.println("proto by single");
+    System.out.println(applicationContext.getBean(Single.class).getProto());
+    System.out.println(applicationContext.getBean(Single.class).getProto());
+    System.out.println(applicationContext.getBean(Single.class).getProto());
+  }
+}
