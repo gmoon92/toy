39fc474f139ed5c96d4f6e3f0fc1a1c9c2fd3c5d
gmoon <gmoon@rsupport.com>
2021-09-21 02:18:43 +0900

    feat: environment getProperty 테스트 코드 추가
    - @Value
    - getProperty

--- spring-framework/src/test/java/com/gmoon/springframework/environment/EnvironmentConfigTest.java
+++ spring-framework/src/test/java/com/gmoon/springframework/environment/EnvironmentConfigTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Import(EnvironmentConfigTest.ProfileTestConfig.class)
-@TestPropertySource(value = "classpath:environment.properties")
+@TestPropertySource(value = "classpath:/environment.properties")
 class EnvironmentConfigTest {
 
   @Autowired
@@ -29,9 +30,12 @@ class EnvironmentConfigTest {
   @Autowired
   Environment environment;
 
+  @Value("${app.about}")
+  String appAbout;
+
   @Test
   @DisplayName("spring.profiles.active=alpha")
-  void getProperties() {
+  void getAlphaProperty() {
     // given
     String alphaProfileName = "alpha";
 
@@ -43,6 +47,20 @@ class EnvironmentConfigTest {
     // then
     assertThat(activeProfiles)
             .contains(alphaProfileName);
+  }
+
+  @Test
+  @DisplayName("app.about=spring-learning-app")
+  void getAppAboutProperty() {
+    // given
+    String key = "app.about";
+
+    // when
+    String value = environment.getProperty(key);
+
+    assertThat(value)
+            .isEqualTo("spring-learning-app")
+            .isEqualTo(appAbout);
   }
 
   @Test
