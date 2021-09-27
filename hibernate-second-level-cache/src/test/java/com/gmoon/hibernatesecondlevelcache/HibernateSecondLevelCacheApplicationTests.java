551be1cb94fb7d7ffe433dd6cef4e89ab0a8adae
gmoon92 <gmoon0929@gmail.com>
2021-09-27 23:09:23 +0900

    init JPA 2차 캐시 학습 프로젝트

---
+++ hibernate-second-level-cache/src/test/java/com/gmoon/hibernatesecondlevelcache/HibernateSecondLevelCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernatesecondlevelcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSecondLevelCacheApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
