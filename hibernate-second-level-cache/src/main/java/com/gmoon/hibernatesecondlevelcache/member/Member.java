331f6fbf754c2c60ceabca699a93121531cc060b
gmoon92 <gmoon0929@gmail.com>
2021-09-28 11:20:17 +0900

    feat: ehcache3 테스트 용, 도메인 레파지토리 추가

---
+++ hibernate-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/member/Member.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernatesecondlevelcache.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+  private Long id;
+  private String name;
+
+}
