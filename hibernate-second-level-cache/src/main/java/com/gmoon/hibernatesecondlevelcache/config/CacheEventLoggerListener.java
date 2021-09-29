ba9ea0b06d4e961ad5a56ec894804f36fb654f07
gmoon <gmoon@rsupport.com>
2021-09-29 09:34:17 +0900

    refactor: cache event listener re namming

--- hibernate-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/CacheEventLoggerListener.java
+++ hibernate-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/CacheEventLoggerListener.java
@@ -4,9 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.cache.event.CacheEntryEvent;
 import javax.cache.event.CacheEntryListenerException;
+import javax.cache.event.EventType;
 
 @Slf4j
-public class CacheEventListener implements CacheEventLoggerListener<Object, Object> {
+public class CacheEventLoggerListener implements CacheEventListener<Object, Object> {
 
   @Override
   public void onCreated(Iterable<CacheEntryEvent<?, ?>> events) throws CacheEntryListenerException {
@@ -26,5 +27,15 @@ public class CacheEventListener implements CacheEventLoggerListener<Object, Obje
   @Override
   public void onUpdated(Iterable<CacheEntryEvent<?, ?>> events) throws CacheEntryListenerException {
     logging(events);
+  }
+
+  private void logging(Iterable<CacheEntryEvent<?,?>> events) {
+    events.forEach(event -> {
+      EventType eventType = event.getEventType();
+      Object key = event.getKey();
+      Object oldValue = event.getOldValue();
+      Object value = event.getValue();
+      log.info("cache {} event. key: {}, oldValue: {}, newValue: {}", eventType, key, oldValue, value);
+    });
   }
 }
