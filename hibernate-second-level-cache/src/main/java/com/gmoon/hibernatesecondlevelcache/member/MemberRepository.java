380c65baf9edde9eef1dfa3f7049c124fa3f1d9e
gmoon <gmoon@rsupport.com>
2021-09-30 13:06:27 +0900

    feat(ehcach3): Cache Config valueType Object로 지정
    - Value Type Object 지정해도 리스트 자동 형변환

--- hibernate-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/member/MemberRepository.java
+++ hibernate-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/member/MemberRepository.java
@@ -1,19 +1,40 @@
 package com.gmoon.hibernatesecondlevelcache.member;
 
 import com.gmoon.hibernatesecondlevelcache.config.CacheConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
 @Repository
 public class MemberRepository {
 
   @Cacheable(value = CacheConfig.MEMBER_FIND_BY_ID, key = "#id")
   public Member getId(Long id) throws InterruptedException {
+    log.info("getId start...");
     Thread.sleep(2000);
 
     Member member = new Member();
     member.setId(id);
     member.setName("admin");
     return member;
+  }
+
+  @Cacheable(value = CacheConfig.MEMBER_ALL)
+  public List<Member> getAll() throws InterruptedException {
+    log.info("getAll start...");
+    Thread.sleep(3000);
+
+    Member adminUser = new Member();
+    adminUser.setId(0L);
+    adminUser.setName("admin");
+
+    Member memberUser = new Member();
+    memberUser.setId(1L);
+    memberUser.setName("admin");
+    return Arrays.asList(adminUser, memberUser);
   }
 }
