82536693c93d9fde0eea107b1fbea3ba04f73869
gmoon92 <gmoon0929@gmail.com>
2021-10-07 21:34:16 +0900

    feat(spring-async): Server 도메인 추가
    - Protocol 이넘 타입 추가

---
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/server/Protocol.java
@@ -0,0 +1,34 @@
+package com.gmoon.springasync.server;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.Getter;
+
+@Getter
+public enum Protocol {
+	HTTPS("https", true),
+	HTTP("http");
+
+	private static final Map<Boolean, Protocol> OPERATOR_MAP = Collections.unmodifiableMap(Stream.of(values())
+		.collect(Collectors.toMap(Protocol::isUseSsl, Function.identity())));
+
+	private final String value;
+	private final boolean useSsl;
+
+	Protocol(String value) {
+		this(value, false);
+	}
+
+	Protocol(String value, boolean useSsl) {
+		this.value = value;
+		this.useSsl = useSsl;
+	}
+
+	public static Protocol of(boolean useSsl) {
+		return OPERATOR_MAP.get(useSsl);
+	}
+}
---
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/server/Server.java
@@ -0,0 +1,46 @@
+package com.gmoon.springasync.server;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+public class Server {
+	private static final String GITHUB_BLOG_DOMAIN = "gmoon92.github.io";
+
+	private final String host;
+	private final String port1;
+	private final String port2;
+
+	public Server(String host, String port1, String port2) {
+		this.host = host;
+		this.port1 = port1;
+		this.port2 = port2;
+	}
+
+	public static Server createGithubBlogServer() {
+		return new Server(GITHUB_BLOG_DOMAIN, "443", "80");
+	}
+
+	public String getWebServerWithoutPortUrl(boolean useSsl) {
+		return getWebServerUrl(useSsl, false);
+	}
+
+	public String getWebServerUrl(boolean useSsl) {
+		return getWebServerUrl(useSsl, true);
+	}
+
+	private String getWebServerUrl(boolean useSsl, boolean includePort) {
+		Protocol protocol = Protocol.of(useSsl);
+		return String.format("%s://%s%s", protocol.getValue(), host, getPort(protocol, includePort));
+	}
+
+	private String getPort(Protocol protocol, boolean includePort) {
+		if (!includePort) {
+			return "";
+		}
+
+		String port = Protocol.HTTPS == protocol ? port1 : port2;
+		return String.format(":%s", port);
+	}
+}
