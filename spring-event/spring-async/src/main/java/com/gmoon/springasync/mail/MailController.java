80f87bdc585d9fe2d9c2f769b684f457db93895e
gmoon92 <gmoon0929@gmail.com>
2021-10-20 00:50:29 +0900

    refactor(spring-async): 메일 발송 로직 Stream API 사용

--- spring-event/spring-async/src/main/java/com/gmoon/springasync/mail/MailController.java
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/mail/MailController.java
@@ -1,5 +1,7 @@
 package com.gmoon.springasync.mail;
 
+import com.gmoon.springasync.server.Server;
+import com.gmoon.springasync.server.ServerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("mail")
 public class MailController {
   private final MailService mailService;
+  private final ServerService serverService;
 
   @PostMapping("/send/invite")
   public ResponseEntity<String> sendInviteMailFromMyGithub() {
-    mailService.sendInviteMailFromMyGithubToAllUser();
+    Server githubServer = Server.createGithubBlogServer();
+    String githubServerUrl = serverService.getWebServerWithoutPortUrl(githubServer);
+    
+    mailService.sendInviteMailFrom(githubServerUrl);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
