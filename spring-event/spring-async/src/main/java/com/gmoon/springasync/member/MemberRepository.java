47eb38cba6458b9f536ec996b4a4d8e26837b236
gmoon92 <gmoon0929@gmail.com>
2021-10-19 23:59:26 +0900

    feat(spring-async): MemberRepository Stream 반환 메서드 추가

--- spring-event/spring-async/src/main/java/com/gmoon/springasync/member/MemberRepository.java
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/member/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.gmoon.springasync.member;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
+
+  @Query("SELECT m FROM Member m")
+  Stream<Member> streamFindAll();
 }
