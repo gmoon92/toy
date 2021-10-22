9d56191fd534664646e55599c9732a1359c4ac25
gmoon92 <gmoon0929@gmail.com>
2021-10-22 23:47:28 +0900

    test(spring-async): TaskExecutor 빈 테스트 코드 추가
    - SimpleAsyncTaskExecutor으로 default를 사용하지 않고 ThreadPoolTaskExecutor를 사용

--- spring-event/spring-async/src/test/java/com/gmoon/springasync/mail/MailServiceTest.java
+++ spring-event/spring-async/src/test/java/com/gmoon/springasync/mail/MailServiceTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.MailSender;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.concurrent.Future;
@@ -25,6 +29,24 @@ class MailServiceTest {
 
   @Mock
   MailSender mailSender;
+
+  @Autowired
+  BeanFactory beanFactory;
+
+  @Test
+  @DisplayName("현재 프로젝트에서 @EnableAsync에서 주입하고 있는 Task Executor 를 알아보자")
+  void taskExecutorName() {
+    // given
+    Class<TaskExecutor> asyncExecutor = TaskExecutor.class;
+
+    // when
+    Object executor = beanFactory.getBean(asyncExecutor);
+
+    // then
+    assertThat(executor)
+            .isNotInstanceOf(SimpleAsyncTaskExecutor.class)
+            .isInstanceOf(ThreadPoolTaskExecutor.class);
+  }
 
   @Test
   @DisplayName("비동기시 초대 메일을 보낸 서버 주소를 반환 받는다.")
