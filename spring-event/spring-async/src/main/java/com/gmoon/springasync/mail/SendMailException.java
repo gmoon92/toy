e27aa8864b209bee1e7531561babe040f3bb89d1
gmoon92 <gmoon0929@gmail.com>
2021-10-23 11:30:36 +0900

    feat(spring-async): 비동기 메서드 강제 에러 반환 코드 추가
    - 비동기 에러 처리를 위함

--- spring-event/spring-async/src/main/java/com/gmoon/springasync/mail/SendMailException.java
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/mail/SendMailException.java
@@ -5,6 +5,10 @@ public class SendMailException extends IllegalArgumentException {
 
   private static final String EXCEPTION_MESSAGE = "메일 발송 실패";
 
+  public SendMailException() {
+    super(EXCEPTION_MESSAGE);
+  }
+
   public SendMailException(Throwable throwable) {
     super(EXCEPTION_MESSAGE, throwable);
   }
