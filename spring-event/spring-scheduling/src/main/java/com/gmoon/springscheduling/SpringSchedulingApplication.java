b8db091aaeb1211b2f999f43f1f54a4fb398097e
gmoon92 <gmoon0929@gmail.com>
2021-11-04 00:35:08 +0900

    chore(spring-scheduling): init project

---
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/SpringSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSchedulingApplication.class, args);
+  }
+
+}
