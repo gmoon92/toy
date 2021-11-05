cb20974ef4ae65bcb154778ad9a230e4775a34ad
gmoon92 <gmoon0929@gmail.com>
2021-11-06 05:30:21 +0900

    feat(spring-scheduling): 비동기 @Scheduled 메서드 추가

---
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/jobs/SimpleScheduledJobs.java
@@ -0,0 +1,58 @@
+package com.gmoon.springscheduling.jobs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SimpleScheduledJobs {
+  private static final String FORMAT_OF_TASK_MESSAGE = "schedule tasks using {} jobs - {}";
+
+  @Async
+  @Scheduled(fixedDelay = 100)
+  public void fixedDelayTask() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "fixedDelayTask", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(fixedDelay = 100, initialDelay = 10)
+  public void fixedDelayWithInitialDelayTask() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "fixedDelayWithInitialDelayTask", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(fixedRate = 100)
+  public void fixedRateTask() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "fixedRateTask", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(cron = "* * * * * *", zone = "Asia/Seoul")
+  public void taskUsingCronExpression() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "taskUsingCronExpression", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(fixedDelayString = "${schedule.gmoon.fixedDelay.ms}")
+  public void fixedDelayTaskUsingExpression() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "fixedDelayTaskUsingExpression", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(fixedRateString = "${schedule.gmoon.fixedRate.ms}")
+  public void fixedRateTaskUsingExpression() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "fixedRateTaskUsingExpression", getCurrentSeconds());
+  }
+
+  @Async
+  @Scheduled(cron = "${schedule.gmoon.cron.expression}", zone = "Asia/Seoul")
+  public void taskUsingExternalizedCronExpression() {
+    log.info(FORMAT_OF_TASK_MESSAGE, "taskUsingExternalizedCronExpression", getCurrentSeconds());
+  }
+
+  private long getCurrentSeconds() {
+    return System.currentTimeMillis() / 1_000;
+  }
+}
---
+++ spring-event/spring-scheduling/src/test/java/com/gmoon/springscheduling/jobs/SimpleScheduledJobsTest.java
@@ -0,0 +1,78 @@
+package com.gmoon.springscheduling.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.atLeastOnce;
+
+@SpringBootTest
+class SimpleScheduledJobsTest {
+
+  @SpyBean
+  SimpleScheduledJobs simpleScheduledJobs;
+
+  @Test
+  void fixedDelayTask() throws InterruptedException {
+    // given when
+    Thread.sleep(200);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).fixedDelayTask();
+  }
+
+  @Test
+  void fixedDelayWithInitialDelayTask() throws InterruptedException {
+    // given when
+    Thread.sleep(200);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).fixedDelayWithInitialDelayTask();
+  }
+
+  @Test
+  void fixedRateTask() throws InterruptedException {
+    // given when
+    Thread.sleep(200);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).fixedRateTask();
+  }
+
+  @Test
+  void taskUsingCronExpression() throws InterruptedException {
+    // given when
+    Thread.sleep(1_000);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).taskUsingCronExpression();
+  }
+
+  @Test
+  void fixedDelayTaskUsingExpression() throws InterruptedException {
+    // given when
+    Thread.sleep(200);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).fixedDelayTaskUsingExpression();
+  }
+
+  @Test
+  void fixedRateTaskUsingExpression() throws InterruptedException {
+    // given when
+    Thread.sleep(200);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).fixedRateTaskUsingExpression();
+  }
+  
+  @Test
+  void taskUsingExternalizedCronExpression() throws InterruptedException {
+    // given when
+    Thread.sleep(1_000);
+
+    // then
+    then(simpleScheduledJobs).should(atLeastOnce()).taskUsingExternalizedCronExpression();
+  }
+}
