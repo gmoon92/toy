149b58c91ab68f3c26c5deb8f85c77f059eea773
gmoon92 <gmoon0929@gmail.com>
2021-11-06 04:44:41 +0900

    feat(spring-scheduling): @Scheduling 활성화 설정 추가

--- spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfig.java
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfig.java
@@ -2,9 +2,11 @@ package com.gmoon.springscheduling.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
+@EnableScheduling
 public class ThreadPoolTaskSchedulerConfig {
   private static final String NAME_OF_THREAD_SCHEDULER_TASK = "ThreadPoolTaskScheduler";
   private static final int SIZE_OF_POOL = 5;
