da3b65cdbcabad325cd8f1d2b459651edb39b33e
gmoon92 <gmoon0929@gmail.com>
2021-11-06 04:13:30 +0900

    feat(spring-scheduling): Spring TaskScheduler 설정 추가
    - ThreadPoolTaskSchedulerConfigTest 테스트 코드 추가
      - SimpleMessageLoggerTask 추가

---
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfig.java
@@ -0,0 +1,19 @@
+package com.gmoon.springscheduling.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class ThreadPoolTaskSchedulerConfig {
+  private static final String NAME_OF_THREAD_SCHEDULER_TASK = "ThreadPoolTaskScheduler";
+  private static final int SIZE_OF_POOL = 5;
+
+  @Bean
+  public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+    ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+    taskScheduler.setThreadNamePrefix(NAME_OF_THREAD_SCHEDULER_TASK);
+    taskScheduler.setPoolSize(SIZE_OF_POOL);
+    return taskScheduler;
+  }
+}
---
+++ spring-event/spring-scheduling/src/test/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfigTest.java
@@ -0,0 +1,65 @@
+package com.gmoon.springscheduling.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.Date;
+
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+
+@Slf4j
+@SpringBootTest
+class ThreadPoolTaskSchedulerConfigTest {
+
+  @Autowired
+  ThreadPoolTaskScheduler taskScheduler;
+
+  @Test
+  @DisplayName("스케쥴러는 1초뒤에 예약된 작업을 실행한다.")
+  void schedule() throws InterruptedException {
+    // given
+    Runnable task = spy(SimpleMessageLoggerTask.create("Test!!!!..."));
+
+    // when
+    executeTaskOnSchedulerAfterOneSeconds(task);
+    Thread.sleep(2_000);
+
+    // then
+    then(task).should(times(3)).run();
+  }
+
+  private void executeTaskOnSchedulerAfterOneSeconds(Runnable task) {
+    Date startTime = new Date(System.currentTimeMillis() + 1_000);
+
+    taskScheduler.schedule(task, startTime);
+    taskScheduler.schedule(task, startTime);
+    taskScheduler.schedule(task, startTime);
+  }
+
+  static class SimpleMessageLoggerTask implements Runnable {
+    private static final String FORMAT_OF_MESSAGE = "[%s] Runnable task with message: %s";
+
+    private final String message;
+
+    private SimpleMessageLoggerTask(String message) {
+      this.message = message;
+    }
+
+    static SimpleMessageLoggerTask create(String message) {
+      return new SimpleMessageLoggerTask(message);
+    }
+
+    @Override
+    public void run() {
+      Thread thread = Thread.currentThread();
+      String currentThreadName = thread.getName();
+      log.info(String.format(FORMAT_OF_MESSAGE, currentThreadName, message));
+    }
+  }
+}
