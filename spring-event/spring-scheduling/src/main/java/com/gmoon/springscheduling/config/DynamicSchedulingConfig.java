6e4848d09ed598469dc4ccb2f5e215251b85aac6
gmoon92 <gmoon0929@gmail.com>
2021-11-08 07:03:23 +0900

    feat(spring-scheduling): 런타임 동적 시간 트리거 추가
    - 실시간 0.5 초마다 로깅되도록 시간 추가

---
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/DynamicSchedulingConfig.java
@@ -0,0 +1,47 @@
+package com.gmoon.springscheduling.config;
+
+import com.gmoon.springscheduling.jobs.PhoneAlarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(value = "schedule.type", havingValue = "dynamic")
+@RequiredArgsConstructor
+public class DynamicSchedulingConfig implements SchedulingConfigurer {
+
+  private final PhoneAlarmService phoneAlarmService;
+
+  @Bean
+  public Executor taskExecutor() {
+    return Executors.newSingleThreadScheduledExecutor();
+  }
+
+  @Override
+  public void configureTasks(ScheduledTaskRegistrar registrar) {
+    registrar.setScheduler(taskExecutor());
+    registrar.addTriggerTask(phoneAlarmService::wakeUp, getDynamicDelayTimeTrigger());
+  }
+
+  private Trigger getDynamicDelayTimeTrigger() {
+    return context -> {
+      Optional<Date> lastCompletionTime = Optional.ofNullable(context.lastCompletionTime());
+      Instant nextExecutionTime = lastCompletionTime.orElseGet(Date::new)
+              .toInstant()
+              .plusMillis(phoneAlarmService.plusOneSecondsDelay());
+      return Date.from(nextExecutionTime);
+    };
+  }
+}
