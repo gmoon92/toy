d4c362ae2939a710ede754df7241d71cb8dd2c2a
gmoon92 <gmoon0929@gmail.com>
2021-11-08 22:51:51 +0900

    feat(spring-scheduling): 비동기 예외 핸들러 추가

---
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/CustomAsyncUncaughtExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.gmoon.springscheduling.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class CustomAsyncUncaughtExceptionHandler implements AsyncUncaughtExceptionHandler {
+  @Override
+  public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+    log.error("async uncaught exception....", ex);
+  }
+}
