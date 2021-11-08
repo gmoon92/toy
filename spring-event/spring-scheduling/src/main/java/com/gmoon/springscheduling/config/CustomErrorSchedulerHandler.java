2fce821b3295e8398a8ee0602213221a160d29c5
gmoon92 <gmoon0929@gmail.com>
2021-11-08 22:52:15 +0900

    refactor(spring-scheduling): 빈 순한 참조 코드 제거

--- spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/CustomErrorSchedulerHandler.java
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/CustomErrorSchedulerHandler.java
@@ -2,15 +2,10 @@ package com.gmoon.springscheduling.config;
 
 import com.gmoon.springscheduling.jobs.exception.PhoneAlarmSchedulerException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.util.ErrorHandler;
 
 @Slf4j
 public class CustomErrorSchedulerHandler implements ErrorHandler {
-
-  @Autowired
-  ThreadPoolTaskScheduler threadPoolTaskScheduler;
 
   @Override
   public void handleError(Throwable t) {
