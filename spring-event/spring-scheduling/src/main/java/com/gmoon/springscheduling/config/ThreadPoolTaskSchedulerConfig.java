60177991f47708e4ddb677574acc45408e943164
gmoon92 <gmoon0929@gmail.com>
2021-11-08 22:51:01 +0900

    feat(spring-scheduling): 스케쥴러 설정 비동기 활성화

--- spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfig.java
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/config/ThreadPoolTaskSchedulerConfig.java
@@ -1,15 +1,21 @@
 package com.gmoon.springscheduling.config;
 
 import com.gmoon.springscheduling.jobs.SimpleScheduledJobs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-@Configuration
+@EnableAsync
 @EnableScheduling
-public class ThreadPoolTaskSchedulerConfig {
+@Configuration
+@RequiredArgsConstructor
+public class ThreadPoolTaskSchedulerConfig implements AsyncConfigurer {
   private static final String NAME_OF_THREAD_SCHEDULER_TASK = "ThreadPoolTaskScheduler";
   private static final int SIZE_OF_POOL = 5;
 
@@ -19,6 +25,7 @@ public class ThreadPoolTaskSchedulerConfig {
     taskScheduler.setThreadNamePrefix(NAME_OF_THREAD_SCHEDULER_TASK);
     taskScheduler.setPoolSize(SIZE_OF_POOL);
     taskScheduler.setErrorHandler(customErrorSchedulerHandler);
+    taskScheduler.initialize();
     return taskScheduler;
   }
 
@@ -31,5 +38,10 @@ public class ThreadPoolTaskSchedulerConfig {
   @ConditionalOnProperty(value = "schedule.type", havingValue = "default")
   public SimpleScheduledJobs simpleScheduledJobs() {
     return new SimpleScheduledJobs();
+  }
+
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return new CustomAsyncUncaughtExceptionHandler();
   }
 }
