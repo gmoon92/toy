027bc48f0a2aee748919c87850f7691cd6bf5160
gmoon92 <gmoon0929@gmail.com>
2021-11-08 22:18:16 +0900

    feat(spring-scheduling): phone 알람 3초 이상 지연될 경우 예외 추가

--- spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/jobs/PhoneAlarmJobs.java
+++ spring-event/spring-scheduling/src/main/java/com/gmoon/springscheduling/jobs/PhoneAlarmJobs.java
@@ -1,5 +1,6 @@
 package com.gmoon.springscheduling.jobs;
 
+import com.gmoon.springscheduling.jobs.exception.PhoneAlarmSchedulerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -7,15 +8,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class PhoneAlarmJobs {
   private static final int MILLISECONDS_OF_PLUS_TIME = 500;
+  private static final int MAX_MILLISECONDS_OF_SLEEP_ON_PHONE = 3_000;
 
   private long delay = 0;
 
   public void wakeUp() {
+    if (delay > MAX_MILLISECONDS_OF_SLEEP_ON_PHONE) {
+      throw new PhoneAlarmSchedulerException(MAX_MILLISECONDS_OF_SLEEP_ON_PHONE);
+    }
+
     final long now = System.currentTimeMillis() / 1000;
     log.info("schedule phone alarm tasks with dynamic delay - {}", now);
   }
 
-  public long plusOneSecondsDelay() {
+  public long getPlusSecondsDelay() {
     this.delay += MILLISECONDS_OF_PLUS_TIME;
     log.info("delaying {} milliseconds...", this.delay);
     return this.delay;
