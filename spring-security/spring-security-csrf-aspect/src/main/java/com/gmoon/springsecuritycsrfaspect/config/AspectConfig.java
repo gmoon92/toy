f87f5e8c92f1f89679068c562601ba124a9ad09f
gmoon92 <gmoon0929@gmail.com>
2021-11-13 01:30:58 +0900

    feat(spring-security-csrf-aspect): Aspect config 추가

---
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/config/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.gmoon.springsecuritycsrfaspect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AspectConfig {
+}
