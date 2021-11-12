9418ccc0bfacdb3a71a5232dc9ce81cc9549ae19
gmoon92 <gmoon0929@gmail.com>
2021-11-13 00:39:40 +0900

    chore(spring-security-csrf-aspect): init project

---
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/SpringSecurityCsrfAspectApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springsecuritycsrfaspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCsrfAspectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityCsrfAspectApplication.class, args);
+  }
+
+}
---
+++ spring-security/spring-security-csrf-aspect/src/test/java/com/gmoon/springsecuritycsrfaspect/SpringSecurityCsrfAspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springsecuritycsrfaspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCsrfAspectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
