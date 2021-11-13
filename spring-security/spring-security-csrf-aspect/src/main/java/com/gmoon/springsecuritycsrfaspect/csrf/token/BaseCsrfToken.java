b0a6b6accaff2c844689f327f285ee3000bac0eb
gmoon92 <gmoon0929@gmail.com>
2021-11-14 00:49:03 +0900

    refactor(spring-security-csrf-aspect): CsrfToken CsrfToken Wrapper 추상화 클래스 추가
    - 공통 헤더, 파라미터 이름 캡슐화

---
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/csrf/token/BaseCsrfToken.java
@@ -0,0 +1,25 @@
+package com.gmoon.springsecuritycsrfaspect.csrf.token;
+
+import org.springframework.security.web.csrf.CsrfToken;
+
+public abstract class BaseCsrfToken implements CsrfToken {
+  private static final String HEADER_NAME = "X-CSRF-TOKEN";
+  private static final String PARAMETER_NAME = "_csrf";
+
+  @Override
+  public String getHeaderName() {
+    return HEADER_NAME;
+  }
+
+  @Override
+  public String getParameterName() {
+    return PARAMETER_NAME;
+  }
+
+  @Override
+  public String getToken() {
+    return getValue();
+  }
+
+  public abstract String getValue();
+}
