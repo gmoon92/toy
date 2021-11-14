c6b80f5061066b0e53194d195891eabd5717175c
gmoon92 <gmoon0929@gmail.com>
2021-11-14 17:37:06 +0900

    feat(spring-security-csrf-aspect): UserDetailsService 패스워드 암호화 복호화 추가
    - 패스워드 bcrypt 복호화 추가

---
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/user/AccountService.java
@@ -0,0 +1,44 @@
+package com.gmoon.springsecuritycsrfaspect.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+  private final SecurityProperties securityProperties;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    checkLoginUsername(username);
+
+    SecurityProperties.User user = securityProperties.getUser();
+    String password = user.getPassword();
+    String grantedAuthority = user.getRoles().stream()
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("user authority is not defined."));
+    log.info("username: {}, password: {}, grantedAuthority: {}", username, password, grantedAuthority);
+    return User.builder()
+            .username(username)
+            .password(passwordEncoder.encode(password))
+            .authorities(String.format("ROLE_%s", grantedAuthority))
+            .build();
+  }
+
+  private void checkLoginUsername(String username) {
+    SecurityProperties.User user = securityProperties.getUser();
+    if (!StringUtils.equals(user.getName(), username)) {
+      throw new UsernameNotFoundException(String.format("username %s is not found", username));
+    }
+  }
+}
