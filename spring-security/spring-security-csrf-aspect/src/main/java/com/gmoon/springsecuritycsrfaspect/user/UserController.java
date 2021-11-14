353f85d2274dbafede7294cfdcec2899d1855abb
gmoon92 <gmoon0929@gmail.com>
2021-11-14 18:55:06 +0900

    refactor(spring-security-csrf-aspect): 사용자 컨트롤러 사용하지 않는 메서드 제거

--- spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/user/UserController.java
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/user/UserController.java
@@ -2,13 +2,11 @@ package com.gmoon.springsecuritycsrfaspect.user;
 
 import com.gmoon.springsecuritycsrfaspect.csrf.annotation.CSRF;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,15 +25,8 @@ public class UserController {
   @CSRF
   @ResponseBody
   @GetMapping("/delete")
-  public ResponseEntity<String> delete() {
+  public String delete() {
     log.info("delete...");
-    return ResponseEntity.ok("delete...");
-  }
-
-  @ResponseBody
-  @PostMapping("/list")
-  public ResponseEntity<String> list() {
-    log.info("list...");
-    return ResponseEntity.ok("[{\"name\": \"gmoon92\"}]");
+    return "delete";
   }
 }
