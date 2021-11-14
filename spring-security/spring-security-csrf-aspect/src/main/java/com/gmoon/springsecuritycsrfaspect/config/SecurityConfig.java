3ac0a9dcc9fe6b789611018200a72c847b7b7343
gmoon92 <gmoon0929@gmail.com>
2021-11-14 17:31:01 +0900

    feat(spring-security-csrf-aspect): 시큐리티 AccessDeniedHandler 설정 추가

--- spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/config/SecurityConfig.java
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.gmoon.springsecuritycsrfaspect.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,13 +27,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
               .csrf().disable()
               .cors().disable()
             .authorizeRequests()
-              .mvcMatchers("/", "/login")
-              .permitAll()
+              .mvcMatchers("**/user/**").hasRole("ADMIN")
               .anyRequest()
-              .authenticated().and()
+              .permitAll().and()
+            .exceptionHandling()
+              .accessDeniedHandler(accessDeniedHandler()).and()
             .formLogin()
-              .loginPage("/")
-            .permitAll();
+              .loginPage("/login")
+              .defaultSuccessUrl("/user/info")
+              .permitAll();
+  }
+
+  private AccessDeniedHandler accessDeniedHandler() {
+    return (request, response, accessDeniedException) -> {
+      SecurityContext context = SecurityContextHolder.getContext();
+      Authentication authentication = context.getAuthentication();
+      UserDetails user = (UserDetails) authentication.getPrincipal();
+
+      String username = user.getUsername();
+      String method = request.getMethod();
+      String url = request.getRequestURI();
+      String errorMessage = String.format("%s denied to access (%s) %s.", username, method, url);
+      log.error(errorMessage, accessDeniedException);
+
+      response.sendRedirect("/error/403");
+    };
   }
 
   @Bean
