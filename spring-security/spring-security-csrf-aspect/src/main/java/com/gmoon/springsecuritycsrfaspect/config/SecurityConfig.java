41cc214787f5d2c34b6136dfd3229b320fabe820
gmoon92 <gmoon0929@gmail.com>
2021-11-14 17:31:33 +0900

    feat(spring-security-csrf-aspect): 시큐리티 static Resource ignore 설정 추가

--- spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/config/SecurityConfig.java
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.gmoon.springsecuritycsrfaspect.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -52,6 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
       response.sendRedirect("/error/403");
     };
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
   }
 
   @Bean
