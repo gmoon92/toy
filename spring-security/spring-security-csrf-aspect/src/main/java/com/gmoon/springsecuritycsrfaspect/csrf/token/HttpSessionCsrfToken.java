1aee92323c5955ec031f6c86764ad4a2e5f80903
gmoon92 <gmoon0929@gmail.com>
2021-11-15 22:41:07 +0900

    feat: CSRF equals, hash code 재정의 및 toString 추가
    spring-security-csrf-aspect

--- spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/csrf/token/HttpSessionCsrfToken.java
+++ spring-security/spring-security-csrf-aspect/src/main/java/com/gmoon/springsecuritycsrfaspect/csrf/token/HttpSessionCsrfToken.java
@@ -1,12 +1,16 @@
 package com.gmoon.springsecuritycsrfaspect.csrf.token;
 
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
+@ToString
+@EqualsAndHashCode(of = "value")
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class HttpSessionCsrfToken extends BaseCsrfToken {
   private final String value;
