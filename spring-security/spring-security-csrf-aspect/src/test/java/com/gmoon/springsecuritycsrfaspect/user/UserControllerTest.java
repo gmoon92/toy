8a9f75c78c342ae58d681d6d84c57bfd2b0af777
gmoon92 <gmoon0929@gmail.com>
2021-11-15 23:57:54 +0900

    test: 사용자 컨트롤러 CSRF 체크 테스트 케이스 추가
    spring-security-csrf-aspect

--- spring-security/spring-security-csrf-aspect/src/test/java/com/gmoon/springsecuritycsrfaspect/user/UserControllerTest.java
+++ spring-security/spring-security-csrf-aspect/src/test/java/com/gmoon/springsecuritycsrfaspect/user/UserControllerTest.java
@@ -1,8 +1,110 @@
 package com.gmoon.springsecuritycsrfaspect.user;
 
+import com.gmoon.springsecuritycsrfaspect.csrf.CsrfTokenRepository;
+import com.gmoon.springsecuritycsrfaspect.csrf.token.BaseCsrfToken;
+import com.gmoon.springsecuritycsrfaspect.csrf.token.HttpSessionCsrfToken;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  WebApplicationContext context;
+
+  @Autowired
+  HttpServletRequest request;
+
+  @Autowired
+  CsrfTokenRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(springSecurity())
+            .alwaysDo(print())
+            .build();
+  }
+
+  @Test
+  @DisplayName("사용자 인증 후 Http Session 에 CSRF 토큰이 생성된다.")
+  void login() throws Exception {
+    // given
+    String attributeName = repository.getSessionAttributeName();
+
+    // when
+    ResultActions result = mockMvc.perform(post("/login")
+            .accept(MediaType.APPLICATION_FORM_URLENCODED)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("username", "admin")
+            .param("password", "123"));
+
+    // then
+    result.andExpect(request().sessionAttribute(attributeName, is(notNullValue(HttpSessionCsrfToken.class))))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "123", roles = "ADMIN")
+  @DisplayName("CSRF 토큰을 체크한다.")
+  void delete() throws Exception {
+    // given
+    repository.saveTokenOnHttpSession(request);
+    BaseCsrfToken token = repository.getToken(request);
+
+    // when
+    String attributeName = repository.getSessionAttributeName();
+    String headerName = token.getHeaderName();
+    String value = token.getValue();
+    ResultActions result = mockMvc.perform(get("/user/delete")
+            .sessionAttr(attributeName, token)
+            .header(headerName, value));
+
+    // then
+    result.andExpect(request().sessionAttribute(attributeName, is(notNullValue(HttpSessionCsrfToken.class))))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(username = "admin", password = "123", roles = "ADMIN")
+  @DisplayName("CSRF 토큰이 없을 경우 예외가 발생한다.")
+  void delete_non_csrf_token_error() throws Exception {
+    // given
+    repository.saveTokenOnHttpSession(request);
+    BaseCsrfToken token = repository.getToken(request);
+
+    // when
+    String attributeName = repository.getSessionAttributeName();
+    ResultActions result = mockMvc.perform(get("/user/delete")
+            .sessionAttr(attributeName, token));
+
+    // then
+    result.andExpect(request().sessionAttribute(attributeName, is(notNullValue(HttpSessionCsrfToken.class))))
+            .andExpect(status().is3xxRedirection());
+  }
 }
