5e08a5a8b2182d47648ceafee79c880018f101a9
gmoon92 <gmoon0929@gmail.com>
2021-11-19 00:15:45 +0900

    chore: spring utils project init
    spring-utils

---
+++ spring-utils/src/main/java/com/gmoon/springutils/SpringUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringUtilsApplication.class, args);
+  }
+
+}
---
+++ spring-utils/src/test/java/com/gmoon/springutils/SpringUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtilsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
