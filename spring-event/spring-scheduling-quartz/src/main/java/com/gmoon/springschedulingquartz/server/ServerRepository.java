7e7168d249a3f058d6e50c69256447a5c9a5d177
gmoon92 <gmoon0929@gmail.com>
2021-11-29 21:54:07 +0900

    feat: POJO에서 생성된 웹 서버 정보 저장 기능 추가
    spring-scheduling-quartz

--- spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepository.java
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepository.java
@@ -3,4 +3,5 @@ package com.gmoon.springschedulingquartz.server;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ServerRepository extends CrudRepository<Server, Long> {
+  Server findServerByName(String name);
 }
