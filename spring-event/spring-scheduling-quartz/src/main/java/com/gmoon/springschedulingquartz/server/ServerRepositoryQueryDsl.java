20ff3ec44396dad859da6289e70ae7c7fd0bc8e8
gmoon92 <gmoon0929@gmail.com>
2021-12-01 03:44:27 +0900

    feat: 서버 도메인 @Immutable 선언하여 불변 보장
    - Transaction 이더라도 dirty checking 하지 않도록 보장
    - https://www.baeldung.com/hibernate-immutable
    
    spring-scheduling-quartz

--- spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepositoryQueryDsl.java
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepositoryQueryDsl.java
@@ -3,5 +3,6 @@ package com.gmoon.springschedulingquartz.server;
 import java.util.List;
 
 public interface ServerRepositoryQueryDsl {
+  List<Server> getEnabledServers();
   List<Server> getServers(String serverName);
 }
