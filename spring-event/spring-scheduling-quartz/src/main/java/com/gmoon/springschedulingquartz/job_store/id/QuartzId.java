97d82cd5eabab731b8a5abb6b85d72ff94f55d00
gmoon92 <gmoon0929@gmail.com>
2021-12-04 00:17:27 +0900

    refactor: Quartz Id toString, equals, hashCode 재정의
    spring-scheduling-quartz

--- spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/job_store/id/QuartzId.java
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/job_store/id/QuartzId.java
@@ -1,17 +1,20 @@
 package com.gmoon.springschedulingquartz.job_store.id;
 
 import com.gmoon.springschedulingquartz.job_store.constants.QuartzColumnLength;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
+@ToString
 @MappedSuperclass
+@EqualsAndHashCode
 public abstract class QuartzId implements Serializable {
 
   @Column(name = "SCHED_NAME", length = QuartzColumnLength.SCHEDULER_NAME)
-  private String schedulerName;
-
+  protected String schedulerName;
 }
