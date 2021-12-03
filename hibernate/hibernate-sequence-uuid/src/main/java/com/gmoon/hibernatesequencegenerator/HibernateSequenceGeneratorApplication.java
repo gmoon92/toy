0ea9aa756f5f0b150ea55eecfa18e26f40327c1c
gmoon92 <gmoon0929@gmail.com>
2021-12-04 04:08:05 +0900

    chore: init hibernate-sequence-uuid project
    hibernate-sequence-uuid

---
+++ hibernate/hibernate-sequence-uuid/src/main/java/com/gmoon/hibernatesequencegenerator/HibernateSequenceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernatesequencegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSequenceGeneratorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HibernateSequenceGeneratorApplication.class, args);
+  }
+
+}
---
+++ hibernate/hibernate-sequence-uuid/src/test/java/com/gmoon/hibernatesequencegenerator/HibernateSequenceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernatesequencegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSequenceGeneratorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
