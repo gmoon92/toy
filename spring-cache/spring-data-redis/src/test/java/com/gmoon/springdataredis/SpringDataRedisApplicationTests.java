6755e6db17230a6ee8bb390df65b8367e3ec0313
gmoon92 <gmoon0929@gmail.com>
2021-12-06 09:11:33 +0900

    chore: init Spring data redis project
    spring-data-redis

---
+++ spring-cache/spring-data-redis/src/test/java/com/gmoon/springdataredis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springdataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
