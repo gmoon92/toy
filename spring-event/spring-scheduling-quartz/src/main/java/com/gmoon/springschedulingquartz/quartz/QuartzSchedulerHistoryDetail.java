175a67a184775663dbba72d69557d3f1c47e490f
gmoon92 <gmoon0929@gmail.com>
2021-12-08 02:23:15 +0900

    feat: 로컬 IP 주소를 관리할 QuartzSchedulerHistory 도메인 추가
    spring-scheduling-quartz

---
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/quartz/QuartzSchedulerHistoryDetail.java
@@ -0,0 +1,34 @@
+package com.gmoon.springschedulingquartz.quartz;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuartzSchedulerHistoryDetail {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "scheduler_id", referencedColumnName = "id")
+  private QuartzSchedulerHistory schedulerHistory;
+
+  private String ip;
+
+  public static QuartzSchedulerHistoryDetail create(QuartzSchedulerHistory schedulerHistory, String ip) {
+    QuartzSchedulerHistoryDetail detail = new QuartzSchedulerHistoryDetail();
+    detail.schedulerHistory = schedulerHistory;
+    detail.ip = ip;
+    return detail;
+  }
+}
