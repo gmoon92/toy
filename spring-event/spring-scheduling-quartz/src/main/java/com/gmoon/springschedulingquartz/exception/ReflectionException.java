b84d12dac3a5eb17a2a815f2a2351d6700dacef8
gmoon92 <gmoon0929@gmail.com>
2021-12-09 10:51:05 +0900

    feat: CPU 사용량 측정 CpuStatusCheckExecutor 빈 추가
    spring-scheduling-quartz

---
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/exception/ReflectionException.java
@@ -0,0 +1,7 @@
+package com.gmoon.springschedulingquartz.exception;
+
+public class ReflectionException extends RuntimeException {
+  public ReflectionException(Throwable cause) {
+    super(cause);
+  }
+}
