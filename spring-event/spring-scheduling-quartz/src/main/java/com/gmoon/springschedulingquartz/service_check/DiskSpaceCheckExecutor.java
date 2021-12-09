7fe80dd6224105a1f22e4a5a39e470a237408a7c
gmoon92 <gmoon0929@gmail.com>
2021-12-09 10:51:37 +0900

    feat: Disk 저장 공간 사용량 측정 DiskSpaceCheckExecutor 빈 추가
    spring-scheduling-quartz

---
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/service_check/DiskSpaceCheckExecutor.java
@@ -0,0 +1,42 @@
+package com.gmoon.springschedulingquartz.service_check;
+
+import com.gmoon.springschedulingquartz.exception.DiskUsageExceedsException;
+import com.gmoon.springschedulingquartz.properties.QuartzProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DiskSpaceCheckExecutor implements ServiceStatusChecker {
+  private final QuartzProperties properties;
+
+  @Override
+  public void check() {
+    if (!properties.isDiskUsageEnabled()) {
+      return;
+    }
+
+    File dir = new File("/");
+    long total = dir.getTotalSpace();
+    long available = dir.getUsableSpace();
+    long used = total - available;
+
+    int thresholdPercentage = properties.getDiskThresholdPercentage();
+    float usage = getPercent(total, used);
+    log.info("thresholdPercentage: {}, usage: {}, path: {}, total: {}, available: {}", thresholdPercentage, usage, dir.getAbsolutePath(), total, available);
+    if (usage > thresholdPercentage) {
+      throw new DiskUsageExceedsException();
+    }
+  }
+
+  private float getPercent(long total, long part) {
+    if (total == 0) {
+      return 0;
+    }
+    return part * 100.0f / total;
+  }
+}
