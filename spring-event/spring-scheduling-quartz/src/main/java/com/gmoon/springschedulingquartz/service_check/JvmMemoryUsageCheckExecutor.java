64b39eb2768a30985d9c7753a45568050a1ef2f6
gmoon92 <gmoon0929@gmail.com>
2021-12-09 10:51:59 +0900

    feat: JVM 메모리 사용량 측정 JvmMemoryUsageCheckExecutor 빈 추가
    spring-scheduling-quartz

---
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/service_check/JvmMemoryUsageCheckExecutor.java
@@ -0,0 +1,39 @@
+package com.gmoon.springschedulingquartz.service_check;
+
+import com.gmoon.springschedulingquartz.exception.JvmMemoryUsageExceedsException;
+import com.gmoon.springschedulingquartz.properties.QuartzProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JvmMemoryUsageCheckExecutor implements ServiceStatusChecker {
+  private final QuartzProperties properties;
+
+  @Override
+  public void check() {
+    if (!properties.isJvmMemoryUsageEnabled()) {
+      return;
+    }
+
+    long memory = Runtime.getRuntime().totalMemory();
+    long available = Runtime.getRuntime().freeMemory();
+    long used = memory - available;
+
+    int thresholdPercentage = properties.getJvmMemoryThresholdPercentage();
+    float usage = getPercent(memory, used);
+    log.info("thresholdPercentage: {}, usage: {}, memory: {}, available: {}", thresholdPercentage, usage, memory, available);
+    if (usage > thresholdPercentage) {
+      throw new JvmMemoryUsageExceedsException();
+    }
+  }
+
+  private float getPercent(long total, long part) {
+    if (total == 0) {
+      return 0;
+    }
+    return part * 100.0f / total;
+  }
+}
