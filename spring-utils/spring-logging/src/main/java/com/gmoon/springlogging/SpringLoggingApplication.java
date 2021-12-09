a7999853a4e1244e8cbe0aac6c56761ea8b00322
gmoon92 <gmoon0929@gmail.com>
2021-12-10 03:11:06 +0900

    feat: logback 설정 추가
    spring-logging

--- spring-utils/spring-logging/src/main/java/com/gmoon/springlogging/SpringLoggingApplication.java
+++ spring-utils/spring-logging/src/main/java/com/gmoon/springlogging/SpringLoggingApplication.java
@@ -1,6 +1,8 @@
 package com.gmoon.springlogging;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +10,17 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @SpringBootApplication
-public class SpringLoggingApplication {
+public class SpringLoggingApplication implements ApplicationRunner {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
     SpringApplication.run(SpringLoggingApplication.class, args);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
     while (true) {
-      log.info("log.. {}", LocalDateTime.now());
+      log.info("message: {}", LocalDateTime.now());
       Thread.sleep(1_000);
     }
   }
-
 }
