9e4d53810d70f436d6c780801f064af9ce88c397
gmoon92 <gmoon0929@gmail.com>
2021-12-11 07:52:07 +0900

    feat(DefaultDataGenerateRunner): 애플리케이션에 필요한 기본 데이터를 셋팅
    spring-scheduling-quartz

---
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/config/DefaultDataGenerateRunner.java
@@ -0,0 +1,59 @@
+package com.gmoon.springschedulingquartz.config;
+
+import com.gmoon.springschedulingquartz.model.WebServerSaveForm;
+import com.gmoon.springschedulingquartz.quartz.QuartzSchedulerHistory;
+import com.gmoon.springschedulingquartz.quartz.QuartzSchedulerHistoryRepository;
+import com.gmoon.springschedulingquartz.server.ServerService;
+import com.gmoon.springschedulingquartz.util.LocalIpAddressUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DefaultDataGenerateRunner implements ApplicationRunner {
+  private final ServerService serverService;
+  private final QuartzSchedulerHistoryRepository schedulerHistoryRepository;
+  private final Scheduler scheduler;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    saveSchedulerHistory();
+    saveEnabledWebServers();
+  }
+
+  private void saveSchedulerHistory() throws SchedulerException {
+    String instanceId = scheduler.getSchedulerInstanceId();
+    List<String> ipAddresses = LocalIpAddressUtil.getIpAddresses();
+    QuartzSchedulerHistory history = QuartzSchedulerHistory.from(instanceId, ipAddresses);
+    schedulerHistoryRepository.save(history);
+  }
+
+  private void saveEnabledWebServers() {
+    log.info("init Web Server Data...");
+    WebServerSaveForm form = getWebServerSaveForm("gmoon");
+    serverService.saveWebServer(form);
+  }
+
+  private WebServerSaveForm getWebServerSaveForm(String serverName) {
+    return Optional.ofNullable(serverService.getServer(serverName))
+            .map(WebServerSaveForm::from)
+            .orElseGet(() -> {
+              WebServerSaveForm form = new WebServerSaveForm();
+              form.setName(serverName);
+              form.setPublicHost("gmoon92.github.io");
+              form.setPrivateHost("127.0.0.1");
+              form.setPort1(443);
+              form.setPort2(80);
+              return form;
+            });
+  }
+}
