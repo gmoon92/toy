6585a9f9ba0f6873371789fecf921a14bf49db56
gmoon92 <gmoon0929@gmail.com>
2021-12-11 07:27:45 +0900

    refactor: 서버 레파지토리 JpaRepository 구현
    spring-scheduling-quartz

--- spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepository.java
+++ spring-event/spring-scheduling-quartz/src/main/java/com/gmoon/springschedulingquartz/server/ServerRepository.java
@@ -1,8 +1,8 @@
 package com.gmoon.springschedulingquartz.server;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServerRepository extends CrudRepository<Server, Long>,
+public interface ServerRepository extends JpaRepository<Server, Long>,
         ServerRepositoryQueryDsl {
   Server findServerByName(String name);
 }
