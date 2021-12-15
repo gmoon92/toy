758b31f5f2674f05a8235d80ffe60e6abe9ec2df
gmoon92 <gmoon0929@gmail.com>
2021-12-16 00:32:19 +0900

    feat: Bookmark @RestController add
    - get
    - save
    - delete
    
    spring-security-cors

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/bookmark/BookmarkController.java
@@ -0,0 +1,39 @@
+package com.gmoon.resourceserver.bookmark;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+@RequestMapping("/bookmark")
+@RequiredArgsConstructor
+public class BookmarkController {
+  private final BookmarkRepository repository;
+
+  @GetMapping("/{name}")
+  public ResponseEntity<Bookmark> get(@PathVariable String name) {
+    return ResponseEntity.ok(repository.findBookmarkByName(name));
+  }
+
+  @PostMapping("/{name}")
+  public ResponseEntity<Bookmark> save(@PathVariable String name) {
+    Bookmark bookmark = repository.save(Bookmark.create(name));
+    return ResponseEntity.status(HttpStatus.CREATED)
+            .body(bookmark);
+  }
+
+  @DeleteMapping("/{name}")
+  public ResponseEntity<Void> remove(@PathVariable String name) {
+    repository.delete(Bookmark.create(name));
+    return ResponseEntity.status(HttpStatus.NO_CONTENT)
+            .build();
+  }
+}
