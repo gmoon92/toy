4d212aef6134a13f4c792998c827c57b5543e321
gmoon92 <gmoon0929@gmail.com>
2021-12-20 03:17:45 +0900

    feat: JWT Authentication schema enum add
    spring-security-jwt

---
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/jwt/AuthenticationSchema.java
@@ -0,0 +1,40 @@
+package com.gmoon.springsecurityjwt.jwt;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthenticationSchema {
+  BASIC("Basic"),
+  BEARER("Bearer"),
+  DIGEST("Digest"),
+  HOBA("HOBA"),
+  MUTUAL("Mutual"),
+  NEGOTIATE("Negotiate"),
+  OAUTH("OAuth"),
+  SHA1("SCRAM-SHA-1"),
+  SHA256("SCRAM-SHA-256"),
+  VAPID("vapid");
+
+  public static final Map<String, AuthenticationSchema> MAP = Collections.unmodifiableMap(Stream.of(values())
+          .collect(Collectors.toMap(AuthenticationSchema::getName, Function.identity())));
+
+  private final String name;
+
+  public static void checkValidSchema(String schema) {
+    if (!contain(schema)) {
+      throw new IllegalArgumentException(String.format("Invalid authentication schema name: %s", schema));
+    }
+  }
+
+  private static boolean contain(String schema) {
+    return MAP.containsKey(schema);
+  }
+}
---
+++ spring-security/spring-security-jwt/src/test/java/com/gmoon/springsecurityjwt/jwt/AuthenticationSchemaTest.java
@@ -0,0 +1,29 @@
+package com.gmoon.springsecurityjwt.jwt;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class AuthenticationSchemaTest {
+  @ParameterizedTest
+  @MethodSource("getSchemaNames")
+  @DisplayName("인증 스키마는 대소문자를 구별한다.")
+  void testCheckValidSchema(String invalidSchema) {
+    // then
+    assertThatThrownBy(() -> AuthenticationSchema.checkValidSchema(invalidSchema))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Invalid authentication schema name: %s", invalidSchema);
+  }
+
+  static Stream<Arguments> getSchemaNames() {
+    return Stream.of(
+            Arguments.of(AuthenticationSchema.BASIC.getName().toUpperCase()),
+            Arguments.of(AuthenticationSchema.BEARER.getName().toLowerCase())
+    );
+  }
+}
