af5a231d9248a66b4457b2bb18e1fe17c2e6f8b7
gmoon92 <gmoon0929@gmail.com>
2021-12-26 04:52:06 +0900

    refactor: logging ApplicationRunner 제거
    - SpringLoggingApplicationTest 무한 루프 영향
    
    spring-logging

--- spring-utils/spring-logging/src/main/java/com/gmoon/springlogging/SpringLoggingApplication.java
+++ spring-utils/spring-logging/src/main/java/com/gmoon/springlogging/SpringLoggingApplication.java
@@ -2,8 +2,6 @@ package com.gmoon.springlogging;
 
 import java.time.LocalDateTime;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
-public class SpringLoggingApplication implements ApplicationRunner {
+public class SpringLoggingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(SpringLoggingApplication.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
 		while (true) {
 			log.info("message: {}", LocalDateTime.now());
 			Thread.sleep(1_000);
