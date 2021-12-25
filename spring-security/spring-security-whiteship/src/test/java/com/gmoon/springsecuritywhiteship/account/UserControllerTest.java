efa1a81bb040abcf2bc14bcf3d6ffd955e5cc123
gmoon92 <gmoon0929@gmail.com>
2021-12-26 03:15:27 +0900

    test: 테스트 코드 멱등성 보장
    - admin ApplicationRunner 에서 저장하기 때문에 중복 에러 발생
    
    spring-security-whiteship

--- spring-security/spring-security-whiteship/src/test/java/com/gmoon/springsecuritywhiteship/account/UserControllerTest.java
+++ spring-security/spring-security-whiteship/src/test/java/com/gmoon/springsecuritywhiteship/account/UserControllerTest.java
@@ -2,9 +2,11 @@ package com.gmoon.springsecuritywhiteship.account;
 
 import static org.hamcrest.core.StringContains.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,32 +18,27 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
-
-	@Autowired
-	MockMvc mockMvc;
-
-	@Autowired
-	AccountRepository accountRepository;
-
-	@Autowired
-	AuthenticationManager authenticationManager;
-
-	@Autowired
-	AccountService accountService;
+	@Autowired MockMvc mockMvc;
+	@Autowired AccountRepository accountRepository;
+	@Autowired AuthenticationManager authenticationManager;
+	@Autowired AccountService accountService;
 
 	@Test
 	@DisplayName("Security Chain이랑 같은 환경에서 도는건지")
 	@WithMockUser
 	public void list() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/user/list")
-				.with(csrf()))
-			.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+		// when
+		ResultActions result = mockMvc.perform(get("/user/list")
+			.with(csrf()));
+
+		// then
+		result.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
 
 	@Test
@@ -49,17 +46,19 @@ class UserControllerTest {
 	void currentUser_annotation_injection_principal_should_set_null_when_anonymous() throws Exception {
 		// given
 		SecurityContext context = SecurityContextHolder.getContext();
-		String principal = "admin";
-		String credentials = "123123";
+		String principal = RandomStringUtils.randomAlphanumeric(4);
+		String credentials = "123";
+
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(principal, credentials);
 		token.setDetails(accountService.createNew(Account.newAdmin(principal, credentials)));
 		context.setAuthentication(authenticationManager.authenticate(token));
 
 		// when
-		mockMvc.perform(MockMvcRequestBuilders.get("/sample/annotation"))
-			.andDo(print())
-			.andExpect(content().string(containsString(principal)))
-			.andExpect(status().isOk())
-			.andReturn();
+		ResultActions result = mockMvc.perform(get("/sample/annotation"))
+			.andDo(print());
+
+		// then
+		result.andExpect(content().string(containsString(principal)))
+			.andExpect(status().isOk());
 	}
 }
