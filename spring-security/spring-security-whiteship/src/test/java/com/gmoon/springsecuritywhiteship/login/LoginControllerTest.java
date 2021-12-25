dc6a38c662c4db6b07996e994f74c56d04cfc4a4
gmoon92 <gmoon0929@gmail.com>
2021-12-26 04:24:42 +0900

    test: LoginControllerTest @WebMvcTest slice test
    spring-security-whiteship

--- spring-security/spring-security-whiteship/src/test/java/com/gmoon/springsecuritywhiteship/login/LoginControllerTest.java
+++ spring-security/spring-security-whiteship/src/test/java/com/gmoon/springsecuritywhiteship/login/LoginControllerTest.java
@@ -1,135 +1,184 @@
 package com.gmoon.springsecuritywhiteship.login;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.gmoon.springsecuritywhiteship.account.Account;
+import com.gmoon.springsecuritywhiteship.account.AccountRepository;
 import com.gmoon.springsecuritywhiteship.account.AccountService;
+import com.gmoon.springsecuritywhiteship.account.UserController;
+import com.gmoon.springsecuritywhiteship.board.BoardRepository;
+import com.gmoon.springsecuritywhiteship.sample.SampleService;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(value = {UserController.class})
 class LoginControllerTest {
-
-	@Autowired
+	@Autowired WebApplicationContext context;
 	MockMvc mockMvc;
 
-	@Autowired
-	AccountService accountService;
+	@MockBean AccountService accountService;
+	@MockBean AccountRepository accountRepository;
+	@MockBean SampleService sampleService;
+	@MockBean BoardRepository boardRepository;
+
+	String username;
+	String password;
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.alwaysDo(print())
+			.apply(springSecurity())
+			.build();
+
+		username = RandomStringUtils.randomAlphanumeric(4);
+		password = "123";
+
+		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		Account account = Account.newUser(username, passwordEncoder.encode(password));
+		when(accountService.createNew(any())).thenReturn(account);
+		when(accountService.loadUserByUsername(any())).thenReturn(account);
+	}
 
 	@Test
 	void index_anonymous() throws Exception {
-		mockMvc.perform(get("/"))
-			.andDo(print()) // 출력 응답
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/"));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	void index_user() throws Exception {
-		mockMvc.perform(get("/").with(user("gmoon").roles("USER"))) // mock already login user
-			.andDo(print())
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/")
+			.with(user("gmoon") // mock already login user
+				.roles("USER")));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	void index_admin() throws Exception {
-		mockMvc.perform(get("/").with(user("admin").roles("ADMIN"))) // mock already login user
-			.andDo(print())
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/")
+			.with(user("admin") /* mock already login user*/
+				.roles("ADMIN")));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	void admin_user() throws Exception {
-		mockMvc.perform(get("/admin").with(user("gmoon").roles("USER")))
-			.andDo(print()) // 출력 응답
-			.andExpect(status().isForbidden());
+		// when
+		ResultActions result = mockMvc.perform(get("/admin")
+			.with(user("gmoon")
+				.roles("USER")));
+
+		// then
+		// result.andExpect(status().isForbidden());
+		result.andExpect(status().is3xxRedirection());
 	}
 
 	@Test
 	void admin_admin() throws Exception {
-		mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
-			.andDo(print()) // 출력 응답
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/admin")
+			.with(user("admin")
+				.roles("ADMIN")));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	@WithAnonymousUser
 	void index_anonymous_with_test_annotation() throws Exception {
-		mockMvc.perform(get("/"))
-			.andDo(print())
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/"));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	@WithMockUser(username = "gmoon", roles = {"USER"})
 	void index_user_with_test_annotation() throws Exception {
-		mockMvc.perform(get("/"))
-			.andDo(print())
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/"));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	@WithMockUser(username = "admin", roles = {"ADMIN"})
 	void admin_admin_with_test_annotation() throws Exception {
-		mockMvc.perform(get("/admin"))
-			.andDo(print())
-			.andExpect(status().isOk());
+		// when
+		ResultActions result = mockMvc.perform(get("/admin"));
+
+		// then
+		result.andExpect(status().isOk());
 	}
 
 	@Test
 	@WithMockUser(username = "user", roles = {"USER"})
 	void admin_user_with_test_annotation() throws Exception {
-		mockMvc.perform(get("/admin"))
-			.andDo(print())
-			.andExpect(status().isForbidden());
+		// when
+		ResultActions result = mockMvc.perform(get("/admin"));
+
+		// then
+		// result.andExpect(status().isForbidden());
+		result.andExpect(status().is3xxRedirection());
 	}
 
 	@Test
 	@DisplayName("Security formLogin test")
-	@Transactional
 	void form_login() throws Exception {
-		String username = "gmoon";
-		String password = "123";
+		// when
+		ResultActions result = mockMvc.perform(formLogin()
+			.userParameter("id")
+			.user(username)
+			.password(password));
 
-		Account member = createUser(username, password);
-		mockMvc.perform(formLogin()
-				.user(member.getUsername())
-				.password(password))
-			.andExpect(authenticated());
+		// then
+		result.andExpect(authenticated());
 	}
 
 	@Test
 	@DisplayName("Security formLogin test")
-	@Transactional
-	void form_login2() throws Exception {
-		String username = "gmoon";
-		String password = "123";
+	void form_login_error() throws Exception {
+		// when
+		ResultActions result = mockMvc.perform(formLogin()
+			.userParameter("id")
+			.user(username)
+			.password(password + "test"));
 
-		Account member = createUser(username, password);
-		mockMvc.perform(formLogin()
-				.user(member.getUsername())
-				.password("1234"))
-			.andExpect(unauthenticated());
+		// then
+		result.andExpect(unauthenticated());
 	}
-
-	private Account createUser(String username, String password) {
-		return accountService.createNew(Account.newUser(username, password));
-	}
-
 }
