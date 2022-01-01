d41a6c7997adb9c159bc6317f896a3ff1da51e80
gmoon92 <gmoon0929@gmail.com>
2022-01-02 00:25:29 +0900

    chore: 사용자 샘플 데이터 data.sql 추가
    - spring.jpa.defer-datasource-initialization: true
    
    spring-security-jwt

---
+++ spring-security/spring-security-jwt/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user (id, role, username, password) VALUES (0, 'ADMIN', 'admin', '{bcrypt}$2a$10$yt2xpbUPeuX6zMuaScPLr.J5n9rBnhtybFlrK/RaQqI9Qh.JwAhyu');
+INSERT INTO tb_user (id, role, username, password) VALUES (1, 'MANAGER', 'manager', '{bcrypt}$2a$10$yt2xpbUPeuX6zMuaScPLr.J5n9rBnhtybFlrK/RaQqI9Qh.JwAhyu');
+INSERT INTO tb_user (id, role, username, password) VALUES (2, 'USER', 'user1', '{bcrypt}$2a$10$yt2xpbUPeuX6zMuaScPLr.J5n9rBnhtybFlrK/RaQqI9Qh.JwAhyu');
+INSERT INTO tb_user (id, role, username, password) VALUES (3, 'USER', 'user2', '{bcrypt}$2a$10$yt2xpbUPeuX6zMuaScPLr.J5n9rBnhtybFlrK/RaQqI9Qh.JwAhyu');
