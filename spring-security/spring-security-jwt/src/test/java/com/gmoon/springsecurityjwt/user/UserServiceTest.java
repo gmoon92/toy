6e35ff28772b3b3d03fdc142d6a284cc1fdf5e0b
gmoon92 <gmoon0929@gmail.com>
2022-01-02 01:03:03 +0900

    feat(UserService): UserDetailsService 구현체 추가
    - UserDetails 반환 메서드 재정의
    
    spring-security-jwt

---
+++ spring-security/spring-security-jwt/src/test/java/com/gmoon/springsecurityjwt/user/UserServiceTest.java
@@ -0,0 +1,53 @@
+package com.gmoon.springsecurityjwt.user;
+
+import static org.assertj.core.api.BDDAssertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(UserService.class)
+class UserServiceTest {
+	@Autowired UserService userService;
+
+	@MockBean UserRepository userRepository;
+
+	@Test
+	@DisplayName("UserDetails 를 조회한다.")
+	void testLoadUserByUsername() {
+		// given
+		String username = "admin";
+
+		// when
+		when(userRepository.findByUsername(username))
+			.thenReturn(Optional.of(User.create(username, "", Role.ADMIN)));
+
+		// then
+		then(userService.loadUserByUsername(username))
+			.isInstanceOf(UserDetails.class)
+			.isInstanceOf(User.class)
+			.isNotNull();
+	}
+
+	@Test
+	@DisplayName("UserDetails 조회시 값이 없으면 에러가 발생한다.")
+	void testLoadUserByUsername_error() {
+		// given
+		String username = "anonymous";
+
+		// when
+		when(userRepository.findByUsername(username))
+			.thenReturn(Optional.empty());
+
+		// then
+		assertThatExceptionOfType(UsernameNotFoundException.class)
+			.isThrownBy(() -> userService.loadUserByUsername(username));
+	}
+}
