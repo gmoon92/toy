4468ee6ea84196482458ed7c00d1bc60a4afb4b8
gmoon92 <gmoon0929@gmail.com>
2022-01-02 01:03:03 +0900

    feat(UserService): UserDetailsService 구현체 추가
    - UserDetails 반환 메서드 재정의
    
    spring-security-jwt

---
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/user/UserService.java
@@ -0,0 +1,20 @@
+package com.gmoon.springsecurityjwt.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)
+			.orElseThrow(() -> new UsernameNotFoundException("Not found username"));
+	}
+}
