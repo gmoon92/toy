c1dd75a2518a351c559e33a3accb6c530086c33a
gmoon92 <gmoon0929@gmail.com>
2022-01-17 08:46:19 +0900

    feat(Team): @JsonIgnoreProperties, @QueryProjection 추가
    - 1-N 연관관계 json ignore 처리
    - QueryProject 추가
    
    spring-security-jwt

--- spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/team/Team.java
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/team/Team.java
@@ -12,7 +12,9 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.gmoon.springsecurityjwt.user.User;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -23,10 +25,13 @@ import lombok.ToString;
 @Table(name = "tb_team", uniqueConstraints = {@UniqueConstraint(name = "u_name", columnNames = {"name"})})
 @Entity
 @Getter
+@JsonIgnoreProperties({"users"})
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(of = "name")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Team implements Serializable {
+	private static final long serialVersionUID = -4214860983283718625L;
+
 	@Id
 	@GeneratedValue
 	private Long id;
@@ -42,5 +47,11 @@ public class Team implements Serializable {
 		Team team = new Team();
 		team.name = name;
 		return team;
+	}
+
+	@QueryProjection
+	public Team(Long id, String name) {
+		this.id = id;
+		this.name = name;
 	}
 }
