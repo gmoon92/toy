d9864f37785a52d140371386090b8c0f9e37fe92
gmoon92 <gmoon0929@gmail.com>
2022-01-25 01:07:21 +0900

    refactor(Team): TeamUser 연관관계 설정 수정
    - CaseCade.ALL 설정 추가
    - 고아 객체 관리 설정 추가
    
    spring-security-jwt

--- spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/team/Team.java
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/team/Team.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +14,6 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.gmoon.springsecurityjwt.user.User;
 import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.AccessLevel;
@@ -30,7 +30,7 @@ import lombok.ToString;
 @EqualsAndHashCode(of = "name")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Team implements Serializable {
-	private static final long serialVersionUID = -4214860983283718625L;
+	private static final long serialVersionUID = 722253006387150023L;
 
 	@Id
 	@GeneratedValue
@@ -40,8 +40,8 @@ public class Team implements Serializable {
 	@Column(nullable = false)
 	private String name;
 
-	@OneToMany(mappedBy = "team")
-	private List<User> users = new ArrayList<>();
+	@OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<TeamUser> users = new ArrayList<>();
 
 	public static Team create(String name) {
 		Team team = new Team();
@@ -53,5 +53,10 @@ public class Team implements Serializable {
 	public Team(Long id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+
+	public Team clearUsers() {
+		users.clear();
+		return this;
 	}
 }
