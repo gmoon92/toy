053296cbfdb7f1a9b941954de907db76f66611ec
gmoon92 <gmoon0929@gmail.com>
2022-01-26 22:34:49 +0900

    refactor(InvalidAuthTokenException): JWTVerificationException 구현체 수정
    spring-security-jwt

--- spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/jwt/exception/InvalidAuthTokenException.java
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/jwt/exception/InvalidAuthTokenException.java
@@ -1,6 +1,8 @@
 package com.gmoon.springsecurityjwt.jwt.exception;
 
-public class InvalidAuthTokenException extends RuntimeException {
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+public class InvalidAuthTokenException extends JWTVerificationException {
 	public InvalidAuthTokenException(String message) {
 		super(message);
 	}
