6fcc6859428e140226431e087357c87424b4af35
gmoon92 <gmoon0929@gmail.com>
2022-01-28 09:24:04 +0900

    feat: JWT 검증 필터 JwtVerifyFilter 추가
    - 사용자 인증후 토큰 생성
    
    spring-security-jwt

---
+++ spring-security/spring-security-jwt/src/main/java/com/gmoon/springsecurityjwt/jwt/JwtVerifyFilter.java
@@ -0,0 +1,45 @@
+package com.gmoon.springsecurityjwt.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.gmoon.springsecurityjwt.jwt.exception.JwtVerifyException;
+import com.gmoon.springsecurityjwt.user.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtVerifyFilter extends BasicAuthenticationFilter {
+	private final JwtUtil jwtUtil;
+
+	public JwtVerifyFilter(AuthenticationEntryPoint authenticationEntryPoint, JwtUtil jwtUtils) {
+		super(authentication -> authentication, authenticationEntryPoint);
+		this.jwtUtil = jwtUtils;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		String token = request.getHeader(JwtAuthenticationFilter.HEADER_NAME);
+		try {
+			User user = jwtUtil.decode(token);
+
+			Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			chain.doFilter(request, response);
+		} catch (JWTVerificationException e) {
+			SecurityContextHolder.clearContext();
+			getAuthenticationEntryPoint().commence(request, response, new JwtVerifyException(e));
+		}
+	}
+}
