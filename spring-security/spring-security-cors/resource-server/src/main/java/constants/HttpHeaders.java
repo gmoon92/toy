6c49e17c4973e8668192b762adcd6086cb45a6f3
gmoon92 <gmoon0929@gmail.com>
2022-01-30 02:46:16 +0900

    feat(HttpHeaders): Http 헤더 상수 클래스 추가
    - org.springframework.http.HttpHeaders 확장
    
    spring-security-cors

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/constants/HttpHeaders.java
@@ -0,0 +1,9 @@
+package constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class HttpHeaders extends org.springframework.http.HttpHeaders {
+	public static final String X_FORWARDED_FOR = "X-Forwarded-For";
+}
