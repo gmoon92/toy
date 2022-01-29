fa026f49edadb24c561ee9eaeea1314a3233a556
gmoon92 <gmoon0929@gmail.com>
2022-01-29 20:03:30 +0900

    feat: Bookmark 샘플 데이터 추가
    - data.sql
    - defer-datasource-initialization
    
    spring-security-jwt

---
+++ spring-security/spring-security-cors/resource-server/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_bookmark (id, name) VALUES (0, 'https://gmoon92.github.io');
+INSERT INTO tb_bookmark (id, name) VALUES (1, 'https://github.com/gmoon92');
+INSERT INTO tb_bookmark (id, name) VALUES (2, 'https://devahea.github.io');
