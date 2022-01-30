a98a6c4265076ab0bc9709d5855f1a3a85c3c291
gmoon92 <gmoon0929@gmail.com>
2022-01-31 03:16:54 +0900

    feat(CorsOrigin): Cors Origin 도메인 추가
    spring-security-cors

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/cors/CorsOrigin.java
@@ -0,0 +1,33 @@
+package com.gmoon.resourceserver.cors;
+
+import java.io.Serializable;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "tb_cors_origin",
+	uniqueConstraints = {@UniqueConstraint(name = "u_schema_host_port", columnNames = {"schema", "host", "port"})})
+@EqualsAndHashCode(of = {"origin"})
+public class CorsOrigin implements Serializable {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "schema", column = @Column(name = "schema")),
+		@AttributeOverride(name = "host", column = @Column(name = "host")),
+		@AttributeOverride(name = "port", column = @Column(name = "port"))
+	})
+	private Origin origin;
+}
