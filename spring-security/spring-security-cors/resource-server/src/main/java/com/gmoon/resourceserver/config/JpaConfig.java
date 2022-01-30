eb9e4569a6f0d87713c0cdde3482809ec0f3bd1a
gmoon92 <gmoon0929@gmail.com>
2022-01-31 03:18:19 +0900

    feat(JpaConfig): Persistence 설정 클래스 추가
    spring-seurity-cors

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/config/JpaConfig.java
@@ -0,0 +1,22 @@
+package com.gmoon.resourceserver.config;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.gmoon.resourceserver.*"})
+public class JpaConfig {
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
+	}
+}
