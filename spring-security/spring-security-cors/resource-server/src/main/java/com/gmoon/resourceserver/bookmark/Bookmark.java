87fe09f52acea5f582d3b9b7546b465d92cacd26
gmoon92 <gmoon0929@gmail.com>
2022-02-01 01:52:51 +0900

    refactor: 도메인 ID 생성 전략 GenerationType.IDENTITY 로 수정
    - H2 -> MySQL DB 벤더 변경으로 인한 생성 전략 수정
    
    spring-security-cors

--- spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/bookmark/Bookmark.java
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/bookmark/Bookmark.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -22,7 +23,7 @@ import lombok.ToString;
 @Table(name = "tb_bookmark", uniqueConstraints = {@UniqueConstraint(name = "u_name", columnNames = {"name"})})
 public class Bookmark implements Serializable {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
