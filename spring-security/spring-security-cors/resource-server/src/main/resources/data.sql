4993056daca053a0cfe2082a80d98abdd6628b6f
gmoon92 <gmoon0929@gmail.com>
2022-02-01 01:51:39 +0900

    refactor(application.yml): H2 -> MySQL DB 벤더 수정
    - schema-mysql.sql 추가
    - data-mysql.sql 추가
    - DB Initialize 를 위한 하이버네이트 ddl-auto 옵션 none 설정
      - spring.init.* 옵션 참고
    
    spring-security-cors

--- spring-security/spring-security-cors/resource-server/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tb_bookmark (id, name) VALUES (0, 'gmoon92.github.io');
-
-INSERT INTO tb_cors_origin (id, schema, host, port) VALUES (0, 'http', '127.0.0.1', 8081);
-INSERT INTO tb_cors_origin (id, schema, host, port) VALUES (1, 'http', 'localhost', 8081);
-INSERT INTO tb_cors_origin (id, schema, host, port) VALUES (2, 'http', 'localhost', 80);
-INSERT INTO tb_cors_origin (id, schema, host, port) VALUES (3, 'https', 'gmoon92.github.io', 443);
---
+++ spring-security/spring-security-cors/resource-server/src/main/resources/db/data-mysql.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_bookmark (name) VALUES ('gmoon92.github.io');
+INSERT INTO tb_bookmark (name) VALUES ('devahea.github.io');
+
+INSERT INTO tb_cors_origin (`schema`, host, port) VALUES ('http', '127.0.0.1', 8081);
+INSERT INTO tb_cors_origin (`schema`, host, port) VALUES ('http', 'localhost', 8081);
+INSERT INTO tb_cors_origin (`schema`, host, port) VALUES ('http', 'localhost', 80);
+INSERT INTO tb_cors_origin (`schema`, host, port) VALUES ('https', 'gmoon92.github.io', 443);
