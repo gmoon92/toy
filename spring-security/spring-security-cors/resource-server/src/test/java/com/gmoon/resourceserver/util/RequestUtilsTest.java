d43d7b13b07907f1e4baf1523561840e773fc285
gmoon92 <gmoon0929@gmail.com>
2022-02-03 22:51:34 +0900

    style(HttpHeaders) package move
    spring-security-cors

--- spring-security/spring-security-cors/resource-server/src/test/java/com/gmoon/resourceserver/util/RequestUtilsTest.java
+++ spring-security/spring-security-cors/resource-server/src/test/java/com/gmoon/resourceserver/util/RequestUtilsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import constants.HttpHeaders;
+import com.gmoon.resourceserver.constants.HttpHeaders;
 
 class RequestUtilsTest {
 	@Test
