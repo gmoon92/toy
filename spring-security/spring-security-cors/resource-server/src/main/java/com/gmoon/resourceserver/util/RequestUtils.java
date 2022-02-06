2ab15a3326f82dbfbc8e9371028b68aad2bba4f2
gmoon92 <gmoon0929@gmail.com>
2022-02-06 22:39:10 +0900

    feat(RequestUtils): Origin 관련 편의 메서드 추가
    - Origin Pattern 추가
      - Origin Pattern 정규식 Group 구분
    - Origin Schema 반환 메서드 추가
    - Origin Host 반환 메서드 추가
    - Origin Port 반환 메서드 추가
    
    spring-security-cors

--- spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/RequestUtils.java
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/RequestUtils.java
@@ -1,5 +1,8 @@
 package com.gmoon.resourceserver.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -15,6 +18,16 @@ import lombok.extern.slf4j.Slf4j;
 public final class RequestUtils {
 	private static final String DELIMITER_OF_IP = ",";
 
+	private static final int PATTERN_GROUP_OF_SCHEMA = 1;
+	private static final int PATTERN_GROUP_OF_HOST = 2;
+	private static final int PATTERN_GROUP_OF_PORT = 3;
+	private static final String PATTERN_OF_SCHEMA = "(^\\w+(?=:\\/\\/))";
+	private static final String PATTERN_OF_HOST = "((?<=:\\/\\/).*?(?=:|$))";
+	private static final String PATTERN_OF_PORT = "((?<=:)\\d+|$)";
+	private static final Pattern PATTERN_OF_ORIGIN = Pattern.compile(PATTERN_OF_SCHEMA
+		+ ".*?" + PATTERN_OF_HOST
+		+ ".*?" + PATTERN_OF_PORT);
+
 	public static String getClientIpAddress(HttpServletRequest request) {
 		String remoteIpAddress = getRemoteIpAddress(request);
 		String[] ipAddresses = StringUtils.split(remoteIpAddress, DELIMITER_OF_IP);
@@ -26,5 +39,37 @@ public final class RequestUtils {
 		String remoteIpAddress = request.getRemoteAddr();
 		log.info("originIpAddress: {}, remoteIpAddress: {}", originIpAddress, remoteIpAddress);
 		return StringUtils.defaultIfBlank(originIpAddress, remoteIpAddress);
+	}
+
+	public static String getOriginSchema(HttpServletRequest request) {
+		return getOriginPatternGroup(getOrigin(request), PATTERN_GROUP_OF_SCHEMA);
+	}
+
+	public static String getOriginHost(HttpServletRequest request) {
+		return getOriginPatternGroup(getOrigin(request), PATTERN_GROUP_OF_HOST);
+	}
+
+	public static Integer getOriginPort(HttpServletRequest request) {
+		String port = getOriginPatternGroup(getOrigin(request), PATTERN_GROUP_OF_PORT);
+		if (StringUtils.isBlank(port)) {
+			return null;
+		}
+
+		return Integer.parseInt(port);
+	}
+
+	private static String getOrigin(HttpServletRequest request) {
+		String origin = request.getHeader(HttpHeaders.ORIGIN);
+		log.info("origin: {}", origin);
+		return StringUtils.defaultString(origin);
+	}
+
+	private static String getOriginPatternGroup(String origin, int group) {
+		Matcher m = PATTERN_OF_ORIGIN.matcher(origin);
+		if (m.find()) {
+			return m.group(group);
+		}
+
+		return "";
 	}
 }
