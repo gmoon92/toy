0c0d72913aff1f30cc93c81f2ac6f36397f6e827
gmoon92 <gmoon0929@gmail.com>
2022-02-07 22:25:50 +0900

    refactor(CorsUtils): CORS Origin 편의 메서드 추가
    - Origin host 기준으로 schema, port 무시하는 정규식 패턴 반환 메서드 추가
    - 중복 코드 제거
    
    spring-security-cors

--- spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/CorsUtils.java
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/CorsUtils.java
@@ -1,5 +1,8 @@
 package com.gmoon.resourceserver.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -10,9 +13,29 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CorsUtils {
+	private static final String PATTERN_OF_ORIGIN = "**://%s:**";
+	private static final String PATTERN_OF_IGNORE_PORT_ORIGIN = "**://%s";
+
 	private final CorsProperties properties;
 
-	private boolean skipCheckedOriginUri(HttpServletRequest request) {
+	public List<String> getAllowedOriginPatterns(List<String> hosts) {
+		List<String> result = new ArrayList<>(hosts.size() * 2);
+		for (String host : hosts) {
+			result.add(String.format(PATTERN_OF_ORIGIN, host));
+			result.add(String.format(PATTERN_OF_IGNORE_PORT_ORIGIN, host));
+		}
+		return result;
+	}
+
+	public List<String> getCorsAllowedMethods(HttpServletRequest request) {
+		if (skipCheckedOriginUri(request)) {
+			return CorsProperties.ALL_OF_HTTP_METHODS;
+		}
+
+		return properties.getAccessControlAllowMethods();
+	}
+
+	public boolean skipCheckedOriginUri(HttpServletRequest request) {
 		return !properties.isEnabled()
 			&& StringUtils.isBlank(RequestUtils.getOrigin(request));
 	}
