e7b6679748b56ed671705f54835fa118a6b9c6ef
gmoon92 <gmoon0929@gmail.com>
2022-02-09 12:38:51 +0900

    chore: spring-redis-session init

---
+++ spring-cache/spring-redis-session/src/test/java/com/gmoon/springredissession/SpringRedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springredissession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
