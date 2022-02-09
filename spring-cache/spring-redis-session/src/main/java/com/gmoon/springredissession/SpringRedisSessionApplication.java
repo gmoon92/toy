4e44ba72cf00cdb95c8b5a831146a9a8e19800cc
gmoon92 <gmoon0929@gmail.com>
2022-02-09 12:38:51 +0900

    chore: spring-redis-session init

---
+++ spring-cache/spring-redis-session/src/main/java/com/gmoon/springredissession/SpringRedisSessionApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springredissession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisSessionApplication.class, args);
+	}
+
+}
