75babed9a7f34f97968719baefa899c123bf32c7
gmoon92 <gmoon0929@gmail.com>
2022-02-14 09:46:44 +0900

    feat(NotFoundDataException): Redis Data Exception add
    spring-data-redis

---
+++ spring-cache/spring-data-redis/src/main/java/com/gmoon/springdataredis/redis/NotFoundDataException.java
@@ -0,0 +1,17 @@
+package com.gmoon.springdataredis.redis;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class NotFoundDataException extends RuntimeException {
+	private static final String MESSAGE_FORMAT = "key %s not be found on the redis server.";
+
+	public NotFoundDataException(String key) {
+		super(String.format(MESSAGE_FORMAT, key));
+	}
+
+	public NotFoundDataException(String key, Throwable cause) {
+		super(String.format(MESSAGE_FORMAT, key), cause);
+	}
+}
