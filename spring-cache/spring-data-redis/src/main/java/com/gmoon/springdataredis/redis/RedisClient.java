8217aab48e007213163ccd5da3f6e5f2dc021563
gmoon92 <gmoon0929@gmail.com>
2022-02-14 10:41:42 +0900

    feat(RedisClient): Redis Util 클래스 추가
    - 저장
    - 삭제
    - TTL 수정/조회
    - 캐시 키 검색
    
    spring-data-redis

---
+++ spring-cache/spring-data-redis/src/main/java/com/gmoon/springdataredis/redis/RedisClient.java
@@ -0,0 +1,76 @@
+package com.gmoon.springdataredis.redis;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisClient {
+	private static final String ALL = "*";
+
+	private final RedisTemplate<String, Object> template;
+
+	public void save(Cache cache, Serializable value) {
+		template.opsForValue()
+			.set(cache.getKey(), value, cache.getTtl());
+	}
+
+	public <T extends Serializable> T find(String key) {
+		return (T)template.opsForValue().get(key);
+	}
+
+	public <T extends Serializable> T getOrElseThrow(String key) {
+		if (BooleanUtils.toBoolean(template.hasKey(key))) {
+			return find(key);
+		}
+
+		throw new NotFoundDataException(key);
+	}
+
+	public <T extends Serializable> T getAndChangedByEternalKey(String key) {
+		return (T)template.opsForValue().getAndPersist(key);
+	}
+
+	public void changeTTL(String key, Duration ttl) {
+		template.expire(key, ttl);
+	}
+
+	public Long getTTL(String key) {
+		return template.getExpire(key);
+	}
+
+	public boolean delete(String... key) {
+		return delete(Arrays.asList(key));
+	}
+
+	// Redis PipeLining used
+	public boolean delete(Collection<String> keys) {
+		Long result = template.delete(keys);
+		if (Objects.isNull(result)) {
+			return false;
+		}
+		return BooleanUtils.toBoolean(result.intValue());
+	}
+
+	public boolean deleteAll() {
+		Set<String> keys = keys(ALL);
+		log.info("delete keys: {}", keys);
+		return delete(keys);
+	}
+
+	public Set<String> keys(String pattern) {
+		return template.keys(pattern);
+	}
+}
