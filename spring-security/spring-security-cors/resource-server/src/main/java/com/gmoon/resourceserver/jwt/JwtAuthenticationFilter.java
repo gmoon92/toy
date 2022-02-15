a1fb827cea375e9785b98014e3f126f01af850e7
gmoon92 <gmoon0929@gmail.com>
2022-02-15 12:48:47 +0900

    feat(JwtAuthenticationFilter): JWT 인증 필터 추가
    - Http Header JWT 생성
    
    spring-security-cors, resource-server

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.gmoon.resourceserver.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.gmoon.resourceserver.constants.HttpHeaders;
+import com.gmoon.resourceserver.user.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private final JwtUtil jwtUtil;
+
+	public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+		super(authenticationManager);
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+		User user = (User)authentication.getPrincipal();
+
+		String token = jwtUtil.generate(user);
+		response.setHeader(HttpHeaders.AUTHORIZATION, token);
+	}
+
+	// todo: login failure logic
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+		log.warn("access unauthenticated user.");
+		super.unsuccessfulAuthentication(request, response, e);
+	}
+}
