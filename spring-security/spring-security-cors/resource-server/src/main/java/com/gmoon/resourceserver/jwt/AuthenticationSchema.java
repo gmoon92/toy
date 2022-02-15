2acc03ebd8b57b363dcb463a4b6beb9a63a78795
gmoon92 <gmoon0929@gmail.com>
2022-02-15 12:46:28 +0900

    feat(AuthenticationSchema): JWT 스키마 이넘 클래스 추가
    spring-security-cors, resource-server

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/jwt/AuthenticationSchema.java
@@ -0,0 +1,40 @@
+package com.gmoon.resourceserver.jwt;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthenticationSchema {
+	BASIC("Basic"),
+	BEARER("Bearer"),
+	DIGEST("Digest"),
+	HOBA("HOBA"),
+	MUTUAL("Mutual"),
+	NEGOTIATE("Negotiate"),
+	OAUTH("OAuth"),
+	SHA1("SCRAM-SHA-1"),
+	SHA256("SCRAM-SHA-256"),
+	VAPID("vapid");
+
+	private static final Map<String, AuthenticationSchema> SCHEMAS = Collections.unmodifiableMap(Stream.of(values())
+		.collect(Collectors.toMap(AuthenticationSchema::getName, Function.identity())));
+
+	private final String name;
+
+	public static void checkValidSchema(String schema) {
+		if (!contain(schema)) {
+			throw new IllegalArgumentException(String.format("Invalid authentication schema name: %s", schema));
+		}
+	}
+
+	private static boolean contain(String schema) {
+		return SCHEMAS.containsKey(schema);
+	}
+}
