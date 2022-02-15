ede51e021ceb3044f0d27055e4bf9f982f791227
gmoon92 <gmoon0929@gmail.com>
2022-02-15 12:44:49 +0900

    feat(SecurityUtils): Security util class add
    - bcrypt password encoder
    
    spring-security-cors, resource-server

---
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/SecurityUtils.java
@@ -0,0 +1,16 @@
+package com.gmoon.resourceserver.util;
+
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SecurityUtils {
+	public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+	public static String encodePassword(CharSequence password) {
+		return PASSWORD_ENCODER.encode(password);
+	}
+}
