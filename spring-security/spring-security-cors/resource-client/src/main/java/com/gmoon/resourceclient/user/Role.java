8fb5440fe178dda5efd3fd69868b84092025d36c
gmoon92 <gmoon0929@gmail.com>
2022-02-24 00:05:02 +0900

    feat(Role): GrantedAuthority implementation enum class add
    spring-security-cors, resource-client

---
+++ spring-security/spring-security-cors/resource-client/src/main/java/com/gmoon/resourceclient/user/Role.java
@@ -0,0 +1,14 @@
+package com.gmoon.resourceclient.user;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role implements GrantedAuthority {
+	RESOURCE_SERVER("ROLE_RESOURCE_SERVER");
+
+	private final String authority;
+}
