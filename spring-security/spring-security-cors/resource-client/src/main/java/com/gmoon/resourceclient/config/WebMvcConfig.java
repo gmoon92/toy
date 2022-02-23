0130dc98b0e878415ab4449f0fa0c0cd5e93c8f2
gmoon92 <gmoon0929@gmail.com>
2022-02-24 00:00:14 +0900

    feat: bookmark view add
    - index.html
    
    spring-security-cors, resource-client

---
+++ spring-security/spring-security-cors/resource-client/src/main/java/com/gmoon/resourceclient/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.gmoon.resourceclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+}
