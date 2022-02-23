5f1bf951c5368b575b90ff640a2fa4b5e4f04bdf
gmoon92 <gmoon0929@gmail.com>
2022-02-24 00:27:54 +0900

    feat(SecurityConfig): 클라이언트 시큐리티 설정 추가
    - Exception Handling 설정 추가
    - SOP 정책 추가
    - 폼 로그인 활성화
    
    spring-security-cors, resource-client

---
+++ spring-security/spring-security-cors/resource-client/src/main/java/com/gmoon/resourceclient/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.gmoon.resourceclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import com.gmoon.resourceclient.security.OAuthenticationFilter;
+import com.gmoon.resourceclient.security.OAuthenticationProvider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.exceptionHandling(this::exceptionHandling)
+			.headers(this::headers)
+			.authorizeRequests(this::authorizeRequests)
+			.authenticationProvider(authenticationProvider())
+			.addFilter(new OAuthenticationFilter(authenticationManager(), authenticationFailureHandler()))
+			.formLogin();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		return new OAuthenticationProvider();
+	}
+
+	private void exceptionHandling(ExceptionHandlingConfigurer<HttpSecurity> configurer) {
+		configurer.accessDeniedHandler((request, response, accessDeniedException) -> {
+			SecurityContext context = SecurityContextHolder.getContext();
+			Authentication authentication = context.getAuthentication();
+			UserDetails user = (UserDetails)authentication.getPrincipal();
+
+			String username = user.getUsername();
+			String method = request.getMethod();
+			String url = request.getRequestURI();
+			log.error(String.format("%s denied to access (%s) %s.", username, method, url), accessDeniedException);
+			response.sendRedirect("/login");
+		});
+	}
+
+	private void headers(HeadersConfigurer<HttpSecurity> config) {
+		config.frameOptions().sameOrigin();
+	}
+
+	private void authorizeRequests(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry) {
+		registry.anyRequest().authenticated();
+	}
+
+	private AuthenticationFailureHandler authenticationFailureHandler() {
+		RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+		return (request, response, exception) -> {
+			log.error("authentication failure.", exception);
+			redirectStrategy.sendRedirect(request, response, "/login");
+		};
+	}
+}
