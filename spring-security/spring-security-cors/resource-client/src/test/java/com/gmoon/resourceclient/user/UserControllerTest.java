0dbfd50a600908c4bb92cf004c42c329619d06bd
gmoon92 <gmoon0929@gmail.com>
2022-02-24 00:27:54 +0900

    feat(SecurityConfig): 클라이언트 시큐리티 설정 추가
    - Exception Handling 설정 추가
    - SOP 정책 추가
    - 폼 로그인 활성화
    
    spring-security-cors, resource-client

---
+++ spring-security/spring-security-cors/resource-client/src/test/java/com/gmoon/resourceclient/user/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.gmoon.resourceclient.user;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+class UserControllerTest {
+
+	@Autowired
+	WebApplicationContext context;
+
+	MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.apply(springSecurity())
+			.alwaysDo(print())
+			.build();
+	}
+
+	@Test
+	@Disabled("Resource Server 어플리케이션 실행 후 테스트")
+	void testFormLogin() throws Exception{
+		// given
+		String username = "admin";
+		String password = "123";
+
+		// when
+		ResultActions result = mockMvc.perform(formLogin()
+			.user(username)
+			.password(password));
+
+		// then
+		result.andExpect(authenticated());
+	}
+}
