5072b990ae76a965221e10ca0f9f5fe7542cdd26
gmoon92 <gmoon0929@gmail.com>
2022-02-26 21:57:36 +0900

    fix: @SpringBootTest db data rollback 처리를 위한 @Transactional 추가
    spring-security-jwt

--- spring-security/spring-security-jwt/src/test/java/com/gmoon/springsecurityjwt/team/TeamServiceTest.java
+++ spring-security/spring-security-jwt/src/test/java/com/gmoon/springsecurityjwt/team/TeamServiceTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class TeamServiceTest {
 	@Autowired TeamService service;
 
