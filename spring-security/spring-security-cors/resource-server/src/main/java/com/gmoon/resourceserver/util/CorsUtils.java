459c50a7b312f3eda3fea166a79e68d4812d0650
gmoon92 <gmoon0929@gmail.com>
2022-03-01 20:52:44 +0900

    refactor(CorsUtils): @UtilityClass using
    - 불변 유틸 클래스 선언
    
    spring-security-cors, resource-server

--- spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/CorsUtils.java
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/util/CorsUtils.java
@@ -3,15 +3,14 @@ package com.gmoon.resourceserver.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class CorsUtils {
-	private static final String PATTERN_OF_ORIGIN = "**://%s:**";
-	private static final String PATTERN_OF_IGNORE_PORT_ORIGIN = "**://%s";
+@UtilityClass
+public class CorsUtils {
+	private final String PATTERN_OF_ORIGIN = "**://%s:**";
+	private final String PATTERN_OF_IGNORE_PORT_ORIGIN = "**://%s";
 
-	public static List<String> getAllowedOriginPatterns(List<String> hosts) {
+	public List<String> getAllowedOriginPatterns(List<String> hosts) {
 		List<String> result = new ArrayList<>(hosts.size() * 2);
 		for (String host : hosts) {
 			result.add(String.format(PATTERN_OF_ORIGIN, host));
