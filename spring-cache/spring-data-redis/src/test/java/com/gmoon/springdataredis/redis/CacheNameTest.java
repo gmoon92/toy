a3adc052404bc2b53efb32523b229561f90be300
gmoon92 <gmoon0929@gmail.com>
2022-03-10 23:51:54 +0900

    feat: CacheName enum class add
    spring-cache, spring-data-redis

---
+++ spring-cache/spring-data-redis/src/test/java/com/gmoon/springdataredis/redis/CacheNameTest.java
@@ -0,0 +1,14 @@
+package com.gmoon.springdataredis.redis;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CacheNameTest {
+	@Test
+	void testGetAll() {
+		// when then
+		assertThat(CacheName.getAll())
+			.hasSize(CacheName.values().length);
+	}
+}
