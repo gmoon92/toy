6276db52dab0450949ece6717a046f13bafece4f
gmoon92 <gmoon0929@gmail.com>
2022-03-11 00:12:16 +0900

    feat: cache expire config first class add
    spring-cache, spring-data-redis

---
+++ spring-cache/spring-data-redis/src/main/java/com/gmoon/springdataredis/config/CacheExpireConfigs.java
@@ -0,0 +1,30 @@
+package com.gmoon.springdataredis.config;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import com.gmoon.springdataredis.redis.CacheName;
+
+import lombok.Getter;
+
+@Getter
+public class CacheExpireConfigs {
+	private final Map<String, RedisCacheConfiguration> values;
+
+	private CacheExpireConfigs() {
+		this.values = new HashMap<>();
+	}
+
+	public static CacheExpireConfigs create() {
+		return new CacheExpireConfigs();
+	}
+
+	public CacheExpireConfigs putExpireMinutes(CacheName cacheName, int minutes) {
+		Duration expireTtl = Duration.ofMinutes(minutes);
+		values.put(cacheName.getValue(), RedisUtils.createRedisCacheConfig(expireTtl));
+		return this;
+	}
+}
