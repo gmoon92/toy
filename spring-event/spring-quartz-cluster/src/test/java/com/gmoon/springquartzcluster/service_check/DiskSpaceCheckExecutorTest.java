899b7efc4a01791decd2243ffb634fb56d091886
gmoon92 <gmoon0929@gmail.com>
2022-03-15 22:26:10 +0900

    fix: service check test case disabled
    - @Disabled("github action 로 인한 테스트 비활성화")

--- spring-event/spring-quartz-cluster/src/test/java/com/gmoon/springquartzcluster/service_check/DiskSpaceCheckExecutorTest.java
+++ spring-event/spring-quartz-cluster/src/test/java/com/gmoon/springquartzcluster/service_check/DiskSpaceCheckExecutorTest.java
@@ -1,5 +1,6 @@
 package com.gmoon.springquartzcluster.service_check;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import com.gmoon.springquartzcluster.test.TestPropertiesConfig;
 
 import lombok.RequiredArgsConstructor;
 
+@Disabled("github action 로 인한 테스트 비활성화")
 @TestPropertiesConfig(DiskSpaceCheckExecutor.class)
 @RequiredArgsConstructor
 class DiskSpaceCheckExecutorTest {
