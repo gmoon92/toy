aff1ff5c47da2e399c4036e6213877d194d3e845
gmoon92 <gmoon0929@gmail.com>
2022-03-16 20:38:33 +0900

    feat(CacheUser): RedisHash Object add
    spring-cache, spring-data-redis

---
+++ spring-cache/spring-data-redis/src/main/java/com/gmoon/springdataredis/user/CacheUser.java
@@ -0,0 +1,59 @@
+package com.gmoon.springdataredis.user;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import com.gmoon.springdataredis.cache.Cache;
+import com.gmoon.springdataredis.cache.CacheName;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RedisHash(value = CacheUser.KEY)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CacheUser implements Cache {
+	static final String KEY = CacheName.Constants.USER;
+	static final long MINUTES_OF_TTL = 5;
+
+	@Id private String id;
+
+	@Indexed
+	@EqualsAndHashCode.Include
+	private String username;
+	private String email;
+	private boolean enabled;
+
+	@TimeToLive(unit = TimeUnit.MINUTES)
+	private Long expiration;
+
+	@Builder
+	private CacheUser(String id, String username, String email, boolean enabled) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.enabled = enabled;
+		this.expiration = MINUTES_OF_TTL;
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public Duration getTtl() {
+		return Duration.ofMinutes(MINUTES_OF_TTL);
+	}
+}
