993ba3c4116f18ad61617d6222e57b67eda3c483
gmoon92 <gmoon0929@gmail.com>
2022-03-21 19:05:04 +0900

    feat(JpaConfig): JpaQueryFactory bean add
    spring-jpa, spring-jpa-pagination

---
+++ spring-jpa/spring-jpa-pagination/src/main/java/com/gmoon/springjpapagination/config/JpaConfig.java
@@ -0,0 +1,16 @@
+package com.gmoon.springjpapagination.config;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class JpaConfig {
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
+	}
+}
