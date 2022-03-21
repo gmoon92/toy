2045f7245767ebd1f4150749c835f0cd26b872fb
gmoon92 <gmoon0929@gmail.com>
2022-03-21 13:30:24 +0900

    chore: spring-jpa-pagination module init

---
+++ spring-jpa/spring-jpa-pagination/src/test/java/com/gmoon/springjpapagination/SpringJpaPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjpapagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
