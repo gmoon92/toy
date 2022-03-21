1750d49f6e9a64d2a73f9f403e824093d2fbfa06
gmoon92 <gmoon0929@gmail.com>
2022-03-21 19:06:05 +0900

    feat(application): jpa, spring.sql prop add
    spring-jpa, spring-jpa-pagination

---
+++ spring-jpa/spring-jpa-pagination/src/main/resources/db/schema-h2.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS lt_user_login;
+
+CREATE TABLE lt_user_login
+(
+    id            varchar(50)  NOT NULL PRIMARY KEY,
+    username      varchar(50),
+    access_device varchar(50)  DEFAULT 'WEB',
+    attempt_ip    varchar(50),
+    attempt_dt    timestamp,
+    succeed       boolean      DEFAULT 0
+);
