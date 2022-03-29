0248615a4d14cb99118db9e8eb01114abd50b850
gmoon92 <gmoon0929@gmail.com>
2022-03-29 11:28:06 +0900

    feat(UserLoginLogListVO): 커서 페이지네이션 구현 클래스 추가
    - isHasNextPage(): 다음 페이지 존재 여부
    - getCursor(): 마지막행 커서 데이터
    
    spring-jpa, spring-jpa-pagination

---
+++ spring-jpa/spring-jpa-pagination/src/main/java/com/gmoon/springjpapagination/domain/UserLoginLogListVO.java
@@ -0,0 +1,48 @@
+package com.gmoon.springjpapagination.domain;
+
+import java.util.List;
+
+import com.gmoon.springjpapagination.common.CursorPagination;
+import com.querydsl.core.annotations.QueryProjection;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserLoginLogListVO extends CursorPagination {
+	private String cursor;
+	private boolean hasNextPage;
+
+	private List<Data> list;
+
+	@Getter
+	@ToString
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	public static class Data {
+		private String username;
+		private AccessDevice accessDevice;
+		private String attemptDateString;
+		private boolean succeed;
+		private String cursor;
+
+		@QueryProjection
+		public Data(String username, AccessDevice accessDevice, String attemptDateString, Boolean succeed, String cursor) {
+			this.username = username;
+			this.accessDevice = accessDevice;
+			this.attemptDateString = attemptDateString;
+			this.succeed = succeed;
+			this.cursor = cursor;
+		}
+	}
+
+	@Override
+	public int getPageSize() {
+		return DEFAULT_PAGE_SIZE;
+	}
+}
