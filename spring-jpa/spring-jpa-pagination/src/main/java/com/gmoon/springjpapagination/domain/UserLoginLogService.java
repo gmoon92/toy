c039f913a73b8d895dbef1d900bb756fd2a09f9f
gmoon92 <gmoon0929@gmail.com>
2022-03-29 11:29:15 +0900

    feat(UserLoginLogService): 사용자 로그인 로그 커서 페이지네이션 구현
    - getUserLoginLogListVO(): 요청 커서에 대한 응답
    - hasNextPage(): 다음 페이지 존재 여부
    
    spring-jpa, spring-jpa-pagination

---
+++ spring-jpa/spring-jpa-pagination/src/main/java/com/gmoon/springjpapagination/domain/UserLoginLogService.java
@@ -0,0 +1,89 @@
+package com.gmoon.springjpapagination.domain;
+
+import static com.gmoon.springjpapagination.domain.QUserLoginLog.*;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gmoon.springjpapagination.common.CursorPagination;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserLoginLogService {
+	private final JPAQueryFactory factory;
+
+	@Transactional(readOnly = true)
+	public UserLoginLogListVO getUserLoginLogListVO(UserLoginLogListVO listVO) {
+		String cursor = listVO.getCursor();
+		Order cursorOrder = Order.DESC;
+
+		JPAQuery<UserLoginLogListVO.Data> query = factory
+			.select(
+				new QUserLoginLogListVO_Data(userLoginLog.username, userLoginLog.accessDevice,
+					userLoginLog.attemptIp, userLoginLog.succeed,
+					cursor())
+			)
+			.from(userLoginLog)
+			.where(cursorPagination(cursor, cursorOrder))
+			.orderBy(userLoginLog.attemptDt.desc(), userLoginLog.id.desc())
+			.limit(listVO.getPageSize());
+
+		List<UserLoginLogListVO.Data> list = query.fetch();
+		listVO.setList(list);
+
+		String lastRowCursor = getLastRowCursor(list);
+		listVO.setCursor(lastRowCursor);
+		listVO.setHasNextPage(hasNextPage(lastRowCursor, cursorOrder));
+		return listVO;
+	}
+
+	@Transactional(readOnly = true)
+	public boolean hasNextPage(String cursor, Order cursorOrder) {
+		return factory.selectOne()
+			.from(userLoginLog)
+			.where(cursorPagination(cursor, cursorOrder))
+			.orderBy(userLoginLog.attemptDt.desc(), userLoginLog.id.desc())
+			.fetchOne() != null;
+	}
+
+	private String getLastRowCursor(List<UserLoginLogListVO.Data> list) {
+		return list.stream()
+			.map(UserLoginLogListVO.Data::getCursor)
+			.reduce((data, other) -> other)
+			.orElse(CursorPagination.EMPTY_CURSOR);
+	}
+
+	private BooleanExpression cursorPagination(String cursor, Order cursorOrder) {
+		boolean isFirstPage = StringUtils.isBlank(cursor);
+		if (isFirstPage) {
+			return null;
+		}
+
+		if (Order.ASC == cursorOrder) {
+			return cursor().gt(cursor);
+		}
+
+		if (Order.DESC == cursorOrder) {
+			return cursor().lt(cursor);
+		}
+
+		throw new RuntimeException("must be specify the order of cursor data.");
+	}
+
+	private StringExpression cursor() {
+		return Expressions.stringTemplate("CONCAT({0}, LPAD({1}, 50, '0'))", userLoginLog.attemptDt, userLoginLog.id);
+	}
+}
---
+++ spring-jpa/spring-jpa-pagination/src/test/java/com/gmoon/springjpapagination/domain/UserLoginLogServiceTest.java
@@ -0,0 +1,38 @@
+package com.gmoon.springjpapagination.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gmoon.springjpapagination.common.CursorPagination;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Transactional
+@SpringBootTest
+class UserLoginLogServiceTest {
+	@Autowired
+	UserLoginLogService service;
+
+	@Test
+	void testGetUserLoginLogListVO() {
+		// given
+		UserLoginLogListVO listVO = new UserLoginLogListVO();
+		listVO.setCursor("1900-02-02 12:34:43000000000000000000000000000000000000000000000lul06");
+
+		// when
+		listVO = service.getUserLoginLogListVO(listVO);
+
+		// then
+		log.info("조회된 마지막 행의 커서 데이터: {}", listVO.getCursor());
+		log.info("다음 페이지 존재 여부: {}", listVO.isHasNextPage());
+		log.info("요청 커서에 대한 결과: {}", listVO.getList());
+		assertThat(listVO)
+			.hasFieldOrPropertyWithValue("cursor", CursorPagination.EMPTY_CURSOR)
+			.hasFieldOrPropertyWithValue("hasNextPage", false);
+	}
+}
