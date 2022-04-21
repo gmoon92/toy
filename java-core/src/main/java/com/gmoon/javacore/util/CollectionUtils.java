38a5b112feb985cffe58775b2d3556cba36800de
gmoon92 <gmoon0929@gmail.com>
2022-04-21 13:12:30 +0900

    feat: StringUtils, CollectionUtils add
    java-core

---
+++ java-core/src/main/java/com/gmoon/javacore/util/CollectionUtils.java
@@ -0,0 +1,66 @@
+package com.gmoon.javacore.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CollectionUtils {
+	public static boolean isEmpty(Collection<?> collection) {
+		return org.apache.commons.collections4.CollectionUtils.isEmpty(collection);
+	}
+
+	public static boolean isNotEmpty(Collection<?> collection) {
+		return !isEmpty(collection);
+	}
+
+	public static boolean isEmptyAny(Collection<?>... collections) {
+		for (Collection<?> collection : collections) {
+			if (isEmpty(collection)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static boolean containsAny(Collection<?> collection, Collection<?> collection2) {
+		return org.apache.commons.collections4.CollectionUtils.containsAny(collection, collection2);
+	}
+
+	public static int size(Collection<?> collection) {
+		return org.apache.commons.collections4.CollectionUtils.size(collection);
+	}
+
+	public static void reverseArray(String[] result) {
+		org.apache.commons.collections4.CollectionUtils.reverseArray(result);
+	}
+
+	public static <T> List<T> removeDuplicates(List<T> list) {
+		if (isEmpty(list)) {
+			return new ArrayList<>();
+		}
+
+		return list.stream()
+			.distinct()
+			.collect(Collectors.toList());
+	}
+
+	public static int minSize(Collection<?>... collections) {
+		if (ArrayUtils.isEmpty(collections)) {
+			return 0;
+		}
+
+		return Stream.of(collections)
+			.mapToInt(CollectionUtils::size)
+			.min()
+			.orElse(0);
+	}
+}
---
+++ java-core/src/test/java/com/gmoon/javacore/util/CollectionUtilsTest.java
@@ -0,0 +1,25 @@
+package com.gmoon.javacore.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CollectionUtilsTest {
+
+	@Test
+	void testMinSize() {
+		// given
+		List<String> names = Arrays.asList("gmoon", "guest");
+		List<Integer> ages = Arrays.asList(10, 20, 30);
+
+		// when
+		int minSize = CollectionUtils.minSize(names, ages);
+
+		// then
+		assertThat(minSize)
+			.isEqualTo(2);
+	}
+}
