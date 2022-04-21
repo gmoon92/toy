240171f1f9168ee0532e9ad6ff70d41fa6f06d18
gmoon92 <gmoon0929@gmail.com>
2022-04-21 10:09:08 +0900

    feat: REST-assured project init
    rest-assured

---
+++ testing/rest-assured/src/main/java/com/gmoon/restassured/RestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.restassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAssuredApplication.class, args);
+	}
+
+}
---
+++ testing/rest-assured/src/test/java/com/gmoon/restassured/RestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
