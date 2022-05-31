a48f5ff92436f015f020f056ad2f50c53a6be3ca
gmoon92 <gmoon0929@gmail.com>
2022-05-31 14:05:07 +0900

    feat(PropertiesUtils): properties 파일 읽기 기능 추가
    java-core

---
+++ java-core/src/main/java/com/gmoon/javacore/util/PropertiesUtils.java
@@ -0,0 +1,23 @@
+package com.gmoon.javacore.util;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PropertiesUtils {
+
+	public static Properties load(String path) {
+		try {
+			Properties properties = new Properties();
+			properties.load(Objects.requireNonNull(PropertiesUtils.class.getClassLoader().getResource(path)).openStream());
+			return properties;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
