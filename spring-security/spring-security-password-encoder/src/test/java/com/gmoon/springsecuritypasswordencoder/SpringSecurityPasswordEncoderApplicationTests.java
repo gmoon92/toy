aa351d5d422a6db47a533bafbd88e6cac25ac885
gmoon92 <gmoon0929@gmail.com>
2022-06-10 14:24:03 +0900

    chore: spring-security-password-encoder project init
    spring-security, spring-security-password-encoder

---
+++ spring-security/spring-security-password-encoder/src/test/java/com/gmoon/springsecuritypasswordencoder/SpringSecurityPasswordEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springsecuritypasswordencoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPasswordEncoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
