6d44abb52d28fe2b6937ec4069c621edf8a34ca6
gmoon92 <gmoon0929@gmail.com>
2022-06-11 15:24:59 +0900

    feat(PasswordUtils): 패스워드 유틸 클래스 추가
    - DelegatingPasswordEncoder 학습 코드 추가
    
    spring-security, spring-security-password-encoder

---
+++ spring-security/spring-security-password-encoder/src/main/java/com/gmoon/springsecuritypasswordencoder/password/PasswordUtils.java
@@ -0,0 +1,27 @@
+package com.gmoon.springsecuritypasswordencoder.password;
+
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordUtils {
+	private static final PasswordEncoderHolder DEFAULT_ENCODE = PasswordEncoderHolder.BCRYPT;
+	private static final PasswordEncoder PASSWORD_ENCODER;
+
+	static {
+		DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(
+			DEFAULT_ENCODE.getId(),
+			PasswordEncoderHolder.ALL,
+			PasswordEncoderHolder.ID_PREFIX, PasswordEncoderHolder.ID_SUFFIX);
+		delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(DEFAULT_ENCODE.getValue());
+
+		PASSWORD_ENCODER = delegatingPasswordEncoder;
+	}
+
+	public static boolean matches(String plainPassword, String encodedPassword) {
+		return PASSWORD_ENCODER.matches(plainPassword, encodedPassword);
+	}
+
+	public static boolean isNeedToUpgradeEncoding(String encodedPassword) {
+		return PASSWORD_ENCODER.upgradeEncoding(encodedPassword);
+	}
+}
