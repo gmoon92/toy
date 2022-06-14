31f8f6a073b3f93e63ab808cca92c2496c09343d
gmoon92 <gmoon0929@gmail.com>
2022-06-14 21:28:16 +0900

    chore: Hibernate Annotations project init
    spring-jpa, hibernate-annotation

---
+++ spring-jpa/hibernate-annotation/src/main/java/com/gmoon/hibernateannotation/HibernateAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernateannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAnnotationApplication.class, args);
+	}
+
+}
