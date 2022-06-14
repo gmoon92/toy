a21d27132f5763cff0f4e6ee91544a74802f5fc6
gmoon92 <gmoon0929@gmail.com>
2022-06-14 21:28:16 +0900

    chore: Hibernate Annotations project init
    spring-jpa, hibernate-annotation

---
+++ spring-jpa/hibernate-annotation/src/test/java/com/gmoon/hibernateannotation/HibernateAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernateannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
