832c1bd3e0bab4c4c9ec3a93dce3b13d8a2a9bc0
gmoon92 <gmoon0929@gmail.com>
2022-06-29 10:02:20 +0900

    test: static method mocking case add
    - Mockito inline
    
    testing, mockito

---
+++ testing/mockito/src/test/java/com/gmoon/mockito/util/StringUtilsTest.java
@@ -0,0 +1,33 @@
+package com.gmoon.mockito.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+class StringUtilsTest {
+
+	@DisplayName("utils class mocking")
+	@Test
+	void testMockingStaticMethods() {
+		try (MockedStatic<StringUtils> utils = Mockito.mockStatic(StringUtils.class)) {
+			String name = "gmoon";
+
+			utils.when(() -> StringUtils.isEmpty(name))
+				.thenReturn(true);
+
+			assertThat(StringUtils.isEmpty("gmoon2")).isFalse();
+			assertThat(StringUtils.isEmpty(name)).isTrue();
+		}
+	}
+
+	static final class StringUtils {
+		private StringUtils() {
+		}
+		static boolean isEmpty(String str) {
+			return str == null || str.length() == 0;
+		}
+	}
+}
