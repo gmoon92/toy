1639cd4abb9ef7da5504e1b51d8aa1f279605db8
gmoon92 <gmoon0929@gmail.com>
2022-07-08 10:03:35 +0900

    feat(Member): 회원 도메인 추가
    hibernate-annotation

---
+++ spring-jpa/hibernate-annotation/src/main/java/com/gmoon/hibernateannotation/domain/Member.java
@@ -0,0 +1,24 @@
+package com.gmoon.hibernateannotation.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class Member implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+}
