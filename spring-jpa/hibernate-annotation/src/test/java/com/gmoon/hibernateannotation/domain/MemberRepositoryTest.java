6a3a17f5b5e8957eb29ba6ea49001d8bde45fc56
gmoon92 <gmoon0929@gmail.com>
2022-07-09 18:26:55 +0900

    test(MemberRepositoryTest): @AutoConfigureTestDatabase 활성화
    - @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
    
    spring-jpa, hibernate-annotation

--- spring-jpa/hibernate-annotation/src/test/java/com/gmoon/hibernateannotation/domain/MemberRepositoryTest.java
+++ spring-jpa/hibernate-annotation/src/test/java/com/gmoon/hibernateannotation/domain/MemberRepositoryTest.java
@@ -8,11 +8,9 @@ import javax.persistence.EntityManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryTest {
 
 	@Autowired MemberRepository memberRepository;
