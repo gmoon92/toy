1cbf949df71d086555a760f966e351bb2b4d2ed4
gmoon92 <gmoon0929@gmail.com>
2022-07-19 23:14:04 +0900

    refactor(CustomAsyncUncaughtExceptionHandler): 비동기 에러 로그 레벨 수정
    spring-async

--- spring-event/spring-async/src/main/java/com/gmoon/springasync/handler/CustomAsyncUncaughtExceptionHandler.java
+++ spring-event/spring-async/src/main/java/com/gmoon/springasync/handler/CustomAsyncUncaughtExceptionHandler.java
@@ -1,19 +1,17 @@
 package com.gmoon.springasync.handler;
 
 import java.lang.reflect.Method;
-
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
 @Slf4j
 public class CustomAsyncUncaughtExceptionHandler implements AsyncUncaughtExceptionHandler {
 	@Override
 	public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-		log.info("Exception message - {}", ex.getMessage());
-		log.info("Method name: {}", method.getName());
+		log.error("Exception message - " + ex.getMessage(), ex);
+		log.error("Method name: {}", method.getName());
 		for (Object param : params) {
-			log.info("param: {}", param);
+			log.error("param: {}", param);
 		}
 	}
 }
