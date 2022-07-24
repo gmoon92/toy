2ef0a7b79bebfab4446c99cb2434a3ec22bd5ac9
gmoon92 <gmoon0929@gmail.com>
2022-07-24 23:15:14 +0900

    test: 서버 생성 조회 테스트 케이스 수정
    spring-quartz-cluster

--- spring-event/spring-quartz-cluster/src/test/java/com/gmoon/springquartzcluster/server/ServerRepositoryTest.java
+++ spring-event/spring-quartz-cluster/src/test/java/com/gmoon/springquartzcluster/server/ServerRepositoryTest.java
@@ -3,7 +3,6 @@ package com.gmoon.springquartzcluster.server;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.gmoon.springquartzcluster.model.WebServerSaveForm;
 import com.gmoon.springquartzcluster.test.BaseDataJpaTest;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -24,8 +23,8 @@ class ServerRepositoryTest extends BaseDataJpaTest {
 
 	@Order(Integer.MIN_VALUE)
 	@Test
-	@DisplayName("활성화된 웹 서버를 생성한다")
-	void testCreateEnabledWebServer() {
+	@DisplayName("활성화된 웹 서버를 생성 조회")
+	void testSaveAndGet() {
 		// given
 		String serverName = "gmoon-web";
 		WebServerSaveForm form = new WebServerSaveForm();
@@ -41,19 +40,6 @@ class ServerRepositoryTest extends BaseDataJpaTest {
 		// then
 		assertThat(webServer)
 			.isEqualTo(repository.findServerByName(serverName));
-	}
-
-	@Test
-	@DisplayName("QueryDsl로 서버들을 조회한다.")
-	void testFindAllByName() {
-		// given
-		String serverName = "gmoon";
-
-		// when
-		List<Server> servers = repository.getServers(serverName);
-
-		// then
-		assertThat(servers).size().isZero();
 	}
 
 	@Order(Integer.MAX_VALUE)
