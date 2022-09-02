7ae98448f208e0068b39004b61f22ed07547987f
gmoon92 <gmoon0929@gmail.com>
2022-09-02 18:12:40 +0900

    chore: init local stack project add
    testing, localstack

---
+++ testing/localstack/src/test/java/com/gmoon/localstack/LocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
