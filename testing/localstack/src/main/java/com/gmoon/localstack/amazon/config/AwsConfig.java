262b026725b7f0d1810bd49a6a37afef98f6cc77
gmoon92 <gmoon0929@gmail.com>
2022-09-03 19:38:03 +0900

    feat(AwsConfig): TransferManager 빈 설정 추가
    testing, localstack

--- testing/localstack/src/main/java/com/gmoon/localstack/amazon/config/AwsConfig.java
+++ testing/localstack/src/main/java/com/gmoon/localstack/amazon/config/AwsConfig.java
@@ -6,6 +6,8 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import com.gmoon.localstack.amazon.constants.AmazonRegion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -32,5 +34,13 @@ public class AwsConfig {
 	private AWSCredentialsProvider awsCredentialsProvider(String awsAccessKey, String awsSecretKey) {
 		AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
 		return new AWSStaticCredentialsProvider(credentials);
+	}
+
+	@Bean
+	public TransferManager transferManager(AmazonS3 amazonS3) {
+		return TransferManagerBuilder
+			.standard()
+			.withS3Client(amazonS3)
+			.build();
 	}
 }
