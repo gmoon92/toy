ff01a5586f52c02aeaaa8fa95a7e2e279906542a
gmoon92 <gmoon0929@gmail.com>
2022-09-03 19:37:41 +0900

    test(LocalStackTest): TransferManager 멀티 파트 파일 업로드 테스트 케이스 추가
    testing, localstack

--- testing/localstack/src/test/java/com/gmoon/localstack/test/LocalStackTest.java
+++ testing/localstack/src/test/java/com/gmoon/localstack/test/LocalStackTest.java
@@ -1,6 +1,7 @@
 package com.gmoon.localstack.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import cloud.localstack.ServiceName;
 import cloud.localstack.awssdkv1.TestUtils;
 import cloud.localstack.docker.LocalstackDockerExtension;
@@ -8,6 +9,10 @@ import cloud.localstack.docker.annotation.LocalstackDockerProperties;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.transfer.Transfer;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 import com.gmoon.javacore.util.FileUtils;
 import java.io.File;
 import java.util.List;
@@ -74,6 +79,35 @@ class LocalStackTest {
 			File actual = FileUtils.convertInputStreamToFile(is);
 			assertThat(FileUtils.convertFileToString(actual))
 				.contains("github");
+		}
+
+		@DisplayName("TransferManager 사용, S3에 지정된 버킷으로 파일을 업로드한다.")
+		@Test
+		void uploadFileWithTransferManager() {
+			// given
+			TransferManager transferManager = TransferManagerBuilder
+				.standard()
+				.withS3Client(s3Client)
+				.build();
+
+			String fileName = "s3/github.txt";
+
+			File file = FileUtils.getResourceFile(fileName);
+			String key = UUID.randomUUID().toString();
+
+			Upload upload = transferManager.upload(
+				BUCKET_NAME,
+				key,
+				file);
+			log.debug("Object upload started");
+
+			// Optionally, wait for the upload to finish before continuing.
+			assertThatCode(() -> {
+				upload.waitForCompletion();
+				log.debug("Object upload complete");
+				assertThat(upload.getState())
+					.isEqualTo(Transfer.TransferState.Completed);
+			}).doesNotThrowAnyException();
 		}
 	}
 }
