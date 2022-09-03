0c0acc8bd237df0c98aef5003c689a4c6d7e7db0
gmoon92 <gmoon0929@gmail.com>
2022-09-03 23:10:00 +0900

    feat(AmazonS3Service): AWS S3 파일 업로드 기능 구현
    testing, localstack

---
+++ testing/localstack/src/main/java/com/gmoon/localstack/aws/AmazonS3Service.java
@@ -0,0 +1,52 @@
+package com.gmoon.localstack.aws;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.gmoon.javacore.util.FileUtils;
+import com.gmoon.localstack.aws.config.AwsProperty;
+import com.gmoon.localstack.aws.model.S3DownloadLink;
+import com.gmoon.localstack.aws.model.S3RequestVO;
+import com.gmoon.localstack.aws.model.S3ResponseVO;
+import java.io.File;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class AmazonS3Service {
+
+	private final AwsProperty awsProperty;
+	private final TransferManager transferManager;
+
+	public S3ResponseVO upload(S3RequestVO requestVO) {
+		File file = uploadFileToAmazonS3(requestVO);
+
+		S3DownloadLink downloadLink = S3DownloadLink.builder()
+			.region(awsProperty.s3.region)
+			.userResourceUri(requestVO.getKey())
+			.fileName(file.getName())
+			.build();
+
+		return S3ResponseVO.ok(downloadLink.getValue());
+	}
+
+	private File uploadFileToAmazonS3(S3RequestVO requestVO) {
+		MultipartFile multipartFile = requestVO.getMultipartFile();
+		File file = FileUtils.convertFileToMultipartFile(multipartFile);
+
+		transferManager.upload(
+			requestVO.getBucketName(),
+			requestVO.getKey(),
+			file
+		);
+		return file;
+	}
+
+	private ObjectMetadata getObjectMetadata(MultipartFile multipartFile) {
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentType(multipartFile.getContentType());
+		metadata.setContentLength(multipartFile.getSize());
+		return metadata;
+	}
+}
