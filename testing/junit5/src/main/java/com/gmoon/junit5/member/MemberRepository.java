2aa8bfd87dda42eb97fb751ac3b7e19602857287
gmoon92 <gmoon0929@gmail.com>
2022-09-05 03:45:13 +0900

    feat(MemberRepository): @Repository bean add
    testing, junit5

--- testing/junit5/src/main/java/com/gmoon/junit5/member/MemberRepository.java
+++ testing/junit5/src/main/java/com/gmoon/junit5/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.gmoon.junit5.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
