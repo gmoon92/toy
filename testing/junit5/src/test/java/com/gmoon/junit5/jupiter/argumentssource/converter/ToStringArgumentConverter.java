e4e15890a5fac1b18c48ef052cd9ea149ca19286
gmoon92 <gmoon0929@gmail.com>
2022-09-05 01:27:52 +0900

    test(ToStringArgumentConverter): 커스텀 인수 컨버터 추가
    testing, junit5

---
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/converter/ToStringArgumentConverter.java
@@ -0,0 +1,17 @@
+package com.gmoon.junit5.jupiter.argumentssource.converter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+public class ToStringArgumentConverter extends SimpleArgumentConverter {
+
+	@Override
+	protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+		assertEquals(String.class, targetType, "Can only convert to String");
+		if (source instanceof Enum<?>) {
+			return ((Enum<?>) source).name();
+		}
+		return String.valueOf(source);
+	}
+}
