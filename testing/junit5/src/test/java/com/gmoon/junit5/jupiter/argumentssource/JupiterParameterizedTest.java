18cc24fb7962f611cb6e95094c9ec267ae408795
gmoon92 <gmoon0929@gmail.com>
2022-09-04 23:35:37 +0900

    test(JupiterParameterizedTest): @NullAndEmptySource, @ValueSource 학습 테스트 추가
    testing, junit5

--- testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/JupiterParameterizedTest.java
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/JupiterParameterizedTest.java
@@ -1,28 +1,41 @@
 package com.gmoon.junit5.jupiter.argumentssource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
+import static org.mockito.Mockito.when;
 import com.gmoon.junit5.jupiter.argumentssource.contants.Role;
+import com.gmoon.junit5.member.MemberRepository;
+import com.gmoon.junit5.member.MemberService;
+import com.gmoon.junit5.member.SystemException;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
- * {@link org.junit.jupiter.params.ParameterizedTestExtension}
  * {@link org.junit.jupiter.params.provider.ArgumentsSource}
  * {@link org.junit.jupiter.params.provider.ArgumentsProvider}
  * <p>
- * {@link org.junit.jupiter.params.provider.NullAndEmptySource}
  * {@link org.junit.jupiter.params.provider.ValueSource}
  * {@link org.junit.jupiter.params.provider.MethodSource}
  */
+@ExtendWith(MockitoExtension.class)
 class JupiterParameterizedTest {
 
 	@DisplayName("사원 CSV 파일 데이터 검증")
@@ -106,6 +119,47 @@ class JupiterParameterizedTest {
 		@EmptySource
 		void case4(Integer[] actual) {
 			assertThat(actual).isEmpty();
+		}
+	}
+
+	@DisplayName("null 또는 빈 문자열")
+	@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+	@NullAndEmptySource
+	void nullAndEmptySource(String actual) {
+		assertThat(StringUtils.isBlank(actual)).isTrue();
+	}
+
+	@DisplayName("값 인수 테스트")
+	@Nested
+	class ValueSourceTest {
+
+		@InjectMocks
+		MemberService memberService;
+
+		@Mock
+		MemberRepository memberRepository;
+
+		@DisplayName("값 인수 테스트")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@ValueSource(ints = {1, 2, 3, 4})
+		void case1(int actual) {
+			assertThat(actual > 0 && actual < 4).isTrue();
+		}
+
+		@DisplayName("예외 클래스 테스트")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@ValueSource(classes = { IllegalArgumentException.class, EntityNotFoundException.class, PersistenceException.class })
+		void case2(Class<? extends RuntimeException> exception) {
+			// given
+			Long memberId = 0L;
+
+			// when
+			when(memberRepository.findById(memberId))
+				.thenThrow(exception);
+
+			// then
+			assertThatThrownBy(() -> memberService.disable(memberId))
+				.isInstanceOf(SystemException.class);
 		}
 	}
 }
