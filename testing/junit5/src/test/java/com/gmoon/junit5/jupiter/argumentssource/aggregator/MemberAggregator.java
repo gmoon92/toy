237e29a733435c541e64126692ca73049d4ad583
gmoon92 <gmoon0929@gmail.com>
2022-09-05 01:27:04 +0900

    test(MemberAggregator): 커스텀 ArgumentsAggregator 추가
    testing, junit5

---
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/aggregator/MemberAggregator.java
@@ -0,0 +1,20 @@
+package com.gmoon.junit5.jupiter.argumentssource.aggregator;
+
+import com.gmoon.junit5.member.Member;
+import com.gmoon.junit5.member.Role;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+
+public class MemberAggregator implements ArgumentsAggregator {
+
+	@Override
+	public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+		Member member = new Member();
+		member.setName(accessor.getString(0));
+		member.setRole(accessor.get(1, Role.class));
+		member.setEnabled(accessor.getBoolean(2));
+		return member;
+	}
+}
