b2eaed5e933c9469d59aef88024e4bf4754dde0c
gmoon92 <gmoon0929@gmail.com>
2022-09-04 22:53:50 +0900

    test(JupiterParameterizedTest): @EmptySource, @NullSource, @EnumSource 학습 테스트 추가
    testing, junit5

--- testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/JupiterParameterizedTest.java
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/JupiterParameterizedTest.java
@@ -2,25 +2,28 @@ package com.gmoon.junit5.jupiter.argumentssource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
+import com.gmoon.junit5.jupiter.argumentssource.contants.Role;
+import java.util.List;
+import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 /**
  * {@link org.junit.jupiter.params.ParameterizedTestExtension}
  * {@link org.junit.jupiter.params.provider.ArgumentsSource}
  * {@link org.junit.jupiter.params.provider.ArgumentsProvider}
  * <p>
- * {@link org.junit.jupiter.params.provider.CsvFileSource}
- * {@link org.junit.jupiter.params.provider.CsvSource}
- * {@link org.junit.jupiter.params.provider.EmptySource}
- * {@link org.junit.jupiter.params.provider.EnumSource}
- * {@link org.junit.jupiter.params.provider.MethodSource}
- * {@link org.junit.jupiter.params.provider.NullSource}
+ * {@link org.junit.jupiter.params.provider.NullAndEmptySource}
  * {@link org.junit.jupiter.params.provider.ValueSource}
+ * {@link org.junit.jupiter.params.provider.MethodSource}
  */
-public class JupiterParameterizedTest {
+class JupiterParameterizedTest {
 
 	@DisplayName("사원 CSV 파일 데이터 검증")
 	@ParameterizedTest(name = "[{index}] {0}({1})... arguments: {arguments}")
@@ -52,5 +55,57 @@ public class JupiterParameterizedTest {
 			() -> assertThat(userName)
 				.containsAnyOf("guest", "super, moon")
 		);
+	}
+
+	@DisplayName("이넘 클래스 인수 테스트")
+	@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+	@EnumSource(Role.class)
+	void enumSource(Role role) {
+		// given
+		int value = role.getValue();
+
+		// when then
+		assertThat(Role.ALL.get(value))
+			.isEqualTo(role);
+	}
+
+	@DisplayName("Null 인수 테스트")
+	@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+	@NullSource
+	void nullSource(String actual) {
+		assertThat(Objects.isNull(actual)).isTrue();
+	}
+
+	@DisplayName("empty 인수 테스트")
+	@Nested
+	class EmptySourceTest {
+
+		@DisplayName("빈 문자열")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@EmptySource
+		void case1(String actual) {
+			assertThat(actual).isBlank();
+		}
+
+		@DisplayName("빈 리스트")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@EmptySource
+		void case2(List<String> actual) {
+			assertThat(actual).isEmpty();
+		}
+
+		@DisplayName("빈 원시 타입 배열")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@EmptySource
+		void case3(byte[] actual) {
+			assertThat(actual).isEmpty();
+		}
+
+		@DisplayName("빈 배열")
+		@ParameterizedTest(name = "{displayName}[{index}] - {argumentsWithNames}")
+		@EmptySource
+		void case4(Integer[] actual) {
+			assertThat(actual).isEmpty();
+		}
 	}
 }
---
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/contants/Role.java
@@ -0,0 +1,26 @@
+package com.gmoon.junit5.jupiter.argumentssource.contants;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+	ADMIN(0),
+	MANAGER(1),
+	USER(2);
+
+	private final int value;
+
+	public static final Map<Integer, Role> ALL = Stream.of(values())
+		.collect(collectingAndThen(
+			toMap(Role::getValue, Function.identity()),
+			Collections::unmodifiableMap
+		));
+}
