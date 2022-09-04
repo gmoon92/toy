20e295e112593b6f29669553158d5a52c35fc4d9
gmoon92 <gmoon0929@gmail.com>
2022-09-05 02:45:16 +0900

    feat(NegativeNumbers): 커스텀 음수 인수 테스트 어노테이션 추가
    testing, junit5

---
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/annotation/NegativeNumbers.java
@@ -0,0 +1,20 @@
+package com.gmoon.junit5.jupiter.argumentssource.annotation;
+
+import com.gmoon.junit5.jupiter.argumentssource.provider.NegativeNumbersProvider;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ArgumentsSource(NegativeNumbersProvider.class)
+public @interface NegativeNumbers {
+
+	int start() default -10;
+
+	int end() default -1;
+}
---
+++ testing/junit5/src/test/java/com/gmoon/junit5/jupiter/argumentssource/provider/NegativeNumbersProvider.java
@@ -0,0 +1,36 @@
+package com.gmoon.junit5.jupiter.argumentssource.provider;
+
+import com.gmoon.junit5.jupiter.argumentssource.annotation.NegativeNumbers;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.support.AnnotationConsumer;
+import org.junit.platform.commons.util.Preconditions;
+
+public class NegativeNumbersProvider implements ArgumentsProvider, AnnotationConsumer<NegativeNumbers> {
+
+	private NegativeNumbers negativeNumbers;
+
+	@Override
+	public void accept(NegativeNumbers negativeNumbers) {
+		this.negativeNumbers = negativeNumbers;
+	}
+
+	@Override
+	public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+		int startInclusive = negativeNumbers.start();
+		int endInclusive = negativeNumbers.end();
+		Preconditions.condition(isNegativeNumbers(startInclusive), String.format("The starting(%d) value must be negative.", startInclusive));
+		Preconditions.condition(isNegativeNumbers(endInclusive), String.format("The end(%d) value must be negative.", endInclusive));
+		Preconditions.condition(startInclusive < endInclusive, "start must be less than end.");
+
+		return IntStream.rangeClosed(startInclusive, endInclusive)
+			.mapToObj(Arguments::of);
+	}
+
+	private boolean isNegativeNumbers(int number) {
+		return number < 0;
+	}
+}
