3e51184695d42e8d3f92573a04e773fc7f245f51
gmoon92 <gmoon0929@gmail.com>
2022-09-09 17:11:41 +0900

    feat(ReflectionUtils): 리플렉션 유틸 클래스 추가
    java-core

---
+++ java-core/src/main/java/com/gmoon/javacore/util/ReflectionUtils.java
@@ -0,0 +1,65 @@
+package com.gmoon.javacore.util;
+
+import static java.util.stream.Collectors.toList;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.reflections.Reflections;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ReflectionUtils {
+
+	private static class ReflectionsHolder {
+
+		private static final Reflections INSTANCE = new Reflections();
+	}
+
+	public static List<Object> getFieldValues(Object object, List<Field> fields) {
+		return fields.stream()
+			.map(field -> getFieldValue(object, field))
+			.collect(toList());
+	}
+
+	private static Object getFieldValue(Object object, Field field) {
+		try {
+			field.setAccessible(true);
+			return field.get(object);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Set<Class<?>> getDeclaredAnnotationClasses(final Class<? extends Annotation> annotation) {
+		return ReflectionsHolder.INSTANCE.getTypesAnnotatedWith(annotation);
+	}
+
+	public static List<Field> getDeclaredAnnotationFields(final Class<?> clazz,
+														  final Class<? extends Annotation>... annotation) {
+		return Stream.of(clazz.getDeclaredFields())
+			.filter(field -> existsDeclaredAnnotation(field, annotation))
+			.collect(toList());
+	}
+
+	public static List<Field> getDeclaredAnnotationFieldsByUsingRecursive(final Class<?> clazz,
+																		  final Class<? extends Annotation>... annotation) {
+		List<Field> fields = getDeclaredAnnotationFields(clazz, annotation);
+		if (CollectionUtils.isEmpty(fields)) {
+			Class<?> superclass = clazz.getSuperclass();
+			if (superclass != null) {
+				return getDeclaredAnnotationFields(superclass, annotation);
+			}
+		}
+
+		return fields;
+	}
+
+	public static boolean existsDeclaredAnnotation(final Field field,
+												   final Class<? extends Annotation>... annotations) {
+		return Stream.of(annotations)
+			.anyMatch(field::isAnnotationPresent);
+	}
+}
---
+++ java-core/src/test/java/com/gmoon/javacore/util/ReflectionUtilsTest.java
@@ -0,0 +1,87 @@
+package com.gmoon.javacore.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.gmoon.javacore.persistence.EmbeddedId;
+import com.gmoon.javacore.persistence.Entity;
+import com.gmoon.javacore.persistence.Id;
+import com.gmoon.javacore.test.domain.Favorites;
+import com.gmoon.javacore.test.domain.User;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class ReflectionUtilsTest {
+
+	@DisplayName("어노테이션이 선언된 클래스 목록 조회")
+	@Test
+	void getDeclaredAnnotationClasses() {
+		// given
+		Class<Entity> annotation = Entity.class;
+
+		// when then
+		assertThat(ReflectionUtils.getDeclaredAnnotationClasses(annotation))
+			.isInstanceOf(Set.class)
+			.contains(User.class, Favorites.class);
+	}
+
+	@DisplayName("지정된 어노테이션이 선언된 필드 목록 조회")
+	@Test
+	void getFieldsByDeclaredAnnotation() {
+		// given
+		Class<?> entity = Favorites.class;
+		Class<? extends Annotation>[] annotations = new Class[] { Id.class, EmbeddedId.class };
+
+		// when
+		List<Field> ids = ReflectionUtils.getDeclaredAnnotationFields(entity, annotations);
+
+		// then
+		assertThat(ids)
+			.isNotEmpty()
+			.allMatch(
+				field -> Stream.of(annotations)
+					.anyMatch(clazz -> field.getAnnotation(clazz) != null)
+			);
+	}
+
+	@DisplayName("현 클래스에 선언된 애노테이션 필드가 없다면" +
+		"재귀 방식으로 상위 클래스 필드 조회")
+	@Test
+	void getDeclaredAnnotationFieldsByUsingRecursive() {
+		// given
+		Class<?> entity = User.class;
+		Class<? extends Annotation>[] annotations = new Class[] { Id.class, EmbeddedId.class };
+
+		// when
+		List<Field> fields = ReflectionUtils.getDeclaredAnnotationFieldsByUsingRecursive(entity, annotations);
+
+		// then
+		assertThat(fields)
+			.isNotEmpty()
+			.map(field -> ReflectionUtils.existsDeclaredAnnotation(field, annotations))
+			.containsExactly(Boolean.TRUE);
+	}
+
+	@DisplayName("지정된 private 필드 값 검증")
+	@Test
+	void getFieldValues() {
+		// given
+		Class<?> entity = User.class;
+		Class<? extends Annotation>[] annotations = new Class[] { Id.class, EmbeddedId.class };
+
+		List<Field> fields = ReflectionUtils.getDeclaredAnnotationFieldsByUsingRecursive(entity, annotations);
+
+		// when
+		UUID id = UUID.randomUUID();
+		User user = new User(id);
+		List<Object> values = ReflectionUtils.getFieldValues(user, fields);
+
+		// then
+		assertThat(values).contains(id);
+	}
+}
