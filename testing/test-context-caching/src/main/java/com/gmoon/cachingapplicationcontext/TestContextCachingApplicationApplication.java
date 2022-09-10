37f7cf9695230eda07820b80204b719472d36f97
gmoon92 <gmoon0929@gmail.com>
2022-09-10 14:06:04 +0900

    chore: Spring Integration test context caching project module rename
    testing, test-context-caching

--- testing/test-context-caching/src/main/java/com/gmoon/cachingapplicationcontext/TestContextCachingApplicationApplication.java
+++ testing/test-context-caching/src/main/java/com/gmoon/cachingapplicationcontext/TestContextCachingApplicationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CachingApplicationContextApplication {
+public class TestContextCachingApplicationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CachingApplicationContextApplication.class, args);
+		SpringApplication.run(TestContextCachingApplicationApplication.class, args);
 	}
 
 }
