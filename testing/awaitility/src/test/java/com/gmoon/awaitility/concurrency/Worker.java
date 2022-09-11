e3d133df539bed5244dba9e6e884a7320fb36367
gmoon92 <gmoon0929@gmail.com>
2022-09-11 22:39:50 +0900

    feat(Worker): 멀티 쓰레드 테스트를 위한 Worker 구현
    testing, awaitility

---
+++ testing/awaitility/src/test/java/com/gmoon/awaitility/concurrency/Worker.java
@@ -0,0 +1,30 @@
+package com.gmoon.awaitility.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor
+public class Worker implements Runnable {
+
+	private CountDownLatch latch;
+	private Runnable executable;
+
+	public Worker(CountDownLatch latch, Runnable executable) {
+		this.latch = latch;
+		this.executable = executable;
+	}
+
+	@Override
+	public void run() {
+		executable.run();
+		countDown();
+	}
+
+	private void countDown() {
+		latch.countDown();
+		Thread thread = Thread.currentThread();
+		log.info("[{}] tid: {}", thread.getName(), thread.getId());
+	}
+}
