32ca373fc58e1bd5353d611bc50561a1cb8392e2
gmoon92 <gmoon0929@gmail.com>
2022-09-11 16:22:58 +0900

    chore(awaitility): awaitility project init
    testing, awaitility

---
+++ testing/awaitility/src/main/java/com/gmoon/awaitility/AwaitilityApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.awaitility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwaitilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwaitilityApplication.class, args);
+	}
+
+}
---
+++ testing/awaitility/src/test/java/com/gmoon/awaitility/AwaitilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.awaitility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwaitilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
