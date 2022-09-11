cb8e4b3d20a69b8f4c8c5138a98d874491b14e48
gmoon92 <gmoon0929@gmail.com>
2022-09-11 23:36:01 +0900

    test(EventServiceTest): awaitility 비동기 테스트 케이스 추가
    testing, awaitility

--- testing/awaitility/src/test/java/com/gmoon/awaitility/event/EventServiceTest.java
+++ testing/awaitility/src/test/java/com/gmoon/awaitility/event/EventServiceTest.java
@@ -1,7 +1,9 @@
-package com.gmoon.awaitility.service;
+package com.gmoon.awaitility.event;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.gmoon.awaitility.concurrency.Worker;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -9,6 +11,10 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionTimeoutException;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -21,6 +27,64 @@ class EventServiceTest {
 
 	@Autowired
 	EventService eventService;
+
+	@Autowired
+	EventRepository eventRepository;
+
+	@DisplayName("비동기 검증, Awaitility 활용")
+	@Nested
+	class AwaitilityTest {
+
+		@BeforeEach
+		void setUp() {
+			eventRepository.clear();
+
+			Event saveEvent = new Event("hello gmoon.");
+			// 저장 시간 2초
+			eventService.save(saveEvent);
+		}
+
+		@DisplayName("lambda 표현식")
+		@Test
+		void case1() {
+			// then
+			Awaitility.await("lambda 표현식")
+				.pollDelay(Duration.ofSeconds(1)) // 1초 대기
+				.pollInterval(Duration.ofSeconds(1)) // 1초 마다 확인
+				.atMost(Duration.ofSeconds(5)) // 최대 대기 시간 설정, default 10 초
+				// assertion
+				// .until(() -> eventRepository.size() == 1)
+				.until(eventRepository::size, CoreMatchers.equalTo(1));
+		}
+
+		@DisplayName("AssertJ 단언문 사용")
+		@Test
+		void withAssertJ() {
+			// then
+			Awaitility.await("AssertJ 단언문 사용")
+				.pollDelay(Duration.ofSeconds(1)) // 1초 대기
+				.pollInterval(Duration.ofSeconds(1)) // 1초 마다 확인
+				.atMost(Duration.ofSeconds(5)) // 최대 대기 시간 설정, default 10 초
+				.untilAsserted(
+					() -> assertThat(eventRepository.size()).isNotZero()
+				);
+		}
+
+		@DisplayName("Timeout 예외")
+		@Test
+		void error1() {
+			// then
+			assertThatThrownBy(() ->
+					Awaitility.await("Timeout 예외")
+						.atMost(Duration.ofMillis(200)) // 최대 대기 시간 설정, default 10 초
+//					.timeout(Duration.ofMillis(200))
+						.untilAsserted(() ->
+							Assertions.assertThat(eventRepository.size())
+								.isNotZero()
+						)
+			).isInstanceOf(ConditionTimeoutException.class);
+		}
+	}
 
 	@DisplayName("비동기 검증, CountDownLatch 활용")
 	@Nested
