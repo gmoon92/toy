acc756378d5cddfb77c2fa0d86e8b29301f900b1
gmoon92 <gmoon0929@gmail.com>
2022-09-23 20:37:18 +0900

    chore(spring-jpa-specs): spring-jpa-specs project init
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/SpringJpaSpecsApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjpaspecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSpecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSpecsApplication.class, args);
+	}
+
+}
---
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/SpringJpaSpecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjpaspecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaSpecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
