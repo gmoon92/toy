73614febf9db18049592c726967781184dd3c336
gmoon92 <gmoon0929@gmail.com>
2022-09-24 17:11:56 +0900

    feat(BookPrice): 가격 밸류 추가
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/book/domain/BookPrice.java
@@ -0,0 +1,37 @@
+package com.gmoon.springjpaspecs.book.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
+public class BookPrice implements Serializable {
+
+	@Column(name = "price")
+	private BigDecimal value;
+
+	public BookPrice(BigDecimal value) {
+		validate(value);
+		this.value = value;
+	}
+
+	private void validate(BigDecimal value) {
+		if (Objects.isNull(value)) {
+			throw new IllegalArgumentException();
+		}
+
+		boolean isLessThenZero = value.compareTo(BigDecimal.ZERO) == -1;
+		if (isLessThenZero) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
---
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/book/domain/BookPriceTest.java
@@ -0,0 +1,27 @@
+package com.gmoon.springjpaspecs.book.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BookPriceTest {
+
+	@Test
+	void create() {
+		assertThatCode(() -> new BookPrice(BigDecimal.ZERO))
+			.doesNotThrowAnyException();
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = "-1")
+	@NullSource
+	void error(BigDecimal price) {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> new BookPrice(price));
+	}
+}
