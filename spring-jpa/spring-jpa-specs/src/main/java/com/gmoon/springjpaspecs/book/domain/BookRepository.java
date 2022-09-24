8dbb058d8704716fa392a7dcd9aa16943517cdcd
gmoon92 <gmoon0929@gmail.com>
2022-09-24 21:01:42 +0900

    feat(BookRepository): 도메인 레파지토리 DIP 적용
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/book/domain/BookRepository.java
@@ -0,0 +1,16 @@
+package com.gmoon.springjpaspecs.book.domain;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookRepository {
+
+	Book save(Book book);
+
+	List<Book> findAll();
+
+	Optional<Book> findById(final UUID id);
+
+	List<Book> findAllById(final List<UUID> ids);
+}
--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/book/infra/BookJpaRepository.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/book/infra/BookJpaRepository.java
@@ -1,9 +1,10 @@
 package com.gmoon.springjpaspecs.book.infra;
 
+import com.gmoon.springjpaspecs.book.domain.BookRepository;
 import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gmoon.springjpaspecs.book.domain.Book;
 
-public interface BookJpaRepository extends JpaRepository<Book, Serializable> {
+public interface BookJpaRepository extends BookRepository, JpaRepository<Book, Serializable> {
 }
