ab3d41cf69b2749b3e40ed1b2e558cac4bc1e51b
gmoon92 <gmoon0929@gmail.com>
2022-09-25 03:52:57 +0900

    feat(BookType): 책 유형, 진열 상태 이넘 추가
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/vo/BookStatus.java
@@ -0,0 +1,6 @@
+package com.gmoon.springjpaspecs.books.bookstore.domain.vo;
+
+public enum BookStatus {
+	DISPLAY,
+	HIDDEN
+}
---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/vo/BookType.java
@@ -0,0 +1,10 @@
+package com.gmoon.springjpaspecs.books.bookstore.domain.vo;
+
+public enum BookType {
+	TEENAGER,
+	MAGAZINE,
+	COMICS,
+	NOVEL,
+	ESSAY,
+	ETC
+}
