246d3f1c4de531db3da88ccc475590fd2288d9e7
gmoon92 <gmoon0929@gmail.com>
2022-09-24 21:02:58 +0900

    style(BookJpaRepositoryTest): 코드 컨벤션
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/book/infra/BookJpaRepositoryTest.java
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/book/infra/BookJpaRepositoryTest.java
@@ -2,12 +2,11 @@ package com.gmoon.springjpaspecs.book.infra;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.gmoon.springjpaspecs.book.domain.Book;
 import com.gmoon.springjpaspecs.book.domain.BookName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.gmoon.springjpaspecs.book.domain.Book;
 
 @DataJpaTest
 class BookJpaRepositoryTest {
