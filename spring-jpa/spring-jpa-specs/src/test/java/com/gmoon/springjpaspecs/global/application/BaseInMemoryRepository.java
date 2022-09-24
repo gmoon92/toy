2b3442969e0a1fa6addb6bf7170be791d773ca38
gmoon92 <gmoon0929@gmail.com>
2022-09-24 21:03:33 +0900

    feat(test): BaseInMemoryRepository fake object add
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/global/application/BaseInMemoryRepository.java
@@ -0,0 +1,84 @@
+package com.gmoon.springjpaspecs.global.application;
+
+import static java.util.stream.Collectors.toList;
+
+import com.gmoon.springjpaspecs.global.BaseEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public abstract class BaseInMemoryRepository<T extends BaseEntity, ID extends Serializable> {
+
+	private final Map<ID, T> CACHE = new HashMap<>();
+
+	public T save(T entity) {
+		CACHE.put((ID) entity.getId(), entity);
+		return entity;
+	}
+
+	public List<T> saveAll(Iterable<T> entities) {
+		for (T entity : entities) {
+			save(entity);
+		}
+
+		return new ArrayList<>(CACHE.values());
+	}
+
+	public Optional<T> findById(ID id) {
+		T entity = CACHE.get(id);
+		return Optional.ofNullable(entity);
+	}
+
+	public boolean existsById(ID id) {
+		return !Objects.isNull(CACHE.get(id));
+	}
+
+	public long count() {
+		return CACHE.size();
+	}
+
+	public List<T> findAll() {
+		return new ArrayList<>(CACHE.values());
+	}
+
+	public List<T> findAllById(List<ID> ids) {
+		return ids.stream()
+			.filter(this::existsById)
+			.map(this::findById)
+			.map(Optional::get)
+			.collect(toList());
+	}
+
+	public T getReferenceById(ID id) {
+		return CACHE.get(id);
+	}
+
+	public void deleteById(ID id) {
+		CACHE.remove(id);
+	}
+
+	public void delete(T entity) {
+		Serializable id = entity.getId();
+		CACHE.remove(id);
+	}
+
+	public void deleteAllById(Iterable<? extends ID> ids) {
+		for (ID id : ids) {
+			deleteById(id);
+		}
+	}
+
+	public void deleteAll(Iterable<? extends T> entities) {
+		for (T entity : entities) {
+			CACHE.remove(entities);
+		}
+	}
+
+	public void deleteAll() {
+		CACHE.clear();
+	}
+}
