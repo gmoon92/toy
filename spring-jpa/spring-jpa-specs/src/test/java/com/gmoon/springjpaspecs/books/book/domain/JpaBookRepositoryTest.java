d9b5620a078d5301fcbdf70c67080ccb14ae01e7
gmoon92 <gmoon0929@gmail.com>
2022-09-26 11:41:43 +0900

    test(JpaBookRepositoryTest): findById test case add
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/books/book/domain/JpaBookRepositoryTest.java
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/books/book/domain/JpaBookRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.gmoon.springjpaspecs.books.book.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.gmoon.springjpaspecs.books.book.domain.vo.BookName;
+import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -38,8 +39,21 @@ class JpaBookRepositoryTest {
 	@Test
 	void ambiguousReference() {
 		repository.save(new Book());
-		repository.findById(UUID.randomUUID());
-		repository.findAllById(Arrays.asList(UUID.randomUUID()));
+		repository.findById("book-no0");
+		repository.findAllById(Arrays.asList("book-no0"));
 		repository.findAll();
+	}
+
+	@Test
+	void findById() {
+		Book book = repository.findById("book-no0").get();
+
+		assertThat(book).isNotNull();
+		assertAll(
+			() -> assertThat(book.getId()).isNotBlank(),
+			() -> assertThat(book.getName()).isEqualTo(new BookName("START! DDD")),
+			() -> assertThat(book.getIsbn()).isNotNull(),
+			() -> assertThat(book.getPrice().getValue().setScale(2)).isEqualTo(
+				BigDecimal.valueOf(36_000).setScale(2)));
 	}
 }
