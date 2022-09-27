f6118b2beb1c9ade0c2aff2c4b5d9c5c096e66c4
gmoon92 <gmoon0929@gmail.com>
2022-09-27 22:17:04 +0900

    feat: Fixtures 추가
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/test/java/com/gmoon/springjpaspecs/books/Fixtures.java
@@ -0,0 +1,19 @@
+package com.gmoon.springjpaspecs.books;
+
+import com.gmoon.springjpaspecs.books.book.domain.Book;
+import com.gmoon.springjpaspecs.books.book.domain.vo.BookName;
+import com.gmoon.springjpaspecs.books.book.domain.vo.BookPrice;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Fixtures {
+
+	public static Book book(String bookName, BigDecimal bookPrice) {
+		return Book.builder()
+			.name(new BookName(bookName))
+			.price(new BookPrice(bookPrice))
+			.build();
+	}
+}
