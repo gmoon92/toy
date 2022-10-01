9a5420aa5424303ba19e960b3fc434648d846d48
gmoon92 <gmoon0929@gmail.com>
2022-10-02 01:17:25 +0900

    chore: spring-web project init
    spring-web, spring-web-converter

---
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/SpringWebConverterApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springwebconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebConverterApplication.class, args);
+	}
+
+}
---
+++ spring-mvc/spring-web-converter/src/test/java/com/gmoon/springwebconverter/SpringWebConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springwebconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
