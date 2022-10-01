475bf6b4a7180862ea755b90166db6f3fb8bb033
gmoon92 <gmoon0929@gmail.com>
2022-10-02 02:18:54 +0900

    feat(model): 이넘 클래스 추가
    spring-web, spring-web-converter

---
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/model/PaymentType.java
@@ -0,0 +1,17 @@
+package com.gmoon.springwebconverter.model;
+
+import com.gmoon.springwebconverter.config.converter.EnumStringParamProvider;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum PaymentType implements EnumStringParamProvider {
+
+	KAKAO_BANK("0001"),
+	NAVER_PAY("0002"),
+	TOSS("0003");
+
+	private final String value;
+}
---
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/model/SearchType.java
@@ -0,0 +1,33 @@
+package com.gmoon.springwebconverter.model;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.gmoon.springwebconverter.config.converter.EnumStringParamProvider;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SearchType implements EnumStringParamProvider {
+
+	DATE("date"),
+	USER_NAME("username");
+
+	private final String value;
+
+	private static final Map<String, SearchType> ALL = Arrays.stream(values())
+		.collect(collectingAndThen(
+			toMap(SearchType::getValue, Function.identity()),
+			Collections::unmodifiableMap
+		));
+
+	public static SearchType from(String value) {
+		return ALL.get(value);
+	}
+}
