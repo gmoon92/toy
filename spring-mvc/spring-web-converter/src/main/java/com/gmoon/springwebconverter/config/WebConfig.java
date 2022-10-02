54f63f738a55e8fde2eba8f8002fd0c9cc3efe01
gmoon92 <gmoon0929@gmail.com>
2022-10-02 23:48:49 +0900

    feat(WebConfig): ConverterFactory 설정 추가
    spring-mvc, spring-web-converter

--- spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/config/WebConfig.java
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/config/WebConfig.java
@@ -1,16 +1,19 @@
 package com.gmoon.springwebconverter.config;
 
+import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.gmoon.springwebconverter.config.converter.SearchTypeConverter;
+import com.gmoon.springwebconverter.config.converter.EnumConverterFactory;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new SearchTypeConverter());
+		ApplicationConversionService.configure(registry);
+		registry.removeConvertible(String.class, Enum.class);
+		registry.addConverterFactory(new EnumConverterFactory());
 	}
 }
