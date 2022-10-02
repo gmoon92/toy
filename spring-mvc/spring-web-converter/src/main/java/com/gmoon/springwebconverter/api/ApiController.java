b28508101306912e1825ccd45b8fb590942dc7ac
gmoon92 <gmoon0929@gmail.com>
2022-10-02 23:48:22 +0900

    refactor(ApiController): 결제 이넘 타입 추가
    spring-mvc, spring-web-converter

--- spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/api/ApiController.java
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/api/ApiController.java
@@ -5,14 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gmoon.springwebconverter.model.PaymentType;
 import com.gmoon.springwebconverter.model.SearchType;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-	@GetMapping
+	@GetMapping("/searchType")
 	public ResponseEntity<SearchType> searchType(SearchType searchType) {
 		return ResponseEntity.ok(searchType);
+	}
+
+	@GetMapping("/paymentType")
+	public ResponseEntity<PaymentType> paymentType(PaymentType paymentType) {
+		return ResponseEntity.ok(paymentType);
 	}
 }
--- spring-mvc/spring-web-converter/src/test/java/com/gmoon/springwebconverter/api/ApiControllerTest.java
+++ spring-mvc/spring-web-converter/src/test/java/com/gmoon/springwebconverter/api/ApiControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import com.gmoon.springwebconverter.model.PaymentType;
 import com.gmoon.springwebconverter.model.SearchType;
 
 @SpringBootTest
@@ -40,7 +41,7 @@ class ApiControllerTest {
 	@ParameterizedTest
 	@EnumSource(SearchType.class)
 	void convertStringToEnum(SearchType searchType) throws Exception {
-		ResultActions result = mockMvc.perform(get("/api")
+		ResultActions result = mockMvc.perform(get("/api/searchType")
 			.accept(MediaType.APPLICATION_JSON)
 			.param("searchType", searchType.getValue())
 		);
@@ -52,7 +53,7 @@ class ApiControllerTest {
 	@DisplayName("요청 문자열을 Enum 타입으로 변환하지 못할 경우 예외가 발생한다.")
 	@Test
 	void error() throws Exception {
-		ResultActions result = mockMvc.perform(get("/api")
+		ResultActions result = mockMvc.perform(get("/api/searchType")
 			.accept(MediaType.APPLICATION_JSON)
 			.param("searchType", "null")
 		);
@@ -64,5 +65,18 @@ class ApiControllerTest {
 				.hasMessageContaining("ConversionFailedException")
 				.hasMessageContaining("요청한 문자열을 이넘 클래스로 변환할 수 없습니다.")
 		);
+	}
+
+	@DisplayName("요청 문자열을 Enum 타입으로 변환한다.")
+	@ParameterizedTest
+	@EnumSource(PaymentType.class)
+	void dynamicConvertFactory(PaymentType paymentType) throws Exception {
+		ResultActions result = mockMvc.perform(get("/api/paymentType")
+			.accept(MediaType.APPLICATION_JSON)
+			.param("paymentType", paymentType.getValue())
+		);
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$").value(paymentType.name()));
 	}
 }
