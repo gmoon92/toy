3346c805aca363cd29ffe25b41cd95eaac783151
gmoon92 <gmoon0929@gmail.com>
2022-10-02 20:02:33 +0900

    style(SearchTypeConverter): package rename
    spring-mvc, spring-web-converter

--- spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/config/converter/SearchTypeConverter.java
+++ spring-mvc/spring-web-converter/src/main/java/com/gmoon/springwebconverter/config/converter/SearchTypeConverter.java
@@ -2,7 +2,7 @@ package com.gmoon.springwebconverter.config.converter;
 
 import org.springframework.core.convert.converter.Converter;
 
-import com.gmoon.springwebconverter.config.converter.exception.ConversionFailedException;
+import com.gmoon.springwebconverter.config.error.exception.ConversionFailedException;
 import com.gmoon.springwebconverter.model.SearchType;
 
 public class SearchTypeConverter implements Converter<String, SearchType> {
