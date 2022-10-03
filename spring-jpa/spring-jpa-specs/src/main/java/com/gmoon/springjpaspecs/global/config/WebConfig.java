ecf7875d34d37730669533447fe21681fddf2b8a
gmoon92 <gmoon0929@gmail.com>
2022-10-03 18:53:58 +0900

    feat(WebConfig): formatter 추가
    spring-jpa, spring-jpa-specs

---
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.gmoon.springjpaspecs.global.config;
+
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		ApplicationConversionService.configure(registry);
+	}
+}
