0d40ff5845e933978cddec62cbd13af7224798e6
gmoon92 <gmoon0929@gmail.com>
2022-10-04 01:08:07 +0900

    feat(AuditedEntityObject): 생성일자 수정일자 Audited 추상화 클래스 추가
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/BookStoreBook.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/BookStoreBook.java
@@ -1,11 +1,8 @@
 package com.gmoon.springjpaspecs.books.bookstore.domain;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -13,25 +10,22 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookQuantity;
 import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookStatus;
 import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookType;
-import com.gmoon.springjpaspecs.global.vo.EntityObject;
+import com.gmoon.springjpaspecs.global.vo.AuditedEntityObject;
 
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "tb_bookstor_book")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class BookStoreBook extends EntityObject<String> {
+public class BookStoreBook extends AuditedEntityObject<String> {
 
 	@Id
 	@GeneratedValue(generator = "system-uuid")
@@ -51,10 +45,6 @@ public class BookStoreBook extends EntityObject<String> {
 
 	@Embedded
 	private BookQuantity quantity;
-
-	@CreatedDate
-	@Column(name = "created_date")
-	private LocalDateTime createdDate;
 
 	@Builder
 	private BookStoreBook(String bookId, BookType type, BookStatus status, BookQuantity quantity) {
