094c202b72f2627d7c0f94e9d9bd84a9a16324b7
gmoon92 <gmoon0929@gmail.com>
2022-10-04 01:02:06 +0900

    feat(BookStoreBook): 생성일자 컬럼 추가
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/BookStoreBook.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/BookStoreBook.java
@@ -1,23 +1,32 @@
 package com.gmoon.springjpaspecs.books.bookstore.domain;
 
-import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookQuantity;
-import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookStatus;
-import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookType;
-import com.gmoon.springjpaspecs.global.vo.EntityObject;
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookQuantity;
+import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookStatus;
+import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookType;
+import com.gmoon.springjpaspecs.global.vo.EntityObject;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "tb_bookstor_book")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -42,6 +51,10 @@ public class BookStoreBook extends EntityObject<String> {
 
 	@Embedded
 	private BookQuantity quantity;
+
+	@CreatedDate
+	@Column(name = "created_date")
+	private LocalDateTime createdDate;
 
 	@Builder
 	private BookStoreBook(String bookId, BookType type, BookStatus status, BookQuantity quantity) {
