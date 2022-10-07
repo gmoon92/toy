830a75b02655f323595e95a213e2a2d567cc0b2a
gmoon92 <gmoon0929@gmail.com>
2022-10-07 16:16:34 +0900

    chore: spring-event-listener init project
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/test/java/com/gmoon/springeventlistener/SpringEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springeventlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
