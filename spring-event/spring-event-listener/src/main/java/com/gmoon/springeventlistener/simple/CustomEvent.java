30ba9ce965880b1238a598d382daa7f2561f77ee
gmoon92 <gmoon0929@gmail.com>
2022-10-07 18:16:17 +0900

    feat(CustomEvent): 커스텀 이벤트 추가
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/main/java/com/gmoon/springeventlistener/simple/CustomEvent.java
@@ -0,0 +1,16 @@
+package com.gmoon.springeventlistener.simple;
+
+import org.springframework.context.ApplicationEvent;
+
+import lombok.Getter;
+
+@Getter
+public class CustomEvent extends ApplicationEvent {
+
+	private final String message;
+
+	public CustomEvent(Object source, String message) {
+		super(source);
+		this.message = message;
+	}
+}
