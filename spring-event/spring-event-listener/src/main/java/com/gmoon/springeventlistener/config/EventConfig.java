e0e57177e9be74928528df8c15edf5baa256c665
gmoon92 <gmoon0929@gmail.com>
2022-10-08 02:44:06 +0900

    feat(EventConfig): 이벤트 비동기 설정
    - 기본적으로 동기 방식으로 동작
    
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/main/java/com/gmoon/springeventlistener/config/EventConfig.java
@@ -0,0 +1,50 @@
+package com.gmoon.springeventlistener.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class EventConfig {
+
+	@Bean(name = "applicationEventMulticaster")
+	public ApplicationEventMulticaster simpleApplicationEventMulticaster(Executor executor) {
+		SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+
+		log.info("executor: {}", executor);
+		eventMulticaster.setTaskExecutor(executor);
+		return eventMulticaster;
+	}
+
+	@Bean
+	@ConditionalOnProperty(value = "async.task", havingValue = "pool", matchIfMissing = true)
+	public Executor asyncTaskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setThreadNamePrefix("thread-gmoon-pool");
+		taskExecutor.setCorePoolSize(16);
+		taskExecutor.setMaxPoolSize(32);
+		taskExecutor.setKeepAliveSeconds(120);
+		taskExecutor.setQueueCapacity(500);
+		taskExecutor.setAllowCoreThreadTimeOut(false);
+		taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		taskExecutor.initialize();
+		return taskExecutor;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(Executor.class)
+	public SimpleAsyncTaskExecutor simpleAsyncTaskExecutor() {
+		return new SimpleAsyncTaskExecutor();
+	}
+}
