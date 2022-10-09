5430f6e8670c818ac90987948043b1ac028149a6
gmoon92 <gmoon0929@gmail.com>
2022-10-09 15:53:32 +0900

    feat(User): 사용자 VO 추가
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/main/java/com/gmoon/springeventlistener/orders/domain/vo/User.java
@@ -0,0 +1,31 @@
+package com.gmoon.springeventlistener.orders.domain.vo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class User implements Serializable {
+
+	@Column(name = "user_id", length = 50, nullable = false)
+	private String userId;
+
+	@Column(name = "user_name", nullable = false)
+	private String userName;
+
+	@Column(name = "user_email", nullable = false)
+	private String userEmail;
+
+	public User(String userId, String userName, String userEmail) {
+		this.userId = userId;
+		this.userName = userName;
+		this.userEmail = userEmail;
+	}
+}
