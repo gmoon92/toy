4e62e9f7a66b0f96c53ae8c8bed6753e47dde757
gmoon92 <gmoon0929@gmail.com>
2022-10-10 11:41:09 +0900

    feat(UserId): 사용자 식별자 밸류 추가
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/main/java/com/gmoon/springeventlistener/orders/cart/domain/vo/UserId.java
@@ -0,0 +1,25 @@
+package com.gmoon.springeventlistener.orders.cart.domain.vo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
+public class UserId implements Serializable {
+
+	@Column(name = "user_id", length = 50, nullable = false)
+	private String value;
+
+	public UserId(String value) {
+		this.value = value;
+	}
+}
