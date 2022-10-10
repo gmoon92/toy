c577e1d99aba747792b4f7017b069dc85f3c60e8
gmoon92 <gmoon0929@gmail.com>
2022-10-10 11:43:37 +0900

    feat(CartService): 카트 서비스 삭제 기능 추가
    - 사용자 식별자와 상품 번호로 지정된 카트 데이터 삭제
    
    spring-event, spring-event-listener

---
+++ spring-event/spring-event-listener/src/main/java/com/gmoon/springeventlistener/orders/cart/application/CartService.java
@@ -0,0 +1,22 @@
+package com.gmoon.springeventlistener.orders.cart.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gmoon.springeventlistener.orders.cart.domain.JpaCartRepository;
+import com.gmoon.springeventlistener.orders.cart.domain.vo.ProductNo;
+import com.gmoon.springeventlistener.orders.cart.domain.vo.UserId;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+	private final JpaCartRepository cartRepository;
+
+	@Transactional
+	public void remove(UserId userId, ProductNo productNo) {
+		cartRepository.deleteAllByUserIdAndProductNo(userId, productNo);
+	}
+}
