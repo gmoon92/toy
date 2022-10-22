0f8ced7d3f47afe6e993a5fcef3e115daa101968
gmoon92 <gmoon0929@gmail.com>
2022-10-23 04:56:50 +0900

    chore: embedded-redis init project
    testing, embedded-redis

---
+++ testing/embedded-redis/README.md
@@ -0,0 +1,7 @@
+# Embedded Redis
+
+## Reference
+
+- [baeldung - Spring Embedded Redis](https://www.baeldung.com/spring-embedded-redis)
+- [ozimov - Embedded Redis](https://github.com/ozimov/embedded-redis)
+- [kstyrc - Embedded Redis](https://github.com/kstyrc/embedded-redis)
---
+++ testing/embedded-redis/src/main/java/com/gmoon/embeddedredis/EmbeddedRedisApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.embeddedredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedRedisApplication.class, args);
+	}
+
+}
