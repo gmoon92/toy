c907fcaf8642851384f232e0741545a0132eb8b8
gmoon92 <gmoon0929@gmail.com>
2022-10-23 15:46:06 +0900

    feat(EmbeddedRedisConfig): 로컬 테스트 환경 kstyrc 방식 Embedded Redis Server 설정 추가
    - EmbeddedRedisConfig.RedisServerConfig
    
    testing, embedded-redis

---
+++ testing/embedded-redis/src/test/java/com/gmoon/embeddedredis/global/config/EmbeddedRedisConfig.java
@@ -0,0 +1,69 @@
+package com.gmoon.embeddedredis.global.config;
+
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import io.lettuce.core.ReadFrom;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class EmbeddedRedisConfig {
+
+	@Configuration
+	protected static class RedisServerConfig {
+
+		// https://github.com/kstyrc/embedded-redis
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		@ConditionalOnProperty(value = "spring.redis.embedded-type", havingValue = "kstyrc")
+		public redis.embedded.RedisServer redisServer(RedisProperties redisProperties) throws IOException {
+			int port = redisProperties.getPort();
+			log.info("embedded redis server(kstyrc) start. port: {}", port);
+			return new redis.embedded.RedisServer(port);
+		}
+	}
+
+	@Configuration
+	@EnableRedisRepositories
+	@AutoConfigureAfter(RedisServerConfig.class)
+	protected static class RedisConnectionConfig {
+
+		@Bean
+		public LettuceConnectionFactory redisConnectionFactory(RedisConfiguration redisConfig) {
+			LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+				// master-replica(master-slave) replication strategy config.
+				.readFrom(ReadFrom.REPLICA_PREFERRED)
+				.build();
+			return new LettuceConnectionFactory(redisConfig, clientConfig);
+		}
+
+		@Bean
+		public RedisConfiguration redisConfiguration(RedisProperties redisProperties) {
+			String host = redisProperties.getHost();
+			int port = redisProperties.getPort();
+			return new RedisStandaloneConfiguration(host, port);
+		}
+
+		@Bean
+		public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+			log.info("connectionFactory: {}", connectionFactory);
+			RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+			redisTemplate.setConnectionFactory(connectionFactory);
+			redisTemplate.setKeySerializer(StringRedisSerializer.UTF_8);
+			return redisTemplate;
+		}
+	}
+}
