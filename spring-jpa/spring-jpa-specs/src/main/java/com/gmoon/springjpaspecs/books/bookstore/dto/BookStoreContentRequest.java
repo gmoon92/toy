b1faac33fab50ddf9ea9a32f0b1187f5acc5612b
gmoon92 <gmoon0929@gmail.com>
2022-11-05 20:52:44 +0900

    refactor(BookStoreContentRequest): 북 이름 필드 제거
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/dto/BookStoreContentRequest.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/dto/BookStoreContentRequest.java
@@ -1,6 +1,5 @@
 package com.gmoon.springjpaspecs.books.bookstore.dto;
 
-import com.gmoon.springjpaspecs.books.book.domain.vo.BookName;
 import com.gmoon.springjpaspecs.books.bookstore.model.SortTargetType;
 import com.querydsl.core.types.Order;
 
@@ -11,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class BookStoreContentRequest {
 
-	private final BookName bookName;
 	private final SortTargetType sortTargetType;
 	private final Order order;
 }
