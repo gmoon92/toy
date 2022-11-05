68c458c9ae415c3a85f57a5bda545ffb1d50158e
gmoon92 <gmoon0929@gmail.com>
2022-11-05 20:53:01 +0900

    refactor(BookStoreContentResponse): 쿼리 프로젝션 추가
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/dto/BookStoreContentResponse.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/dto/BookStoreContentResponse.java
@@ -2,15 +2,16 @@ package com.gmoon.springjpaspecs.books.bookstore.dto;
 
 import java.time.LocalDateTime;
 
+import com.gmoon.springjpaspecs.books.book.domain.Book;
 import com.gmoon.springjpaspecs.books.book.domain.vo.BookName;
 import com.gmoon.springjpaspecs.books.book.domain.vo.BookPrice;
+import com.gmoon.springjpaspecs.books.bookstore.domain.BookStoreBook;
 import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookQuantity;
 import com.gmoon.springjpaspecs.books.bookstore.domain.vo.BookType;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class BookStoreContentResponse {
 
@@ -21,4 +22,15 @@ public class BookStoreContentResponse {
 	private final BookPrice bookPrice;
 	private final BookQuantity bookQuantity;
 	private final LocalDateTime createdDateTime;
+
+	@QueryProjection
+	public BookStoreContentResponse(Book book, BookStoreBook bookStoreBook) {
+		this.bookId = book.getId();
+		this.isbn = book.getIsbn();
+		this.bookType = bookStoreBook.getType();
+		this.bookName = book.getName();
+		this.bookPrice = book.getPrice();
+		this.bookQuantity = bookStoreBook.getQuantity();
+		this.createdDateTime = bookStoreBook.getCreatedDate();
+	}
 }
