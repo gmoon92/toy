48ced9fc919b5450e4d5ea16dbfbfeb38b193b4c
gmoon92 <gmoon0929@gmail.com>
2022-11-12 07:25:55 +0900

    feat(JpaBookStoreRepository): QuerydslPredicateExecutor add
    spring-jpa, spring-jpa-specs

--- spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/JpaBookStoreRepository.java
+++ spring-jpa/spring-jpa-specs/src/main/java/com/gmoon/springjpaspecs/books/bookstore/domain/JpaBookStoreRepository.java
@@ -1,8 +1,10 @@
 package com.gmoon.springjpaspecs.books.bookstore.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface JpaBookStoreRepository extends JpaRepository<BookStore, String>,
-	SupportBookStoreRepository {
+	SupportBookStoreRepository,
+	QuerydslPredicateExecutor<BookStore> {
 
 }
