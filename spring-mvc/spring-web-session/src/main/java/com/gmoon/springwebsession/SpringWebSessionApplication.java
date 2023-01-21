cf01825e7003922d564915655aa03521f59d1324
gmoon92 <gmoon0929@gmail.com>
2023-01-21 19:36:58 +0900

    chore: spring-web-session project init
    spring-mvc

---
+++ spring-mvc/spring-web-session/src/main/java/com/gmoon/springwebsession/SpringWebSessionApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springwebsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSessionApplication.class, args);
+	}
+
+}
---
+++ spring-mvc/spring-web-session/src/test/java/com/gmoon/springwebsession/SpringWebSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springwebsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
