d426f4a50312b3817a6c3783220e383860fd5531
gmoon92 <gmoon0929@gmail.com>
2023-02-11 14:39:57 +0900

    feat(CacheEventListener): JCache 직렬화Serializable 구현
    jpa-second-level-cache

--- spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/CacheEventListener.java
+++ spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/CacheEventListener.java
@@ -1,11 +1,14 @@
 package com.gmoon.hibernatesecondlevelcache.config;
 
+import java.io.Serializable;
+
 import javax.cache.event.CacheEntryCreatedListener;
 import javax.cache.event.CacheEntryExpiredListener;
 import javax.cache.event.CacheEntryRemovedListener;
 import javax.cache.event.CacheEntryUpdatedListener;
 
-interface CacheEventListener<K, V> extends CacheEntryCreatedListener<K, V>,
+interface CacheEventListener<K, V> extends Serializable,
+	CacheEntryCreatedListener<K, V>,
 	CacheEntryRemovedListener<K, V>,
 	CacheEntryUpdatedListener<K, V>,
 	CacheEntryExpiredListener<K, V> {
