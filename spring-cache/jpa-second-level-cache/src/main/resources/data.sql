0ba5a41e5f976549a36ae558832f7af48d6afff3
gmoon92 <gmoon0929@gmail.com>
2023-02-13 02:14:42 +0900

    chore(application.yml): jpa config prop add
    jpa-second-level-cache

---
+++ spring-cache/jpa-second-level-cache/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_common_code (code, value) VALUES ('c1', 'value');
+INSERT INTO tb_common_code (code, value) VALUES ('c2', 'value');
+INSERT INTO tb_common_code (code, value) VALUES ('c3', 'value');
+INSERT INTO tb_common_code (code, value) VALUES ('c4', 'value');
