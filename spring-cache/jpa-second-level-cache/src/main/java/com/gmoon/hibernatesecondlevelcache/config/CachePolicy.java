7f1fdc85d237305e4ea1978c43d8dea72b2bd748
gmoon92 <gmoon0929@gmail.com>
2023-02-13 00:48:20 +0900

    feat(CachePolicy): JCache 정책 이넘 클래스 추가
    jpa-second-level-cache

---
+++ spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/CachePolicy.java
@@ -0,0 +1,37 @@
+package com.gmoon.hibernatesecondlevelcache.config;
+
+import javax.cache.expiry.Duration;
+
+import com.gmoon.hibernatesecondlevelcache.member.Member;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum CachePolicy {
+
+	MEMBER(Member.class),
+	MEMBER_ALL(CacheName.MEMBER_ALL),
+	MEMBER_FIND_BY_ID(CacheName.MEMBER_FIND_BY_ID);
+
+	private final String cacheName;
+	private final Duration ttl;
+
+	CachePolicy(String cacheName) {
+		this(cacheName, Duration.ONE_MINUTE);
+	}
+
+	CachePolicy(Class<?> entity) {
+		this(entity.getName(), Duration.ONE_MINUTE);
+	}
+
+	@NoArgsConstructor(access = AccessLevel.PRIVATE)
+	public static class CacheName {
+
+		public static final String MEMBER_ALL = "MEMBER_ALL";
+		public static final String MEMBER_FIND_BY_ID = "MEMBER_FIND_BY_ID";
+	}
+}
