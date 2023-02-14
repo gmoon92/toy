34158daa4a0816840f20d672fea7fa312d2388ad
gmoon92 <gmoon0929@gmail.com>
2023-02-14 22:05:56 +0900

    feat(JCacheConfig): Redisson JCache 설정 추가
    jpa-second-level-cache

--- spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/JCacheConfig.java
+++ spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/JCacheConfig.java
@@ -1,7 +1,6 @@
 package com.gmoon.hibernatesecondlevelcache.config;
 
 import javax.cache.CacheManager;
-import javax.cache.Caching;
 import javax.cache.configuration.CacheEntryListenerConfiguration;
 import javax.cache.configuration.FactoryBuilder;
 import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
@@ -10,18 +9,29 @@ import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
 import javax.cache.spi.CachingProvider;
 
+import org.ehcache.jsr107.EhcacheCachingProvider;
 import org.hibernate.cache.jcache.ConfigSettings;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.jcache.JCacheManager;
+import org.redisson.jcache.JCachingProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheType;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableConfigurationProperties(L2CacheProperties.class)
+@RequiredArgsConstructor
 public class JCacheConfig {
+
+	private final RedissonClient redissonClient;
 
 	@Bean
 	public HibernatePropertiesCustomizer hibernateSecondLevelCacheCustomizer(CacheManager jCacheManager) {
@@ -29,35 +39,59 @@ public class JCacheConfig {
 	}
 
 	@Bean
-	public CacheManager jCacheManager(CachingProvider jCacheProvider) {
-		CacheManager jCacheManager = jCacheProvider.getCacheManager();
+	public CachingProvider cacheProvider(@Value("${l2.cache-storage}") String cacheStorage) {
+		if (CacheType.EHCACHE.name().equals(cacheStorage)) {
+			return new EhcacheCachingProvider();
+		} else if (CacheType.HAZELCAST.name().equals(cacheStorage)){
+			// Hazelcast server/client
+			// return new com.hazelcast.cache.HazelcastCachingProvider();
+			// return new com.hazelcast.client.cache.HazelcastClientCachingProvider();
+
+			// Hazelcast Embedded Server
+			return new HazelcastServerCachingProvider();
+		} else if (CacheType.REDIS.name().equals(cacheStorage)) {
+			return new JCachingProvider();
+		}
+
+		// String fqcn = "com.hazelcast.cache.HazelcastCachingProvider";
+		// CachingProvider cachingProvider = Caching.getCachingProvider(fqcn);
+		throw new IllegalArgumentException("cache storage not defined.");
+	}
+
+	@Bean
+	public CacheManager jCacheManager(CachingProvider cacheProvider) {
+		CacheManager jCacheManager = createJCacheManager(cacheProvider);
 
 		// add cache
 		createJCaches(jCacheManager);
 		return jCacheManager;
 	}
 
-	@Bean
-	public CachingProvider jCacheProvider(L2CacheProperties properties) {
-		log.info("L2CacheProperties: {}", properties);
-		// EH Cache 3
-		// return new EhcacheCachingProvider();
-		String fqcn = properties.getCachingProviderFqcn();
-		return Caching.getCachingProvider(fqcn);
+	public CacheManager createJCacheManager(CachingProvider cachingProvider) {
+		if (cachingProvider instanceof JCachingProvider) { // redisson
+			return new JCacheManager(
+				(Redisson)redissonClient,
+				getClass().getClassLoader(),
+				cachingProvider,
+				cachingProvider.getDefaultProperties(),
+				cachingProvider.getDefaultURI()
+			);
+		}
+
+		return cachingProvider.getCacheManager();
 	}
 
 	private void createJCaches(CacheManager jCacheManager) {
-		for (CachePolicy cachePolicy : CachePolicy.values()) {
-			String cacheName = cachePolicy.getCacheName();
+		for (CachePolicy policy : CachePolicy.values()) {
+			String cacheName = policy.getCacheName();
 
 			// ehcache3 는 @Cache 어노테이션이 선언된 엔티티는 자동으로 캐시 등록
-			boolean existsAlreadyCacheConfig = jCacheManager.getCache(cacheName) != null;
-			if (existsAlreadyCacheConfig) {
+			boolean existsCacheConfig = jCacheManager.getCache(cacheName) != null;
+			if (existsCacheConfig) {
 				continue;
 			}
 
-			Duration ttl = cachePolicy.getTtl();
-			jCacheManager.createCache(cacheName, getJCacheConfig(ttl));
+			jCacheManager.createCache(cacheName, getJCacheConfig(policy.getTtl()));
 		}
 	}
 
