89868559a7e6280ade3f6c730422436c9637252d
gmoon92 <gmoon0929@gmail.com>
2023-02-20 15:13:54 +0900

    style: org.apache.commons:commons-lang3 import
    jpa-second-level-cache

--- spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/RedissonConfig.java
+++ spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/RedissonConfig.java
@@ -2,6 +2,7 @@ package com.gmoon.hibernatesecondlevelcache.config;
 
 import java.time.Duration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -11,7 +12,6 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
