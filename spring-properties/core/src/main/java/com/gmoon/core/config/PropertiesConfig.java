a1676f2c32d334df03e9101cfc3c8ffb30e78260
gmoon92 <gmoon0929@gmail.com>
2023-03-02 09:07:06 +0900

    style(PropertiesConfig): jasypt auto config 주석 추가
    spring-properties

--- spring-properties/core/src/main/java/com/gmoon/core/config/PropertiesConfig.java
+++ spring-properties/core/src/main/java/com/gmoon/core/config/PropertiesConfig.java
@@ -42,6 +42,7 @@ public class PropertiesConfig {
 		}
 	}
 
+	// ref @EnableEncryptableProperties, EncryptablePropertySourceConverter
 	class JasyptConfig {
 
 		//JasyptEncryptorConfigurationProperties
