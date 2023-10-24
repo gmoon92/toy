6bb6cefa646e3748b2293b144c59338481548454
gmoon92 <gmoon0929@gmail.com>
2023-10-24 14:36:24 +0900

    test(ThreadLocalTest): 학습 테스트 추가
    java-core

---
+++ java-core/src/test/java/com/gmoon/javacore/basic/ThreadLocalTest.java
@@ -0,0 +1,28 @@
+package com.gmoon.javacore.basic;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadLocalTest {
+
+	@Test
+	void learning() {
+		ThreadLocal<List<String>> threadLocal = ThreadLocal.withInitial(ArrayList::new);
+
+		List<String> filedNames = Arrays.stream(Account.class.getDeclaredFields())
+			.map(Field::getName)
+			.collect(Collectors.toList());
+
+		threadLocal.set(filedNames);
+		System.out.println(Thread.currentThread().getName() + ", objs: " + threadLocal.get());
+	}
+
+	static class Account {
+		private String username;
+	}
+}
