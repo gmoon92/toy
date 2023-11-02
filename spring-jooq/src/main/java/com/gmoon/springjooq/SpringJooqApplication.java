553108ff689c48b5ce75ea7d48e763b813527ddb
gmoon92 <gmoon0929@gmail.com>
2023-11-02 09:44:45 +0900

    chore: init spring jooq project

---
+++ spring-jooq/src/main/java/com/gmoon/springjooq/SpringJooqApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJooqApplication.class, args);
+	}
+
+}
---
+++ spring-jooq/src/test/java/com/gmoon/springjooq/SpringJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
