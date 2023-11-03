fff9fef7e6fa2739b57aff2cb3f7a2476f5fbbbe
gmoon92 <gmoon0929@gmail.com>
2023-11-03 18:38:41 +0900

    test: Jooq CRUD Operations test case add
    spring-jooq

---
+++ spring-jooq/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_access_log (id, username, ip, os, attempt_dt) VALUES ('tal01', 'admin', '220.0.0.0', 'WINDOW','2022-02-24 12:34:43');
+INSERT INTO tb_access_log (id, username, ip, os, attempt_dt) VALUES ('tal02', 'admin', '220.0.0.0', 'WINDOW','2022-02-24 12:34:43');
+INSERT INTO tb_access_log (id, username, ip, os, attempt_dt) VALUES ('tal03', 'admin', '220.0.0.0', 'WINDOW','2022-02-24 12:34:43');
+INSERT INTO tb_access_log (id, username, ip, os, attempt_dt) VALUES ('tal04', 'admin', '220.0.0.0', 'WINDOW','2022-02-24 12:34:43');
+INSERT INTO tb_access_log (id, username, ip, os, attempt_dt) VALUES ('tal05', 'admin', '220.0.0.0', 'WINDOW','2022-02-24 12:34:43');
