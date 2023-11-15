03b9b226db03cf03ecd1e2169c4435278280f8d7
gmoon92 <gmoon0929@gmail.com>
2023-11-15 15:25:54 +0900

    feat(batch-insert): add flyway config
    - spring-jpa

---
+++ spring-jpa/batch-insert/src/main/java/com/gmoon/batchinsert/global/FlywayConfig.java
@@ -0,0 +1,20 @@
+package com.gmoon.batchinsert.global;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+	/**
+	 * @link {https://www.baeldung.com/database-migrations-with-flyway}
+	 * */
+	@Bean
+	public FlywayMigrationStrategy flywayMigrationStrategy() {
+		return flyway -> {
+			flyway.repair();
+			flyway.migrate();
+		};
+	}
+}
