8e26333c8a3bd1e3c397e058c51be3b6a284163f
gmoon92 <gmoon0929@gmail.com>
2023-11-16 14:16:59 +0900

    feat: access log excel download rest api addition.
    batch-insert

---
+++ spring-jpa/batch-insert/src/main/java/com/gmoon/batchinsert/accesslogs/api/AccessLogController.java
@@ -0,0 +1,61 @@
+package com.gmoon.batchinsert.accesslogs.api;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gmoon.batchinsert.accesslogs.application.AccessLogService;
+import com.gmoon.batchinsert.global.prop.StorageProperties;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/accesslog")
+@RestController
+@RequiredArgsConstructor
+public class AccessLogController {
+
+	private final AccessLogService accessLogService;
+	private final StorageProperties storageProperties;
+	private final ResourceLoader resourceLoader;
+
+	@GetMapping("/download")
+	public ResponseEntity<Resource> download(
+		@RequestParam @DateTimeFormat(pattern = "yyyyMMdd") LocalDate attemptDt) {
+		log.info("accessLog {} download.", attemptDt);
+		String filePath = getLocalStorageFilePath(attemptDt);
+		File excelFile = accessLogService.createExcelFile(filePath, attemptDt);
+
+		// Resource resource = resourceLoader.getResource("classpath:storage/" + excelFile.getName());
+		Resource resource = new FileSystemResource(excelFile);
+		return ResponseEntity.ok()
+			.contentType(MediaType.APPLICATION_OCTET_STREAM)
+			.cacheControl(CacheControl.noCache())
+			.header(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment()
+				.filename(excelFile.getName())
+				.build()
+				.toString())
+			.body(resource);
+	}
+
+	private String getLocalStorageFilePath(LocalDate attemptDt) {
+		String fileName = "accesslog_" + attemptDt;
+		return Paths.get(storageProperties.getAbsolutePath(), fileName)
+			.toString();
+	}
+}
---
+++ spring-jpa/batch-insert/src/test/java/com/gmoon/batchinsert/accesslogs/api/AccessLogControllerTest.java
@@ -0,0 +1,53 @@
+package com.gmoon.batchinsert.accesslogs.api;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@WebAppConfiguration
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AccessLogControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.build();
+	}
+
+	@Test
+	void download() throws Exception {
+		// given
+		String attemptDt = "20220201";
+
+		// when
+		ResultActions result = mockMvc.perform(get("/accesslog/download")
+			.queryParam("attemptDt", attemptDt));
+
+		// then
+		result.andExpectAll(status().isOk(),
+			header().exists(HttpHeaders.CONTENT_LENGTH),
+			header().string(HttpHeaders.CONTENT_TYPE, startsWith(MediaType.APPLICATION_OCTET_STREAM_VALUE)),
+			header().string(HttpHeaders.CONTENT_DISPOSITION, containsString("attachment")),
+			header().string(HttpHeaders.CONTENT_DISPOSITION, containsString(".xlsx")));
+	}
+}
