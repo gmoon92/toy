b310b8989eadeda4d2ca37a2372b750f8ed0fdcb
gmoon92 <gmoon0929@gmail.com>
2023-11-16 14:16:50 +0900

    feat: access log excel download service addition.
    batch-insert

---
+++ spring-jpa/batch-insert/src/main/java/com/gmoon/batchinsert/accesslogs/application/AccessLogService.java
@@ -0,0 +1,49 @@
+package com.gmoon.batchinsert.accesslogs.application;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gmoon.batchinsert.accesslogs.domain.AccessLog;
+import com.gmoon.batchinsert.accesslogs.domain.AccessLogExcelDownload;
+import com.gmoon.batchinsert.accesslogs.domain.AccessLogExcelDownloadRepository;
+import com.gmoon.batchinsert.accesslogs.domain.AccessLogRepository;
+import com.gmoon.javacore.util.ExcelUtils;
+import com.gmoon.javacore.util.FileUtils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccessLogService {
+
+	private final AccessLogRepository accessLogRepository;
+	private final AccessLogExcelDownloadRepository accessLogExcelDownloadRepository;
+
+	@Transactional(readOnly = true)
+	public File createExcelFile(String filePath, LocalDate attemptDt) {
+		if (FileUtils.exists(filePath)) {
+			return FileUtils.getResourceFile(filePath);
+		}
+
+		List<AccessLogExcelDownload> accessLogExcelDownloads = getAccessLogExcelDownloads(attemptDt);
+		Path uploadPath = ExcelUtils.upload(filePath, accessLogExcelDownloads, AccessLogExcelDownload.class);
+		log.info("upload file to local storage: {}", uploadPath);
+		return uploadPath.toFile();
+	}
+
+	private List<AccessLogExcelDownload> getAccessLogExcelDownloads(LocalDate attemptDt) {
+		LocalDateTime from = attemptDt.atStartOfDay();
+		LocalDateTime to = attemptDt.plusYears(1).atTime(LocalTime.MAX);
+		List<AccessLog> accessLogs = accessLogRepository.findAllByAttemptDtBetween(from, to);
+		return accessLogExcelDownloadRepository.bulkSaveAll(accessLogs);
+	}
+}
---
+++ spring-jpa/batch-insert/src/test/java/com/gmoon/batchinsert/accesslogs/application/AccessLogServiceTest.java
@@ -0,0 +1,30 @@
+package com.gmoon.batchinsert.accesslogs.application;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gmoon.batchinsert.global.prop.StorageProperties;
+
+@SpringBootTest
+class AccessLogServiceTest {
+
+	@Autowired
+	private AccessLogService service;
+
+	@Autowired
+	private StorageProperties storageProperties;
+
+	@Test
+	void createExcelFile() {
+		String filePath = storageProperties.getAbsolutePath() + "/test.xlsx";
+		LocalDate attemptDt = LocalDate.of(2022, 1, 1);
+
+		assertThatCode(() -> service.createExcelFile(filePath, attemptDt))
+				.doesNotThrowAnyException();
+	}
+}
