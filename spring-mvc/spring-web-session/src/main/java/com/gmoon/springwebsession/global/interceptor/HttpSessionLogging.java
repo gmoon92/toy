e2a3d1f24a2148be297cdb5202fd23cfe26badde
gmoon92 <gmoon0929@gmail.com>
2023-11-17 13:43:48 +0900

    feat(spring-web-session): web session trace logging interceptor addition

---
+++ spring-mvc/spring-web-session/src/main/java/com/gmoon/springwebsession/global/interceptor/HttpSessionLogging.java
@@ -0,0 +1,41 @@
+package com.gmoon.springwebsession.global.interceptor;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HttpSessionLogging implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Object handler,
+		Exception ex
+	) {
+		loggingForHttpSessionAttributes(request);
+	}
+
+	private void loggingForHttpSessionAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		log.info("==================HTTP Session Start==================");
+		if (session == null) {
+			log.info("Empty HttpSession");
+		} else {
+			Enumeration<String> attributeNames = session.getAttributeNames();
+			while (attributeNames.hasMoreElements()) {
+				String name = attributeNames.nextElement();
+				Object value = session.getAttribute(name);
+				log.info("attr: {}={}", name, value);
+			}
+		}
+		log.info("==================HTTP Session End==================");
+	}
+}
