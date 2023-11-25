f57e670899e98c809a7e112c6a743625b3517740
gmoon92 <gmoon0929@gmail.com>
2023-11-25 21:49:50 +0900

    chore(spring-jpa-replication): init project

---
+++ spring-jpa/spring-jpa-replication/src/main/java/com/gmoon/springjpareplication/SpringJpaReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjpareplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaReplicationApplication.class, args);
+	}
+
+}
---
+++ spring-jpa/spring-jpa-replication/src/test/java/com/gmoon/springjpareplication/SpringJpaReplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.springjpareplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaReplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
