4fb8eece169271ce4253713167b028c44b738c71
gmoon92 <gmoon0929@gmail.com>
2023-12-29 07:50:26 +0900

    refactor(RedissonConfig): redisson config channing

--- spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/RedissonConfig.java
+++ spring-cache/jpa-second-level-cache/src/main/java/com/gmoon/hibernatesecondlevelcache/config/RedissonConfig.java
@@ -1,12 +1,9 @@
 package com.gmoon.hibernatesecondlevelcache.config;
 
-import java.time.Duration;
-
 import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.SingleServerConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -21,21 +18,26 @@ public class RedissonConfig {
 	@Bean
 	@ConditionalOnProperty(name = "cache.storage", havingValue = "REDIS")
 	public RedissonClient redissonClient(RedisProperties properties) {
-		Config config = new Config();
-		SingleServerConfig singleServerConfig = config.useSingleServer();
-
-		String password = properties.getPassword();
-		if (StringUtils.isNotBlank(password)) {
-			singleServerConfig.setPassword(password);
-		}
-
-		Duration connectTimeout = properties.getConnectTimeout();
-		config.useSingleServer()
-			// .setAddress(properties.getUrl())
-			.setAddress(String.format("redis://%s:%d", properties.getHost(), properties.getPort()))
-			.setTimeout((int)connectTimeout.toMillis());
+		Config config = getServerConfig(properties);
 		return Redisson.create(config);
 	}
 
+	private Config getServerConfig(RedisProperties properties) {
+		Config config = new Config();
+		config.useSingleServer()
+			.setPassword(getPassword(properties))
+			.setAddress(properties.getUrl())
+			.setAddress(String.format("redis://%s:%d", properties.getHost(), properties.getPort()))
+			.setTimeout((int)properties.getConnectTimeout().toMillis());
+		return config;
+	}
+
+	private String getPassword(RedisProperties properties) {
+		String password = properties.getPassword();
+		if (StringUtils.isBlank(password)) {
+			return null;
+		}
+		return password;
+	}
 }
 
