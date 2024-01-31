f7fc9e33c176e2cfa7809961327f930f6eb8d32b
gmoon92 <gmoon0929@gmail.com>
2024-01-31 14:44:09 +0900

    chore: hibernate type project init

---
+++ spring-jpa/hibernate-type/src/main/java/com/gmoon/hibernatetype/HibernateTypeApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.hibernatetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTypeApplication.class, args);
+	}
+
+}
