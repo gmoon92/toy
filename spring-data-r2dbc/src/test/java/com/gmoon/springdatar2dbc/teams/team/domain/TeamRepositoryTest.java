8959274004c57b9fecbe444506c8554441d5ba72
gmoon92 <gmoon0929@gmail.com>
2024-04-08 23:45:16 +0900

    test: TeamRepositoryTest jpa config add
    spring-data-r2dbc

--- spring-data-r2dbc/src/test/java/com/gmoon/springdatar2dbc/teams/team/domain/TeamRepositoryTest.java
+++ spring-data-r2dbc/src/test/java/com/gmoon/springdatar2dbc/teams/team/domain/TeamRepositoryTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+import com.gmoon.springdatar2dbc.global.JpaConfig;
+
+@Import(JpaConfig.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TeamRepositoryTest {
