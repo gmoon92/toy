50c766aeeb1d94d087f60ceafe2dad759dcae77b
gmoon92 <gmoon0929@gmail.com>
2024-04-21 22:00:05 +0900

    chore: jason web key project init
    spring-security, spring-security-jwk

---
+++ spring-security/spring-security-jwk/src/main/java/com/gmoon/springsecurityjwk/SpringSecurityJwkApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.springsecurityjwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwkApplication.class, args);
+	}
+
+}
