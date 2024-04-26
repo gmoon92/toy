d5696707f7cf8ae48e34ad7f391e59ae35da3993
gmoon92 <gmoon0929@gmail.com>
2024-04-27 01:42:42 +0900

    chore: QueryDSL Projections init

---
+++ spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/QueryDslProjectionsApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.querydslprojections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslProjectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslProjectionsApplication.class, args);
+	}
+
+}
---
+++ spring-jpa/querydsl-projections/src/test/java/com/gmoon/querydslprojections/QueryDslProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.querydslprojections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslProjectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
