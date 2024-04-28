540cea75f845eaa4364851beabd3ffd69e27244c
gmoon92 <gmoon0929@gmail.com>
2024-04-29 00:39:50 +0900

    feat: coupon entity add
    querydsl-projections

---
+++ spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/Coupon.java
@@ -0,0 +1,23 @@
+package com.gmoon.querydslprojections;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Coupon implements Serializable {
+
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	@Column(name = "id", length = 50)
+	private String id;
+
+	private LocalDateTime expireDt;
+}
