462c7c15f9697a64d6c979e4bcb4e585719a3d8b
gmoon92 <gmoon0929@gmail.com>
2024-05-03 04:58:05 +0900

    feat(Movie): Collection 멤버 변수 초기화 설정, 편의 메서드 추가
    - lombok.@Default 적용
    - Collection 인스턴스 변수 초기화, NPE 및 하이버네이트에 의한 참조 오류 방지
    
    querydsl-projections

--- spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/movies/movie/domain/Movie.java
+++ spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/movies/movie/domain/Movie.java
@@ -1,6 +1,7 @@
 package com.gmoon.querydslprojections.movies.movie.domain;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.AttributeOverride;
@@ -20,7 +21,12 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import com.gmoon.querydslprojections.movies.users.user.domain.Actor;
+import com.gmoon.querydslprojections.movies.users.user.domain.Director;
+
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,7 +39,9 @@ import lombok.NoArgsConstructor;
 	}
 )
 @Entity
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @EqualsAndHashCode(of = "id")
 public class Movie implements Serializable {
@@ -50,27 +58,44 @@ public class Movie implements Serializable {
 	@OneToOne(mappedBy = "movie", optional = false, cascade = CascadeType.ALL, orphanRemoval = true)
 	private MovieDirector director;
 
+	@Builder.Default
 	@OneToMany(mappedBy = "movie", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-	private Set<MovieCast> castMembers;
+	private Set<MovieCast> castMembers = new HashSet<>();
 
+	@Builder.Default
 	@Enumerated(EnumType.STRING)
 	@Column(name = "genre", length = 50, nullable = false)
-	private MovieGenre genre;
+	private MovieGenre genre = MovieGenre.ACTION;
 
+	@Builder.Default
 	@Enumerated(EnumType.STRING)
 	@Column(name = "film_rating", length = 50, nullable = false)
-	private FilmRatings filmRatings;
+	private FilmRatings filmRatings = FilmRatings.G;
 
 	@Column(name = "running_minutes")
 	private long runningMinutes;
 
 	@Embedded
 	@AttributeOverrides({
-		@AttributeOverride(name = "releaseTime", column = @Column(name = "release_time", nullable = false)),
+		@AttributeOverride(name = "value", column = @Column(name = "release_time", nullable = false)),
 		@AttributeOverride(name = "year", column = @Column(name = "release_year", nullable = false)),
 		@AttributeOverride(name = "month", column = @Column(name = "release_month", nullable = false)),
 		@AttributeOverride(name = "dayOfMonth", column = @Column(name = "release_day", nullable = false)),
 		@AttributeOverride(name = "hour", column = @Column(name = "release_hour", nullable = false))
 	})
 	private MovieReleaseTime releaseTime;
+
+	public Movie chooseDirector(Director director) {
+		this.director = MovieDirector.create(this, director);
+		return this;
+	}
+
+	public Movie castingActor(Actor actor) {
+		castMembers.add(MovieCast.create(this, actor));
+		return this;
+	}
+
+	public String getDirectorName() {
+		return director.getName();
+	}
 }
