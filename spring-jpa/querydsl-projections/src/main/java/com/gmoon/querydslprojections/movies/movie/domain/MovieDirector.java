ea00b7109c3225876c6cb8ec89d6d7c133f54f1a
gmoon92 <gmoon0929@gmail.com>
2024-05-03 04:28:04 +0900

    refactor: MovieDirector 비식별 관계에서 식별 관계 복합키로 모델링 수정
    - MovieDirector 엔티티는 영화와 감독의 중간 테이블 역할
    - 자연 키를 사용한 복합 키 개념보단, 중간 테이블로서 기존 대체 키 사용하여 비즈니스와는 무관하고, 기존 PK 인덱스를 활용과, 필수적인 객체 관점에서 비식별 관계보단 식별 관계가 맞다고 판단.
    
    querydsl-projections

--- spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/movies/movie/domain/MovieDirector.java
+++ spring-jpa/querydsl-projections/src/main/java/com/gmoon/querydslprojections/movies/movie/domain/MovieDirector.java
@@ -3,14 +3,12 @@ package com.gmoon.querydslprojections.movies.movie.domain;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 import com.gmoon.querydslprojections.movies.users.user.domain.Director;
 
@@ -26,17 +24,42 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 public class MovieDirector implements Serializable {
 
-	@Id
-	@GeneratedValue(generator = "system-uuid")
-	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
-	@Column(name = "id", length = 50)
-	private String id;
+	@EmbeddedId
+	private Id id = new Id();
 
 	@OneToOne(optional = false)
-	@JoinColumn(name = "movie_id", referencedColumnName = "id", nullable = false)
+	@JoinColumn(name = "movie_id", referencedColumnName = "id", nullable = false, insertable = false, updatable = false)
 	private Movie movie;
 
 	@OneToOne(optional = false)
-	@JoinColumn(name = "director_id", referencedColumnName = "id", nullable = false)
+	@JoinColumn(name = "director_id", referencedColumnName = "id", nullable = false, insertable = false, updatable = false)
 	private Director director;
+
+	protected static MovieDirector create(final Movie movie, final Director director) {
+		MovieDirector movieDirector = new MovieDirector();
+		movieDirector.id = new Id(movie, director);
+		movieDirector.director = director;
+		return movieDirector;
+	}
+
+	public String getName() {
+		return director.getName();
+	}
+
+	@Embeddable
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@EqualsAndHashCode
+	static class Id implements Serializable {
+
+		@Column(name = "movie_id", length = 50, nullable = false)
+		private String movieId;
+
+		@Column(name = "director_id", length = 50, nullable = false)
+		private String directorId;
+
+		protected Id(Movie movie, Director director) {
+			this.movieId = movie.getId();
+			this.directorId = director.getId();
+		}
+	}
 }
