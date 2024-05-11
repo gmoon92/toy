23b82d27e990dc1a4d1f7ee3001fd8401adf0996
gmoon92 <gmoon0929@gmail.com>
2024-05-11 18:05:18 +0900

    feat(ZeroBasedPage): 편의 팩토리 메서드 추가

--- java-core/src/main/java/com/gmoon/javacore/domain/ZeroBasedPage.java
+++ java-core/src/main/java/com/gmoon/javacore/domain/ZeroBasedPage.java
@@ -2,31 +2,43 @@ package com.gmoon.javacore.domain;
 
 import static com.gmoon.javacore.util.NumberUtils.*;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Getter
+@ToString
+@EqualsAndHashCode
 public class ZeroBasedPage {
+
+	private static final ZeroBasedPage ZERO = new ZeroBasedPage(0);
 
 	private final int value;
 
-	private ZeroBasedPage(Number page) {
-		if (page == null) {
-			value = 0;
-		} else {
-			value = getValueOrMax(page);
-		}
+	private <T extends Number> ZeroBasedPage(T page) {
+		value = positiveNumberOrZero(toInt(page));
 	}
 
-	private int getValueOrMax(Number page) {
+	public static ZeroBasedPage zero() {
+		return ZERO;
+	}
+
+	public static <T extends Number> ZeroBasedPage from(T page) {
+		return new ZeroBasedPage(page);
+	}
+
+	public static <T extends Number> ZeroBasedPage adjust(T page) {
+		if (page == null) {
+			return ZERO;
+		}
+
 		boolean overflows = page.longValue() > Integer.MAX_VALUE;
 		if (overflows) {
-			return Integer.MAX_VALUE;
+			return new ZeroBasedPage(Integer.MAX_VALUE);
 		}
-		
-		return positiveNumberOrZero(toInt(page) - 1);
-	}
 
-	public static <T extends Number> int from(T page) {
-		return new ZeroBasedPage(page).value;
+		return new ZeroBasedPage(toInt(page) - 1);
 	}
 }
--- java-core/src/test/java/com/gmoon/javacore/domain/ZeroBasedPageTest.java
+++ java-core/src/test/java/com/gmoon/javacore/domain/ZeroBasedPageTest.java
@@ -10,19 +10,19 @@ class ZeroBasedPageTest {
 	@DisplayName("클라이언트 페이지 1부터 시작, 페이지 처리시 0 부터 시작")
 	@Test
 	void nullSafe() {
-		assertThat(ZeroBasedPage.from(null)).isZero();
-		assertThat(ZeroBasedPage.from(0)).isZero();
-		assertThat(ZeroBasedPage.from(1)).isZero();
+		assertThat(ZeroBasedPage.adjust(null)).isEqualTo(ZeroBasedPage.zero());
+		assertThat(ZeroBasedPage.adjust(0)).isEqualTo(ZeroBasedPage.zero());
+		assertThat(ZeroBasedPage.adjust(1)).isEqualTo(ZeroBasedPage.zero());
 
-		assertThat(ZeroBasedPage.from(2)).isOne();
+		assertThat(ZeroBasedPage.adjust(2)).isEqualTo(ZeroBasedPage.from(1));
 	}
 
 	@DisplayName("Integer 값 범위를 초과할 경우 MAX 값 반환")
 	@Test
-	void integerValueOverflows() {
-		assertThat(ZeroBasedPage.from(Integer.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE - 1);
+	void adjust() {
+		assertThat(ZeroBasedPage.adjust(Integer.MAX_VALUE)).isEqualTo(ZeroBasedPage.from(Integer.MAX_VALUE - 1));
 
-		assertThat(ZeroBasedPage.from(Double.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE);
-		assertThat(ZeroBasedPage.from(Long.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE);
+		assertThat(ZeroBasedPage.adjust(Double.MAX_VALUE)).isEqualTo(ZeroBasedPage.from(Integer.MAX_VALUE));
+		assertThat(ZeroBasedPage.adjust(Long.MAX_VALUE)).isEqualTo(ZeroBasedPage.from(Integer.MAX_VALUE));
 	}
 }
