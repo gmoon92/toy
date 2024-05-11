9fc2faad29a53a100850e7af337ad4e3a19f03dc
gmoon92 <gmoon0929@gmail.com>
2024-05-11 17:48:43 +0900

    fix(ZeroBasedPage): 페이지 값이 overflow 되는 경우 페이지 데이터 형의 max 값 반환

--- java-core/src/main/java/com/gmoon/javacore/domain/ZeroBasedPage.java
+++ java-core/src/main/java/com/gmoon/javacore/domain/ZeroBasedPage.java
@@ -5,19 +5,28 @@ import static com.gmoon.javacore.util.NumberUtils.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ZeroBasedPage<T extends Number> {
+public class ZeroBasedPage {
 
 	private final int value;
 
-	private ZeroBasedPage(T page) {
+	private ZeroBasedPage(Number page) {
 		if (page == null) {
 			value = 0;
 		} else {
-			value = positiveNumberOrZero(toInt(page) - 1);
+			value = getValueOrMax(page);
 		}
 	}
 
+	private int getValueOrMax(Number page) {
+		boolean overflows = page.longValue() > Integer.MAX_VALUE;
+		if (overflows) {
+			return Integer.MAX_VALUE;
+		}
+		
+		return positiveNumberOrZero(toInt(page) - 1);
+	}
+
 	public static <T extends Number> int from(T page) {
-		return new ZeroBasedPage<>(page).value;
+		return new ZeroBasedPage(page).value;
 	}
 }
