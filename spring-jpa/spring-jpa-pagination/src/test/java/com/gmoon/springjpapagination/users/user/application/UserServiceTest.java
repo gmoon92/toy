b30956038223c9797271f4cde99791ec95ebd558
gmoon92 <gmoon0929@gmail.com>
2024-05-20 13:26:08 +0900

    test(UserServiceTest): 유니온 응답 데이터 사이즈 검증
    spring-jpa-pagination

--- spring-jpa/spring-jpa-pagination/src/test/java/com/gmoon/springjpapagination/users/user/application/UserServiceTest.java
+++ spring-jpa/spring-jpa-pagination/src/test/java/com/gmoon/springjpapagination/users/user/application/UserServiceTest.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.gmoon.springjpapagination.users.user.dto.UserContentListVO;
+import com.gmoon.springjpapagination.users.user.dto.UserContentVO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class UserServiceTest {
 
@@ -17,9 +21,20 @@ class UserServiceTest {
 	@Test
 	void getUserContentListVO() {
 		UserContentListVO listVO = new UserContentListVO();
+		listVO.setPage(1);
+		listVO.setPageSize(10);
 
 		listVO = service.getUserContentListVO(listVO);
 
 		assertThat(listVO.getContent()).isNotEmpty();
+		assertThat(listVO.getContent().size()).isLessThanOrEqualTo(listVO.getPageSize());
+		assertThat(listVO.getContent())
+			 .filteredOn(vo -> vo.getType().equals(UserContentVO.Type.USER_GROUP))
+			 .size()
+			 .isEqualTo(3);
+		assertThat(listVO.getContent())
+			 .filteredOn(vo -> vo.getType().equals(UserContentVO.Type.USER))
+			 .size()
+			 .isLessThanOrEqualTo(7);
 	}
 }
