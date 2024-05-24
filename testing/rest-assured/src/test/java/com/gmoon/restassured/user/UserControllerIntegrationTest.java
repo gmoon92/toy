4c669ebe20044b37aeb7f88c8a52bc6d2f20bd6b
gmoon92 <gmoon0929@gmail.com>
2024-05-25 02:55:02 +0900

    chore(springboot3): LocalServerPort package rename
    - org.springframework.boot.web.server.LocalServerPort
    - org.springframework.boot.test.web.server.LocalServerPort

--- testing/rest-assured/src/test/java/com/gmoon/restassured/user/UserControllerIntegrationTest.java
+++ testing/rest-assured/src/test/java/com/gmoon/restassured/user/UserControllerIntegrationTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
