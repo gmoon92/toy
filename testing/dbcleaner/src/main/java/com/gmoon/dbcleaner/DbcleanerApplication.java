d0acd9fa4158b324bec82b3261cdc814ddea233e
gmoon92 <gmoon0929@gmail.com>
2024-07-22 22:42:35 +0900

    chore(dbcleaner): init project

---
+++ testing/dbcleaner/src/main/java/com/gmoon/dbcleaner/DbcleanerApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.dbcleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcleanerApplication.class, args);
+	}
+
+}
---
+++ testing/dbcleaner/src/test/java/com/gmoon/dbcleaner/DbcleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.dbcleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
