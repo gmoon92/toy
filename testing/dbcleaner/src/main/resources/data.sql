ef8ec1965ae0401b5a32dd8dfc88d98b1ca0fff8
gmoon92 <gmoon0929@gmail.com>
2024-07-23 10:42:46 +0900

    chore(datacleaner): sample data init

---
+++ testing/dbcleaner/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_ticket_office (id, name) VALUES (1, 'gmoon');
+INSERT INTO tb_movie (id, title, ticket_office_id) VALUES (1, 'movie1', 1);
+INSERT INTO tb_movie (id, title, ticket_office_id) VALUES (2, 'movie2', 1);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (1, 1, 3000);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (2, 1, 3000);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (3, 1, 3000);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (4, 1, 3000);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (5, 1, 3000);
+INSERT INTO tb_ticket (id, ticket_office_id, selling_price) VALUES (6, 1, 3000);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (1, 1);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (1, 2);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (1, 3);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (1, 4);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (1, 5);
+INSERT INTO tb_movie_ticket (movie_id, ticket_id) VALUES (2, 6);
