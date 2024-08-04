fd7fda888e177942b1dda101c587acb9015b6683
gmoon92 <gmoon0929@gmail.com>
2024-08-04 11:21:26 +0900

    feat(dbrestore): spring async config

---
+++ testing/dbrestore/web/src/main/java/com/gmoon/dbrestore/web/global/SpringAsyncConfig.java
@@ -0,0 +1,9 @@
+package com.gmoon.dbrestore.web.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class SpringAsyncConfig {
+}
