ececbafee7885e8e75e041ffd5bbe33db79483c6
gmoon92 <gmoon0929@gmail.com>
2024-08-23 20:48:08 +0900

    test: chained AssertJ assertions should be simplified to the corresponding dedicated assertion

--- testing/dbrestore/web/src/test/java/com/gmoon/dbrestore/web/movies/infra/JpaTicketOfficeRepositoryTest.java
+++ testing/dbrestore/web/src/test/java/com/gmoon/dbrestore/web/movies/infra/JpaTicketOfficeRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.gmoon.dbrestore.web.movies.infra;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 import com.gmoon.dbrestore.web.movies.domain.TicketOfficeRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,10 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 @Import(TicketOfficeRepositoryAdapter.class)
 @DataJpaTest
@@ -35,7 +33,6 @@ class JpaTicketOfficeRepositoryTest {
 		long id = 1L;
 		long movieId = 1L;
 
-		Assertions.assertThat(repository.findMovie(id, movieId))
-			 .isNotEqualTo(Optional.empty());
+		assertThat(repository.findMovie(id, movieId)).isPresent();
 	}
 }
