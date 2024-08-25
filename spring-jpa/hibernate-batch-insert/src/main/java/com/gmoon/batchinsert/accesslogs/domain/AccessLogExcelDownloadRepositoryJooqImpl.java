bec0ef641c303c2cf58d0fb8e6a9c05e17c3ea8a
gmoon92 <gmoon0929@gmail.com>
2024-08-25 23:10:46 +0900

    style: Sections of code should not be commented out

--- spring-jpa/hibernate-batch-insert/src/main/java/com/gmoon/batchinsert/accesslogs/domain/AccessLogExcelDownloadRepositoryJooqImpl.java
+++ spring-jpa/hibernate-batch-insert/src/main/java/com/gmoon/batchinsert/accesslogs/domain/AccessLogExcelDownloadRepositoryJooqImpl.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jooq.DSLContext;
+import org.jooq.InsertValuesStepN;
+
+import com.gmoon.batchinsert.global.jmodel.tables.ExAccessLog;
+import com.gmoon.batchinsert.global.jmodel.tables.records.ExAccessLogRecord;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,39 +16,38 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AccessLogExcelDownloadRepositoryJooqImpl implements AccessLogExcelDownloadRepositoryJooq {
 
-	// private static final ExAccessLog jExAccessLog = ExAccessLog.EX_ACCESS_LOG;
+	private static final ExAccessLog jExAccessLog = ExAccessLog.EX_ACCESS_LOG;
 	private final DSLContext dsl;
 
 	// https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/insert-statement/insert-values/
 	// Jooq 타입 추론 이슈 https://github.com/jOOQ/jOOQ/issues/6604
 	@Override
 	public List<AccessLogExcelDownload> bulkSaveAllAtJooq(List<AccessLog> accessLogs) {
-		// InsertValuesStepN<ExAccessLogRecord> insertInto = (InsertValuesStepN<ExAccessLogRecord>)dsl.insertInto(
-		// 	 jExAccessLog,
-		// 	 jExAccessLog.ID,
-		// 	 jExAccessLog.USERNAME,
-		// 	 jExAccessLog.IP,
-		// 	 jExAccessLog.OS,
-		// 	 jExAccessLog.ATTEMPT_DT
-		// );
-		//
-		// List<AccessLogExcelDownload> result = new ArrayList<>(accessLogs.size());
-		// for (AccessLog accessLog : accessLogs) {
-		// 	AccessLogExcelDownload accessLogExcelDownload = AccessLogExcelDownload.create(accessLog);
-		// 	insertInto.values(
-		// 		 accessLogExcelDownload.getId(),
-		// 		 accessLogExcelDownload.getUsername(),
-		// 		 accessLogExcelDownload.getIp(),
-		// 		 accessLogExcelDownload.getOs(),
-		// 		 accessLogExcelDownload.getAttemptDt()
-		// 	);
-		//
-		// 	result.add(accessLogExcelDownload);
-		// }
-		//
-		// insertInto.execute();
+		InsertValuesStepN<ExAccessLogRecord> insertInto = (InsertValuesStepN<ExAccessLogRecord>)dsl.insertInto(
+			 jExAccessLog,
+			 jExAccessLog.ID,
+			 jExAccessLog.USERNAME,
+			 jExAccessLog.IP,
+			 jExAccessLog.OS,
+			 jExAccessLog.ATTEMPT_DT
+		);
 
-		// return result;
-		return new ArrayList<>();
+		List<AccessLogExcelDownload> result = new ArrayList<>(accessLogs.size());
+		for (AccessLog accessLog : accessLogs) {
+			AccessLogExcelDownload accessLogExcelDownload = AccessLogExcelDownload.create(accessLog);
+			insertInto.values(
+				 accessLogExcelDownload.getId(),
+				 accessLogExcelDownload.getUsername(),
+				 accessLogExcelDownload.getIp(),
+				 accessLogExcelDownload.getOs(),
+				 accessLogExcelDownload.getAttemptDt()
+			);
+
+			result.add(accessLogExcelDownload);
+		}
+
+		insertInto.execute();
+
+		return result;
 	}
 }
