3e7e3c3c9cc4e0dd6c09ca161e72133a86424111
gmoon92 <gmoon0929@gmail.com>
2024-09-24 11:29:04 +0900

    feat: response entity using by stream

--- spring-jpa/time-sorted-unique-identifier/src/main/java/com/gmoon/timesorteduniqueidentifier/users/user/adapter/in/web/UserController.java
+++ spring-jpa/time-sorted-unique-identifier/src/main/java/com/gmoon/timesorteduniqueidentifier/users/user/adapter/in/web/UserController.java
@@ -1,16 +1,23 @@
 package com.gmoon.timesorteduniqueidentifier.users.user.adapter.in.web;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.gmoon.timesorteduniqueidentifier.global.base.WebAdapter;
 import com.gmoon.timesorteduniqueidentifier.users.user.application.dto.UserProfile;
 import com.gmoon.timesorteduniqueidentifier.users.user.application.port.in.UpdateUserPasswordUseCase;
 import com.gmoon.timesorteduniqueidentifier.users.user.application.port.in.UserPasswordUpdateCommand;
 import com.gmoon.timesorteduniqueidentifier.users.user.application.port.in.UserQueryUseCase;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @WebAdapter
 @RequestMapping("/user")
@@ -22,7 +29,9 @@ public class UserController {
 
 	@GetMapping
 	public HttpEntity<List<UserProfile>> view() {
-		return ResponseEntity.ok(userQueryUseCase.getAllUsers());
+		return userQueryUseCase.getAllUsers()
+			 .stream()
+			 .collect(Collectors.collectingAndThen(Collectors.toList(), ResponseEntity::ok));
 	}
 
 	@GetMapping("/{id}")
