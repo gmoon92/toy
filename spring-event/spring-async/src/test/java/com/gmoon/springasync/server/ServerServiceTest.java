6020bac1f32ed4d27e7fbcf68690edc2f8b628c1
gmoon92 <gmoon0929@gmail.com>
2025-01-31 04:47:42 +0900

    refactor: 잘못 선언된 @ExtendWith(SpringExtension.class) 제거

--- spring-event/spring-async/src/test/java/com/gmoon/springasync/server/ServerServiceTest.java
+++ spring-event/spring-async/src/test/java/com/gmoon/springasync/server/ServerServiceTest.java
@@ -1,16 +1,13 @@
 package com.gmoon.springasync.server;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Disabled("application.yml 메일 설정 후 테스트 진행 "
 	 + "username: @test.mail.username@"
