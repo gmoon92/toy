a2b74d5f6f283abfedf0790574d95b6dcf2466dd
gmoon92 <gmoon0929@gmail.com>
2025-01-31 04:33:04 +0900

    fix: factory bean afterPropertiesSet called

--- spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/config/YamlPropertySourceFactory.java
+++ spring-security/spring-security-cors/resource-server/src/main/java/com/gmoon/resourceserver/config/YamlPropertySourceFactory.java
@@ -1,14 +1,14 @@
 package com.gmoon.resourceserver.config;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 	@Override
@@ -17,6 +17,8 @@ public class YamlPropertySourceFactory implements PropertySourceFactory {
 
 		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
 		factory.setResources(resource);
+		factory.afterPropertiesSet();
+
 		Properties properties = factory.getObject();
 		return new PropertiesPropertySource(resource.getFilename(), properties);
 	}
--- testing/localstack/src/main/java/com/gmoon/localstack/aws/config/YamlPropertySourceFactory.java
+++ testing/localstack/src/main/java/com/gmoon/localstack/aws/config/YamlPropertySourceFactory.java
@@ -1,13 +1,13 @@
 package com.gmoon.localstack.aws.config;
 
-import java.util.Properties;
-
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.util.Properties;
 
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
@@ -24,6 +24,7 @@ public class YamlPropertySourceFactory implements PropertySourceFactory {
 	private Properties getProperties(Resource resource) {
 		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
 		factory.setResources(resource);
+		factory.afterPropertiesSet();
 		return factory.getObject();
 	}
 }
