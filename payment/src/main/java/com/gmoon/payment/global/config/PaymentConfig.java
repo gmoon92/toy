6a593b7c4b9ef065eda9998b6a02fd959ca39624
gmoon92 <gmoon0929@gmail.com>
2025-02-11 00:33:43 +0900

    chore(payment): 불필요한 설정 제거

--- payment/src/main/java/com/gmoon/payment/global/config/PaymentConfig.java
+++ payment/src/main/java/com/gmoon/payment/global/config/PaymentConfig.java
@@ -1,18 +1,17 @@
 package com.gmoon.payment.global.config;
 
+import com.gmoon.payment.appstore.infra.AppStoreClient;
+import com.gmoon.payment.global.properties.AppStoreProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
 
-import com.gmoon.payment.appstore.infra.AppStoreClient;
-import com.gmoon.payment.global.properties.AppStoreProperties;
-
 @Configuration
 public class PaymentConfig {
 
 	@Bean
-	@ConditionalOnProperty(name = "payment.appstore.enabled", havingValue = "1")
+	@ConditionalOnProperty(name = "service.payment.appstore.enabled", havingValue = "1")
 	public AppStoreClient appStoreClient(AppStoreProperties properties, ResourceLoader resourceLoader) {
 		return new AppStoreClient(properties, resourceLoader);
 	}
