989c557d5eb5a1536657156c80b05c2905a1d1d3
gmoon92 <gmoon0929@gmail.com>
2025-02-12 21:03:49 +0900

    refactor(payment): appstore 영수증 결제 금액 계산 수정

--- payment/src/main/java/com/gmoon/payment/appstore/model/AppStorePaymentResponse.java
+++ payment/src/main/java/com/gmoon/payment/appstore/model/AppStorePaymentResponse.java
@@ -1,19 +1,31 @@
 package com.gmoon.payment.appstore.model;
 
 import com.apple.itunes.storekit.model.JWSTransactionDecodedPayload;
+import com.gmoon.payment.global.utils.NumberUtils;
 
 public record AppStorePaymentResponse(
 	 String transactionId,
 	 String orgTransactionId,
-	 Long price,
+	 double price,
 	 Long purchaseTime
 ) {
 
+	/**
+	 * The price, in milliunits, of the in-app purchase or subscription offer that you configured in App Store Connect.
+	 *
+	 * @return price
+	 * @see <a href="https://developer.apple.com/documentation/appstoreserverapi/price">price</a>
+	 **/
 	public static AppStorePaymentResponse from(JWSTransactionDecodedPayload payload) {
+		double price = NumberUtils.toBigDecimal(payload.getPrice())
+			 .multiply(NumberUtils.toBigDecimal(payload.getQuantity()))
+			 .divide(NumberUtils.toBigDecimal(1_000))
+			 .doubleValue();
+
 		return new AppStorePaymentResponse(
 			 payload.getTransactionId(),
 			 payload.getOriginalTransactionId(),
-			 payload.getPrice(),
+			 price,
 			 payload.getPurchaseDate()
 		);
 	}
--- payment/src/main/java/com/gmoon/payment/global/utils/NumberUtils.java
+++ payment/src/main/java/com/gmoon/payment/global/utils/NumberUtils.java
@@ -3,6 +3,8 @@ package com.gmoon.payment.global.utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class NumberUtils {
 
@@ -21,4 +23,38 @@ public final class NumberUtils {
 
 		return Integer.toString(i);
 	}
+
+	public static BigDecimal toBigDecimal(Long l) {
+		return BigDecimal.valueOf(defaultValue(l));
+	}
+
+	public static BigDecimal toBigDecimal(Integer i) {
+		return BigDecimal.valueOf(defaultValue(i));
+	}
+
+	public static BigDecimal toBigDecimal(Double d) {
+		return BigDecimal.valueOf(defaultValue(d));
+	}
+
+	public static double defaultValue(Double d) {
+		if (d == null) {
+			return 0;
+		}
+		return d;
+	}
+
+	public static long defaultValue(Integer i) {
+		if (i == null) {
+			return 0;
+		}
+		return i;
+	}
+
+	public static long defaultValue(Long l) {
+		if (l == null) {
+			return 0;
+		}
+		return l;
+	}
+
 }
