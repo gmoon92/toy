e800d585a8d6cb349726d6342a24e8da1bd451a9
gmoon92 <gmoon0929@gmail.com>
2025-03-26 22:04:36 +0900

    refactor(accesslog): 불필요한 HttpContentCacheFilter 필터 제거

--- spring-mvc/spring-access-log/src/main/java/com/rsupport/rv5x/springaccesslog/global/config/WebConfig.java
+++ spring-mvc/spring-access-log/src/main/java/com/rsupport/rv5x/springaccesslog/global/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.rsupport.rv5x.springaccesslog.global.config;
 
-import com.rsupport.rv5x.springaccesslog.global.filter.HttpContentCacheFilter;
 import com.rsupport.rv5x.springaccesslog.global.filter.WebAccessLogFilter;
+import com.rsupport.rv5x.springaccesslog.global.utils.RequestUtils;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebMvc
@@ -20,29 +24,26 @@ public class WebConfig implements WebMvcConfigurer {
 		WebMvcConfigurer.super.addViewControllers(registry);
 	}
 
-	@Bean
-	public Filter contentCachingFilter() {
-		return new HttpContentCacheFilter();
-	}
-
 	/**
 	 * @see org.springframework.web.util.ContentCachingRequestWrapper#ContentCachingRequestWrapper(HttpServletRequest, int)
 	 */
 	@Bean
-	public Filter accessLogFilter() {
-		WebAccessLogFilter filter = new WebAccessLogFilter(
-			 "/user",
-			 "/user/view"
-		);
-		filter.setIncludeClientInfo(false);
-		filter.setIncludePayload(true);
-		filter.setIncludeHeaders(false);
-		filter.setIncludeQueryString(true);
-//		filter.setMaxPayloadLength(50);
-		filter.setBeforeMessagePrefix(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_PREFIX);
-		filter.setBeforeMessageSuffix(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_SUFFIX);
-		filter.setAfterMessagePrefix(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_PREFIX);
-		filter.setAfterMessageSuffix(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_SUFFIX);
-		return filter;
+	public FilterRegistrationBean<Filter> webAccessFilter() {
+		WebAccessLogFilter webAccessFilter = new WebAccessLogFilter();
+		webAccessFilter.setIncludeClientInfo(true);
+		webAccessFilter.setIncludePayload(true);
+		webAccessFilter.setIncludeHeaders(false);
+		webAccessFilter.setIncludeQueryString(true);
+		webAccessFilter.setMaxPayloadLength((int) RequestUtils.MAX_BODY_LENGTH.toBytes());
+		webAccessFilter.setBeforeMessagePrefix(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_PREFIX);
+		webAccessFilter.setBeforeMessageSuffix(AbstractRequestLoggingFilter.DEFAULT_BEFORE_MESSAGE_SUFFIX);
+		webAccessFilter.setAfterMessagePrefix(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_PREFIX);
+		webAccessFilter.setAfterMessageSuffix(AbstractRequestLoggingFilter.DEFAULT_AFTER_MESSAGE_SUFFIX);
+
+		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>(webAccessFilter);
+		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		registration.setUrlPatterns(Collections.singleton("*"));
+//		registration.setUrlPatterns(Collections.singleton("/user/*"));
+		return registration;
 	}
 }
