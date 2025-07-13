752b963ac49ebdbf26e69b4b9e40975eb77a728b
gmoon92 <gmoon0929@gmail.com>
2025-07-13 12:43:53 +0900

    chore: commons apache poi init

---
+++ commons-apache-poi/src/main/java/com/gmoon/commons/commonsapachepoi/CommonsApachePoiApplication.java
@@ -0,0 +1,13 @@
+package com.gmoon.commons.commonsapachepoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsApachePoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsApachePoiApplication.class, args);
+	}
+
+}
---
+++ commons-apache-poi/src/test/java/com/gmoon/commons/commonsapachepoi/CommonsApachePoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoon.commons.commonsapachepoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApachePoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
