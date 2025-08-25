21603fa6816b96d17609e1759aef96eaf64672e7
gmoon92 <gmoon0929@gmail.com>
2025-08-25 12:38:53 +0900

    feat(spring-poi): 컬림 길이 상수 추가

--- spring-poi/src/main/java/com/gmoon/springpoi/base/persistence/constants/ColumnLength.java
+++ spring-poi/src/main/java/com/gmoon/springpoi/base/persistence/constants/ColumnLength.java
@@ -10,6 +10,10 @@ public final class ColumnLength {
 	public static final int USERNAME = 20;
 
 	public static final int SHA_256 = 64;
-	public static final int ENUM = 20;
+	public static final int CODE10 = 10;
+	public static final int ENUM = 50;
 	public static final int PASSWORD_BCRYPT = 60;
+
+	public static final int FILENAME = 100;
+	public static final int EMAIL = 50;
 }
