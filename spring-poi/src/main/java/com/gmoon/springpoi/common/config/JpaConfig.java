2c73e0ddd2dd4141380195c5529bfcc70cc896dc
gmoon92 <gmoon0929@gmail.com>
2025-08-25 12:42:05 +0900

    chore(spring-poi): JPA 설정 추가

---
+++ spring-poi/src/main/java/com/gmoon/springpoi/common/config/JpaConfig.java
@@ -0,0 +1,24 @@
+package com.gmoon.springpoi.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.gmoon.springpoi.SpringPoiApplication;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackageClasses = SpringPoiApplication.class)
+public class JpaConfig {
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
+	}
+}
